24ee5618ce06105fc8dd7baf641af08d96365c41
Tom Hughes <tom@compton.nu>
2007-06-10 08:13:03 +0000

    Change server_settings to smtp_settings to avoid deprecation warning.

--- config/environment.rb
+++ config/environment.rb
@@ -56,7 +56,7 @@ end
 # Include your application configuration below
 SERVER_URL = ENV['OSM_SERVER_URL'] || 'www.openstreetmap.org'
 
-ActionMailer::Base.server_settings = {
+ActionMailer::Base.smtp_settings = {
   :address  => "localhost",
   :port  => 25, 
   :domain  => 'localhost',
