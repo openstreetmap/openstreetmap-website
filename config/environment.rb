7d6c6622968d670e6a2929afbd8efae41610db2c
Tom Hughes <tom@compton.nu>
2007-07-24 22:57:59 +0000

    Use SQL schemas, as ruby can't cope with the things we do...

--- config/environment.rb
+++ config/environment.rb
@@ -47,7 +47,7 @@ Rails::Initializer.run do |config|
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper, 
   # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
+  config.active_record.schema_format = :sql
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
