87a673e74c181c97931ad68c234fbff4293ee8de
Tom Hughes <tom@compton.nu>
2007-07-30 22:59:20 +0000

    Add migration to create session table in the database.

---
+++ db/migrate/003_sql_session_store_setup.rb
@@ -0,0 +1,15 @@
+class SqlSessionStoreSetup < ActiveRecord::Migration
+  def self.up
+    create_table "sessions", :options => "ENGINE=InnoDB" do |t|
+      t.column "session_id", :string
+      t.column "data",       :text
+      t.column "created_at", :timestamp
+      t.column "updated_at", :timestamp
+    end
+    add_index "sessions", ["session_id"], :name => "sessions_session_id_idx", :unique => true
+  end
+
+  def self.down
+    drop_table "sessions"
+  end
+end
