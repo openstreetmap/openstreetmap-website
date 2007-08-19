9861ca8001769b7cced2c8b5bcc0a87896f5439e
Tom Hughes <tom@compton.nu>
2007-08-19 09:16:33 +0000

    Move tag validation to the right place.

--- app/models/tracetag.rb
+++ app/models/tracetag.rb
@@ -1,6 +1,7 @@
 class Tracetag < ActiveRecord::Base
   set_table_name 'gpx_file_tags'
 
-  belongs_to :trace, :foreign_key => 'gpx_id'
+  validates_format_of :tag, :with => /^[^\/;.,?]*$/
 
+  belongs_to :trace, :foreign_key => 'gpx_id'
 end
