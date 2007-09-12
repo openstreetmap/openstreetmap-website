ebef7b8009928dafb9153fbf57531e021284db72
Tom Hughes <tom@compton.nu>
2007-09-12 17:59:48 +0000

    Add a migration to upgrade gps_points for quad tile support.

---
+++ db/migrate/005_tile_tracepoints.rb
@@ -0,0 +1,15 @@
+class TileTracepoints < ActiveRecord::Migration
+  def self.up
+    add_column "gps_points", "tile", :integer, :null => false, :options => "UNSIGNED"
+    add_index "gps_points", ["tile"], :name => "points_tile_idx"
+    remove_index "gps_points", :name => "points_idx"
+
+    Tracepoint.update_all("tile = tile_for_point(latitude, longitude)")
+  end
+
+  def self.down
+    add_index "gps_points", ["latitude", "longitude"], :name => "points_idx"
+    remove_index "gps_points", :name => "points_tile_idx"
+    remove_column "gps_points", "tile"
+  end
+end
