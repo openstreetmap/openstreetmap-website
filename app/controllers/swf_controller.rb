4c00efd678bf171d09a61d2d53b3d6ad52c44a40
Tom Hughes <tom@compton.nu>
2007-09-19 09:18:59 +0000

    Cope with change in number of decimals for GPS point positions.

--- app/controllers/swf_controller.rb
+++ app/controllers/swf_controller.rb
@@ -47,7 +47,7 @@ class SwfController < ApplicationController
 	
 		if params['token']
                         user=User.authenticate(:token => params[:token])
-			sql="SELECT gps_points.latitude*0.000001 AS lat,gps_points.longitude*0.000001 AS lon,gpx_files.id AS fileid,UNIX_TIMESTAMP(gps_points.timestamp) AS ts "+
+			sql="SELECT gps_points.latitude*0.0000001 AS lat,gps_points.longitude*0.0000001 AS lon,gpx_files.id AS fileid,UNIX_TIMESTAMP(gps_points.timestamp) AS ts "+
 				 " FROM gpx_files,gps_points "+
 				 "WHERE gpx_files.id=gpx_id "+
 				 "  AND gpx_files.user_id=#{user.id} "+
@@ -56,7 +56,7 @@ class SwfController < ApplicationController
 				 "ORDER BY fileid DESC,ts "+
 				 "LIMIT 10000"
 		else
-			sql="SELECT latitude*0.000001 AS lat,longitude*0.000001 AS lon,gpx_id AS fileid,UNIX_TIMESTAMP(timestamp) AS ts "+
+			sql="SELECT latitude*0.0000001 AS lat,longitude*0.0000001 AS lon,gpx_id AS fileid,UNIX_TIMESTAMP(timestamp) AS ts "+
 				 " FROM gps_points "+
 				 "WHERE "+OSM.sql_for_area(ymin,xmin,ymax,xmax,"gps_points.")+
 				 "  AND (gps_points.timestamp IS NOT NULL) "+
