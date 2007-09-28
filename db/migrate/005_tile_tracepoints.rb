e7d834625c8c97cde3c9d7f82535ffc43742756e
Tom Hughes <tom@compton.nu>
2007-09-28 23:18:13 +0000

    Fall back to a pure ruby update implementation if the tile_for_point function is not present in MySQL.

--- db/migrate/005_tile_tracepoints.rb
+++ db/migrate/005_tile_tracepoints.rb
@@ -4,7 +4,15 @@ class TileTracepoints < ActiveRecord::Migration
     add_index "gps_points", ["tile"], :name => "points_tile_idx"
     remove_index "gps_points", :name => "points_idx"
 
-    Tracepoint.update_all("latitude = latitude * 10, longitude = longitude * 10, tile = tile_for_point(latitude * 10, longitude * 10)")
+    begin
+      Tracepoint.update_all("latitude = latitude * 10, longitude = longitude * 10, tile = tile_for_point(latitude * 10, longitude * 10)")
+    rescue ActiveRecord::StatementInvalid => ex
+      Tracepoint.find(:all).each do |tp|
+        tp.latitude = tp.latitude * 10
+        tp.longitude = tp.longitude * 10
+        tp.save!
+      end
+    end
   end
 
   def self.down
