3aa8ffbcb8fa60a71f3f58b869ecd10bb397fd10
Tom Hughes <tom@compton.nu>
2007-10-30 00:48:45 +0000

    Tidy up mail notifiers and make them more rails style rather then relying on ActionMailer implementation details.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,62 +1,72 @@
 class Notifier < ActionMailer::Base
-
-  def signup_confirm( user, token )
-    @recipients = user.email
-    @from = 'abuse@openstreetmap.org'
-    @subject = '[OpenStreetMap] Confirm your email address'
-    @body['url'] = "http://#{SERVER_URL}/user/confirm?confirm_string=#{token.token}"
+  def signup_confirm(user, token)
+    recipients user.email
+    from "webmaster@openstreetmap.org"
+    subject "[OpenStreetMap] Confirm your email address"
+    body :url => url_for(:host => SERVER_URL,
+                         :controller => "user", :action => "confirm",
+                         :confirm_string => token.token)
   end
 
-  def lost_password( user, token )
-    @recipients = user.email
-    @from = 'abuse@openstreetmap.org'
-    @subject = '[OpenStreetMap] Password reset request'
-    @body['url'] = "http://#{SERVER_URL}/user/reset_password?email=#{user.email}&token=#{token.token}"
+  def lost_password(user, token)
+    recipients user.email
+    from "webmaster@openstreetmap.org"
+    subject "[OpenStreetMap] Password reset request"
+    body :url => url_for(:host => SERVER_URL,
+                         :controller => "user", :action => "reset_password",
+                         :email => user.email, :token => token.token)
   end
 
   def reset_password(user, pass)
-    @recipients = user.email
-    @from = 'abuse@openstreetmap.org'
-    @subject = '[OpenStreetMap] Password reset'
-    @body['pass'] = pass
+    recipients user.email
+    from "webmaster@openstreetmap.org"
+    subject "[OpenStreetMap] Password reset"
+    body :pass => pass
   end
 
   def gpx_success(trace, possible_points)
-    @recipients = trace.user.email
-    @from = 'abuse@openstreetmap.org'
-    @subject = '[OpenStreetMap] GPX Import success'
-    @body['trace_name'] = trace.name
-    @body['trace_points'] = trace.size
-    @body['possible_points'] = possible_points
+    recipients trace.user.email
+    from "webmaster@openstreetmap.org"
+    subject "[OpenStreetMap] GPX Import success"
+    body :trace_name => trace.name,
+         :trace_points => trace.size,
+         :possible_points => possible_points
   end
 
   def gpx_failure(trace, error)
-    @recipients = trace.user.email
-    @from = 'abuse@openstreetmap.org'
-    @subject = '[OpenStreetMap] GPX Import failure'
-    @body['trace_name'] = trace.name
-    @body['error'] = error
+    recipients trace.user.email
+    from "webmaster@openstreetmap.org"
+    subject "[OpenStreetMap] GPX Import failure"
+    body :trace_name => trace.name,
+         :error => error
   end
   
   def message_notification(message)
-    @recipients = message.recipient.email
-    @from = 'abuse@openstreetmap.org'
-    @subject = "[OpenStreetMap] #{message.sender.display_name} sent you a new message"
-    @body['to_user'] = message.recipient.display_name
-    @body['from_user'] = message.sender.display_name
-    @body['body'] = message.body
-    @body['subject'] = message.title
-    @body['readurl'] = "http://#{SERVER_URL}/message/read/#{message.id}"
-    @body['replyurl'] = "http://#{SERVER_URL}/message/new/#{message.from_user_id}"
+    recipients message.recipient.email
+    from "webmaster@openstreetmap.org"
+    subject "[OpenStreetMap] #{message.sender.display_name} sent you a new message"
+    body :to_user => message.recipient.display_name,
+         :from_user => message.sender.display_name,
+         :body => message.body,
+         :subject => message.title,
+         :readurl => url_for(:host => SERVER_URL,
+                             :controller => "message", :action => "read",
+                             :message_id => message.id),
+         :replyurl => url_for(:host => SERVER_URL,
+                              :controller => "message", :action => "new",
+                              :user_id => message.from_user_id)
   end
 
   def friend_notification(friend)
-    @friend = User.find_by_id(friend.user_id)
-    @new_friend = User.find_by_id(friend.friend_user_id)
-    @recipients = @new_friend.email
-    @from = 'abuse@openstreetmap.org'
-    @subject = "[OpenStreetMap] #{@friend.display_name} added you as a friend"
-    @body['user'] = @friend.display_name
-    @body['userurl'] = "http://#{SERVER_URL}/user/#{@friend.display_name}"
+    befriender = User.find_by_id(friend.user_id)
+    befriendee = User.find_by_id(friend.friend_user_id)
+
+    recipients befriendee.email
+    from "webmaster@openstreetmap.org"
+    subject "[OpenStreetMap] #{befriender.display_name} added you as a friend"
+    body :user => befriender.display_name,
+         :userurl => url_for(:host => SERVER_URL,
+                             :controller => "user", :action => "view",
+                             :display_name => befriender.display_name)
   end
 end
