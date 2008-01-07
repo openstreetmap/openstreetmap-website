38b7137fe3c5e88b0551c536aa1087a0b32145f4
Tom Hughes <tom@compton.nu>
2008-01-07 00:30:44 +0000

    Add migration to add an index on node_id to way_nodes. This index was added to the live database a month or so back to support rollback in Potlatch.

---
+++ db/migrate/009_way_nodes_node_idx.rb
@@ -0,0 +1,9 @@
+class WayNodesNodeIdx  < ActiveRecord::Migration
+  def self.up
+    add_index "way_nodes", ["node_id"], :name => "way_nodes_node_idx"
+  end
+
+  def self.down
+    remove_index "way_nodes", :name => "way_nodes_node_idx"
+  end
+end
