065d3dc16b5d70f2e3b40640e6c56af4e733b8f4
Tom Hughes <tom@compton.nu>
2008-01-15 00:41:07 +0000

    Don't try and limit the address space if the OS does not support it.

--- config/initializers/limits.rb
+++ config/initializers/limits.rb
@@ -1,5 +1,7 @@
-# Limit each rails process to a 512Mb resident set size
-Process.setrlimit Process::RLIMIT_AS, 640*1024*1024, Process::RLIM_INFINITY
+# Limit each rails process to a 512Mb resident set size if possible
+if Process.const_defined?(:RLIMIT_AS)
+  Process.setrlimit Process::RLIMIT_AS, 640*1024*1024, Process::RLIM_INFINITY
+end
 
 # Force a restart after every 10000 requests
 COUNT = 0
