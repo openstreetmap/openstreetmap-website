5692b8f3418c3d5e33add3801b29a1bb280bd157
Nick Black <nickb@svn.openstreetmap.org>
2008-01-24 11:24:32 +0000

    commented out migration which deletes the ways column nodes

--- db/migrate/013_populate_node_tags_and_remove.rb
+++ db/migrate/013_populate_node_tags_and_remove.rb
@@ -1,12 +1,13 @@
 class PopulateNodeTagsAndRemove < ActiveRecord::Migration
   def self.up
     #rake import 
-    remove_column :nodes, :tags
-    remove_column :current_nodes, :tags
+    #commented out to stop people from breaking their db
+#    remove_column :nodes, :tags
+#    remove_column :current_nodes, :tags
   end
 
   def self.down
-    add_column :nodes, "tags", :text, :default => "", :null => false
-    add_column :current_nodes, "tags", :text, :default => "", :null => false
+#    add_column :nodes, "tags", :text, :default => "", :null => false
+#    add_column :current_nodes, "tags", :text, :default => "", :null => false
   end
 end
