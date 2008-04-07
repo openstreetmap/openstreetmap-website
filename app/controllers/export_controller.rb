f8adbf080a672372dd8404481e5f8f8b9550b454
Tom Hughes <tom@compton.nu>
2008-04-07 23:11:25 +0000

    Remove export controller from branches where it isn't needed yet.

--- app/controllers/export_controller.rb
+++
@@ -1,8 +0,0 @@
-class ExportController < ApplicationController
-  def start
-    render :update do |page|
-      page.replace_html :sidebar_content, :partial => 'start'
-      page.call "openSidebar"
-    end
-  end
-end
