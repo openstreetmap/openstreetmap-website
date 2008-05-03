e15fa25639e96c7c1582d793b5b6c22c1969d2f9
Gabriel Ebner <gabriel@svn.openstreetmap.org>
2008-05-03 14:56:43 +0000

    rails_port: fix migration to create a primary key with auto increment (don't know if that will actually generate valid keys if there is already data in the table.)

--- db/migrate/012_add_user_preference_id.rb
+++ db/migrate/012_add_user_preference_id.rb
@@ -1,7 +1,9 @@
 class AddUserPreferenceId < ActiveRecord::Migration
   def self.up
-    remove_index 'user_preferences', ['id', 'k']
-    add_column "user_preferences", "id", :bigint, :limit => 64, :null => false, :options => "AUTO_INCREMENT"
+    remove_primary_key 'user_preferences'
+    add_column "user_preferences", "id", :bigint, :limit => 64, :null => false
+    add_primary_key "user_preferences", ["id"]
+    change_column "user_preferences", "id", :bigint, :limit => 64, :null => false, :options => "AUTO_INCREMENT"
     add_index "user_preferences", ["id"], :name => "user_preferences_id_idx"
   end
 
