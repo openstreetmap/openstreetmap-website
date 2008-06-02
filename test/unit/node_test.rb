f97c16d5c412a88e25d8eda160cee542d0bbd920
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2008-06-02 15:28:03 +0000

    You can now test nodes. This test needs looked at again, as it currently doesn't test the node tags

--- test/unit/node_test.rb
+++ test/unit/node_test.rb
@@ -1,16 +1,18 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class NodeTest < Test::Unit::TestCase
-  fixtures :current_nodes, :nodes, :users
+  fixtures :current_nodes, :nodes, :users, :current_node_tags, :node_tags
   set_fixture_class :current_nodes => :Node
   set_fixture_class :nodes => :OldNode
+  set_fixture_class :node_tags => :OldNodeTag
+  set_fixture_class :currenr_node_tags => :NodeTag
 
   def test_create
     node_template = Node.new(:latitude => 12.3456,
                              :longitude => 65.4321,
-                             :user_id => users(:normal_user).id,
-                             :visible => 1,
-                             :tags => "")
+                             :user_id => users(:normal_user),
+                             :visible => 1, 
+                             :version => 1)
     assert node_template.save_with_history!
 
     node = Node.find(node_template.id)
@@ -19,7 +21,6 @@ class NodeTest < Test::Unit::TestCase
     assert_equal node_template.longitude, node.longitude
     assert_equal node_template.user_id, node.user_id
     assert_equal node_template.visible, node.visible
-    assert_equal node_template.tags, node.tags
     assert_equal node_template.timestamp.to_i, node.timestamp.to_i
 
     assert_equal OldNode.find(:all, :conditions => [ "id = ?", node_template.id ]).length, 1
@@ -34,7 +35,7 @@ class NodeTest < Test::Unit::TestCase
   end
 
   def test_update
-    node_template = Node.find(1)
+    node_template = Node.find(current_nodes(:visible_node).id)
     assert_not_nil node_template
 
     assert_equal OldNode.find(:all, :conditions => [ "id = ?", node_template.id ]).length, 1
@@ -43,7 +44,7 @@ class NodeTest < Test::Unit::TestCase
 
     node_template.latitude = 12.3456
     node_template.longitude = 65.4321
-    node_template.tags = "updated=yes"
+    #node_template.tags = "updated=yes"
     assert node_template.save_with_history!
 
     node = Node.find(node_template.id)
@@ -52,7 +53,7 @@ class NodeTest < Test::Unit::TestCase
     assert_equal node_template.longitude, node.longitude
     assert_equal node_template.user_id, node.user_id
     assert_equal node_template.visible, node.visible
-    assert_equal node_template.tags, node.tags
+    #assert_equal node_template.tags, node.tags
     assert_equal node_template.timestamp.to_i, node.timestamp.to_i
 
     assert_equal OldNode.find(:all, :conditions => [ "id = ?", node_template.id ]).length, 2
@@ -63,12 +64,12 @@ class NodeTest < Test::Unit::TestCase
     assert_equal node_template.longitude, old_node.longitude
     assert_equal node_template.user_id, old_node.user_id
     assert_equal node_template.visible, old_node.visible
-    assert_equal node_template.tags, old_node.tags
+    #assert_equal node_template.tags, old_node.tags
     assert_equal node_template.timestamp.to_i, old_node.timestamp.to_i
   end
 
   def test_delete
-    node_template = Node.find(1)
+    node_template = Node.find(current_nodes(:visible_node))
     assert_not_nil node_template
 
     assert_equal OldNode.find(:all, :conditions => [ "id = ?", node_template.id ]).length, 1
@@ -84,7 +85,7 @@ class NodeTest < Test::Unit::TestCase
     assert_equal node_template.longitude, node.longitude
     assert_equal node_template.user_id, node.user_id
     assert_equal node_template.visible, node.visible
-    assert_equal node_template.tags, node.tags
+    #assert_equal node_template.tags, node.tags
     assert_equal node_template.timestamp.to_i, node.timestamp.to_i
 
     assert_equal OldNode.find(:all, :conditions => [ "id = ?", node_template.id ]).length, 2
@@ -95,7 +96,7 @@ class NodeTest < Test::Unit::TestCase
     assert_equal node_template.longitude, old_node.longitude
     assert_equal node_template.user_id, old_node.user_id
     assert_equal node_template.visible, old_node.visible
-    assert_equal node_template.tags, old_node.tags
+    #assert_equal node_template.tags, old_node.tags
     assert_equal node_template.timestamp.to_i, old_node.timestamp.to_i
   end
 end
