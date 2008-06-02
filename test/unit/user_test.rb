c373220bd22c81ed7fbdf0e08cf11823b7361b28
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2008-06-02 11:00:09 +0000

    sort the default test database user and password. Adding new test for the user

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -7,4 +7,18 @@ class UserTest < Test::Unit::TestCase
   def test_truth
     assert true
   end
+  
+  def test_invalid_with_empty_attributes
+    user = User.new
+    assert !user.valid?
+    assert user.errors.invalid?(:email)
+    assert user.errors.invalid?(:pass_crypt)
+    assert user.errors.invalid?(:display_name)
+    assert user.errors.invalid?(:email)
+    assert !user.errors.invalid?(:home_lat)
+    assert !user.errors.invalid?(:home_lon)
+    assert !user.errors.invalid?(:home_zoom)
+  end
+  
+  
 end
