f58fb85e03750eec372e381c1517035e9eb9837a
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2008-06-04 06:48:49 +0000

    improved messages fixtures. Additional user fixture (for sending messages between the users). Checking that an empty message fails.

--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -3,8 +3,13 @@ require File.dirname(__FILE__) + '/../test_helper'
 class MessageTest < Test::Unit::TestCase
   fixtures :messages
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_check_empty_message_fails
+    message = Message.new
+    assert !message.valid?
+    assert message.errors.invalid?(:title)
+    assert message.errors.invalid?(:body)
+    assert message.errors.invalid?(:sent_on)
+    assert true, message.message_read
   end
+  
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,11 +2,6 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < Test::Unit::TestCase
   fixtures :users
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
   
   def test_invalid_with_empty_attributes
     user = User.new
