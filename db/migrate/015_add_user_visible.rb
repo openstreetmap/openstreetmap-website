3c85b2ceea7e030f580d3febfad9f2b0c3217303
Tom Hughes <tom@compton.nu>
2008-08-24 16:56:34 +0000

    Fix migration.

--- db/migrate/015_add_user_visible.rb
+++ db/migrate/015_add_user_visible.rb
@@ -1,6 +1,6 @@
 class AddUserVisible < ActiveRecord::Migration
   def self.up
-    add_column "users", "visible", :boolean
+    add_column "users", "visible", :boolean, :default => true, :null => false
     User.update_all("visible = 1")
   end
 
