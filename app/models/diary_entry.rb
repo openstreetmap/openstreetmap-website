c771557b38a2f935ea79d113386c759636f4e31f
Tom Hughes <tom@compton.nu>
2008-10-12 18:42:34 +0000

    Disambiguate order clause.

--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -2,7 +2,7 @@ class DiaryEntry < ActiveRecord::Base
   belongs_to :user
   has_many :diary_comments, :include => :user,
                             :conditions => "users.visible = 1",
-                            :order => "id"
+                            :order => "diary_comments.id"
 
   validates_presence_of :title, :body
   validates_numericality_of :latitude, :allow_nil => true
