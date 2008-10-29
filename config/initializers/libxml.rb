b4c46ba4b8796d21c38fe737953808139adae741
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2008-10-29 15:45:48 +0000

    add a comment about why the error handler is required, as per mail from TomH. Also adding the option so that we are using the old style of migrations, rather than the 2.1+ migrations.

--- config/initializers/libxml.rb
+++ config/initializers/libxml.rb
@@ -1,8 +1,5 @@
-#require 'rubygems'
-#gem 'libxml-ruby', '>= 0.8.3'
-#require 'libxml'
-
-# Is this really needed?
+# This is required otherwise libxml writes out memory errors to
+# the standard output and exits uncleanly 
 LibXML::XML::Parser.register_error_handler do |message|
   raise message
 end
