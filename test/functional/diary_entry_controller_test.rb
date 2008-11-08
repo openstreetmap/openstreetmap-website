c6592045c482d9086d5db6ebab87db49aa85cefa
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2008-11-08 11:34:58 +0000

    Adding some diary fixtures and test stubs

--- test/functional/diary_entry_controller_test.rb
+++ test/functional/diary_entry_controller_test.rb
@@ -1,8 +1,35 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class DiaryEntryControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def basic_authorization(user, pass)
+    @request.env["HTTP_AUTHORIZATION"] = "Basic %s" % Base64.encode64("#{user}:#{pass}")
+  end
+
+  def content(c)
+    @request.env["RAW_POST_DATA"] = c.to_s
+  end
+  
+  def test_showing_create_diary_entry
+    
+  end
+  
+  def test_editing_diary_entry
+    
+  end
+  
+  def test_editing_creating_diary_comment
+    
+  end
+  
+  def test_listing_diary_entries
+    
+  end
+  
+  def test_rss
+    
+  end
+  
+  def test_viewing_diary_entry
+    
   end
 end
