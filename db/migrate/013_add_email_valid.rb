061d77726666beb8bccc27e0d410c4dc0ee4ef09
Andy Allan <gravitystorm@gmail.com>
2008-11-08 15:01:14 +0000

    Boolean correctness in migrations

--- db/migrate/013_add_email_valid.rb
+++ db/migrate/013_add_email_valid.rb
@@ -1,7 +1,7 @@
 class AddEmailValid < ActiveRecord::Migration
   def self.up
     add_column "users", "email_valid", :boolean, :default => false, :null => false
-    User.update_all("email_valid = active")
+    User.update_all(:email_valid => true)
   end
 
   def self.down
