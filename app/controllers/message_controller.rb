1a7ab3570ce850d7f541e2c709ad98986c7b35ef
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2008-12-03 12:56:02 +0000

    Deal with active record not found exceptions when creating message. Some tidying of html. For some reason in rails 2.1.2 having a comment at the end of a line, in the view,  means that the closing tag needs to be on the next line.

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -7,11 +7,13 @@ class MessageController < ApplicationController
   # Allow the user to write a new message to another user. This action also 
   # deals with the sending of that message to the other user when the user
   # clicks send.
+  # The user_id param is the id of the user that the message is being sent to.
   def new
     @title = 'send message'
+    @to_user = User.find(params[:user_id])
     if params[:message]
       @message = Message.new(params[:message])
-      @message.to_user_id = params[:user_id]
+      @message.to_user_id = @to_user.id
       @message.from_user_id = @user.id
       @message.sent_on = Time.now
    
@@ -23,6 +25,8 @@ class MessageController < ApplicationController
     else
       @title = params[:title]
     end
+  rescue ActiveRecord::RecordNotFound
+    render :action => 'no_such_user', :status => :not_found
   end
 
   # Allow the user to reply to another message.
@@ -33,7 +37,7 @@ class MessageController < ApplicationController
     @user_id = message.from_user_id
     render :action => 'new'
   rescue ActiveRecord::RecordNotFound
-    render :nothing => true, :status => :not_found
+    render :action => 'no_such_user', :status => :not_found
   end
 
   # Show a message
@@ -43,7 +47,7 @@ class MessageController < ApplicationController
     @message.message_read = true if @message.to_user_id == @user.id
     @message.save
   rescue ActiveRecord::RecordNotFound
-    render :nothing => true, :status => :not_found
+    render :action => 'no_such_user', :status => :not_found
   end
 
   # Display the list of messages that have been sent to the user.
@@ -82,5 +86,7 @@ class MessageController < ApplicationController
         redirect_to :controller => 'message', :action => 'inbox', :display_name => @user.display_name
       end
     end
+  rescue ActiveRecord::RecordNotFound
+    render :action => 'no_such_user', :status => :not_found
   end
 end
