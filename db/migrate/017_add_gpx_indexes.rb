66a2558e65ff6d2b786ce9673639d74b985c934f
Tom Hughes <tom@compton.nu>
2008-12-12 15:43:02 +0000

    Add some extra indexes.

---
+++ db/migrate/017_add_gpx_indexes.rb
@@ -0,0 +1,11 @@
+class AddGpxIndexes < ActiveRecord::Migration
+  def self.up
+    add_index "gpx_files", ["user_id"], :name => "gpx_files_user_id_idx"
+    add_index "gpx_file_tags", ["tag"], :name => "gpx_file_tags_tag_idx"
+  end
+
+  def self.down
+    remove_index "gpx_file_tags", :name => "gpx_file_tags_tag_idx"
+    remove_index "gpx_files", :name => "gpx_files_user_id_idx"
+  end
+end
