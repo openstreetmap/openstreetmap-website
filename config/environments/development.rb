3f30bc53f543e13221ba0b7c52284865f9593107
Paul Gillard <paulmgillard@googlemail.com>
2009-02-10 22:42:45 +0000

    Ensure homepage source validates.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,3 +16,5 @@ config.action_view.cache_template_extensions         = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+ENV['RAILS_ASSET_ID'] = '' # http://wiki.openstreetmap.org/wiki/The_Rails_Port
--- config/initializers/libxml.rb
+++ config/initializers/libxml.rb
@@ -2,6 +2,4 @@ require 'rubygems'
 gem 'libxml-ruby', '>= 0.8.3'
 require 'libxml'
 
-LibXML::XML::Parser.register_error_handler do |message|
-  raise message
-end
+LibXML::XML::Parser.register_error_handler(proc{})
