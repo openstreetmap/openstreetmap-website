45dfd0f05f5400f7a9702a3d82494c2697d0476c
Tom Hughes <tom@compton.nu>
2009-04-15 08:03:26 +0000

    Fix spelling mistake.

--- app/controllers/relation_controller.rb
+++ app/controllers/relation_controller.rb
@@ -3,7 +3,7 @@ class RelationController < ApplicationController
 
   session :off
   before_filter :authorize, :only => [:create, :update, :delete]
-  before_filter :check_api_writeable, :only => [:create, :update, :delete]
+  before_filter :check_api_writable, :only => [:create, :update, :delete]
   before_filter :check_api_readable, :except => [:create, :update, :delete]
   after_filter :compress_output
 
