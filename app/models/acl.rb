bb7c4f93e239c6ff6155b09ffe72a090be3af4f0
Tom Hughes <tom@compton.nu>
2009-04-16 20:40:32 +0000

    Cope with MySQL and Postgres.

--- app/models/acl.rb
+++ app/models/acl.rb
@@ -1,13 +1,23 @@
 class Acl < ActiveRecord::Base
   def self.find_by_address(address, options)
-    self.with_scope(:find => {:conditions => ["inet_aton(?) & netmask = address", address]}) do
+    self.with_scope(:find => {:conditions => ["#{inet_aton} & netmask = address", address]}) do
       return self.find(:first, options)
     end
   end
 
   def self.find_all_by_address(address, options)
-    self.with_scope(:find => {:conditions => ["inet_aton(?) & netmask = address", address]}) do
+    self.with_scope(:find => {:conditions => ["#{inet_aton} & netmask = address", address]}) do
       return self.find(:all, options)
+    end
+  end
+
+private
+
+  def self.inet_aton
+    if self.connection.adapter_name == "MySQL"
+      "inet_aton(?)"
+    else
+      "?"
     end
   end
 end
