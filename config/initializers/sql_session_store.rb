66c95aa3f909f9887593fd16174d1d8a966cad2e
Tom Hughes <tom@compton.nu>
2009-04-16 20:29:18 +0000

    Work out which session store class to use automatically.

--- config/initializers/sql_session_store.rb
+++ config/initializers/sql_session_store.rb
@@ -1,2 +1,7 @@
-# Use the MySQL interface for SqlSessionStore
-SqlSessionStore.session_class = MysqlSession
+# Work out which session store adapter to use
+environment = Rails.configuration.environment
+adapter = Rails.configuration.database_configuration[environment]["adapter"]
+session_class = adapter + "_session"
+
+# Configure SqlSessionStore
+SqlSessionStore.session_class = session_class.camelize.constantize
