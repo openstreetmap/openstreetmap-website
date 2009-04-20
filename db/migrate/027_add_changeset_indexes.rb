7c791f302c68be7110deabe73d7c8db97d1d13ef
Tom Hughes <tom@compton.nu>
2009-04-20 13:39:20 +0000

    Add changeset indexes to nodes, ways and relations.

---
+++ db/migrate/027_add_changeset_indexes.rb
@@ -0,0 +1,13 @@
+class AddChangesetIndexes < ActiveRecord::Migration
+  def self.up
+    add_index "nodes", ["changeset_id"], :name => "nodes_changeset_id_idx"
+    add_index "ways", ["changeset_id"], :name => "ways_changeset_id_idx"
+    add_index "relations", ["changeset_id"], :name => "relations_changeset_id_idx"
+  end
+
+  def self.down
+    remove_index "relations", :name => "relations_changeset_id_idx"
+    remove_index "ways", :name => "ways_changeset_id_idx"
+    remove_index "nodes", :name => "nodes_changeset_id_idx"
+  end
+end
