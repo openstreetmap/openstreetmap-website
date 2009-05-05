589e5835c8a01ffc1ac52a995b9c428224e890a5
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2009-05-05 18:31:37 +0000

    some tests for the site and export controllers

--- test/functional/export_controller_test.rb
+++ test/functional/export_controller_test.rb
@@ -2,7 +2,25 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ExportControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_start
+    get :start
+    assert_response :success
+    assert_template 'start'
   end
+  
+  def test_finish_osm
+    get :finish, {:minlon => 0, :minlat => 50, :maxlon => 1, :maxlat => 51, :format => 'osm'}
+    assert_response :redirect
+  end
+  
+  def test_finish_mapnik
+    get :finish, {:minlon => 0, :minlat => 50, :maxlon => 1, :maxlat => 51, :format => 'mapnik', :mapnik_format => 'test', :mapnik_scale => '12'}
+    assert_response :redirect
+  end
+  
+  def test_finish_osmarender
+    get :finish, {:minlon => 0, :minlat => 50, :maxlon => 1, :maxlat => 51, :format => 'osmarender', :osmarender_format => 'test', :osmarender_zoom => '12'}
+    assert_response :redirect
+  end
+    
 end
--- test/functional/site_controller_test.rb
+++ test/functional/site_controller_test.rb
@@ -1,8 +1,35 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class SiteControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  ## Lets check that we can get all the pages without any errors
+  
+  # Get the index
+  def test_index
+    get :index
+    assert_response :success
+    assert_template 'index'
+    # Seems that we need to wait for Rails 2.3 for this one
+    # assert_template :partial => '_search', :count => 1
+  end
+  
+  # Get the edit page
+  def test_edit
+    get :edit
+    # Should be redirected
+    assert_response :redirect
+  end
+  
+  # Get the export page
+  def test_export
+    get :export
+    assert_response :success
+    assert_template 'index'
+  end
+  
+  # Offline page
+  def test_offline
+    get :offline
+    assert_response :success
+    assert_template 'offline'
   end
 end
