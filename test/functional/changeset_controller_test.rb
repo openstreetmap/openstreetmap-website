86caeb336089de295c2490912c01c6534634f6af
Thomas Wood <grand.edgemaster@gmail.com>
2009-05-07 22:58:32 +0000

    Fix the test I just broke...

--- test/functional/changeset_controller_test.rb
+++ test/functional/changeset_controller_test.rb
@@ -1423,7 +1423,7 @@ EOF
     assert_template "list"
     # Now check that all 20 (or however many were returned) changesets are in the html
     assert_select "h1", :text => "Recent Changes", :count => 1
-    assert_select "table[id='keyvalue'] tr", :count => @changesets.size + 1
+    assert_select "table[id='changeset_list'] tr", :count => @changesets.size + 1
     @changesets.each do |changeset|
       # FIXME this test needs rewriting - test for table contents
     end
