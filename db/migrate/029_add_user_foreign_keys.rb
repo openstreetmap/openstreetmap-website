3404fd5bbe0550348a4c0a2024024db61b656f04
Tom Hughes <tom@compton.nu>
2009-05-07 17:54:52 +0000

    Add foreign key constraints for users.

---
+++ db/migrate/029_add_user_foreign_keys.rb
@@ -0,0 +1,18 @@
+class AddUserForeignKeys < ActiveRecord::Migration
+  def self.up
+    add_foreign_key :changesets, [:user_id], :users, [:id]
+    add_foreign_key :diary_comments, [:user_id], :users, [:id]
+    add_foreign_key :diary_entries, [:user_id], :users, [:id]
+    add_foreign_key :friends, [:user_id], :users, [:id]
+    add_foreign_key :friends, [:friend_user_id], :users, [:id]
+    add_foreign_key :gpx_files, [:user_id], :users, [:id]
+    add_foreign_key :messages, [:from_user_id], :users, [:id]
+    add_foreign_key :messages, [:to_user_id], :users, [:id]
+    add_foreign_key :user_preferences, [:user_id], :users, [:id]
+    add_foreign_key :user_tokens, [:user_id], :users, [:id]
+  end
+
+  def self.down
+    raise IrreversibleMigration.new
+  end
+end
