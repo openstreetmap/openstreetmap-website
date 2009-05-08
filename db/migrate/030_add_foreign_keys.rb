a0a6dd36190788b89c5d97f981b4e9b563eb385e
Tom Hughes <tom@compton.nu>
2009-05-08 00:03:40 +0000

    Add yet more foreign keys.

---
+++ db/migrate/030_add_foreign_keys.rb
@@ -0,0 +1,12 @@
+class AddForeignKeys < ActiveRecord::Migration
+  def self.up
+    add_foreign_key :changeset_tags, [:id], :changesets
+    add_foreign_key :diary_comments, [:diary_entry_id], :diary_entries, [:id]
+    add_foreign_key :gps_points, [:gpx_id], :gpx_files, [:id]
+    add_foreign_key :gpx_file_tags, [:gpx_id], :gpx_files, [:id]
+  end
+
+  def self.down
+    raise IrreversibleMigration.new
+  end
+end
