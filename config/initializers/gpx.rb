6fe22d41a96ea01cc02c9439a195fe1cda4fb0c7
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2009-05-22 18:41:33 +0000

    Reverting config file that shouldn't have been committed.

--- config/initializers/gpx.rb
+++ config/initializers/gpx.rb
@@ -1,5 +1,5 @@
 # Set location of GPX trace files
-GPX_TRACE_DIR = "/Users/shaunmcdonald/Documents/osm/pg_api06/public/traces"
+GPX_TRACE_DIR = "/home/osm/traces"
 
 # Set location of GPX image files
-GPX_IMAGE_DIR = "/Users/shaunmcdonald/Documents/osm/pg_api06/public/images"
+GPX_IMAGE_DIR = "/home/osm/images"
