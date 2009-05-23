0e7127832037902f83cd15db2d413c23576eade7
Tom Hughes <tom@compton.nu>
2009-05-23 00:00:25 +0000

    Rename language_code to code - language_code is tautological when it's in the language table anyway.

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,6 @@
 class Language < ActiveRecord::Base
-  set_primary_key :language_code
-  
+  set_primary_key :code
+
   has_many :users, :foreign_key => 'locale'
   has_many :diary_entries, :foreign_key => 'language'
 end
--- db/migrate/034_create_languages.rb
+++ db/migrate/034_create_languages.rb
@@ -2,23 +2,22 @@ require 'lib/migrate'
 
 class CreateLanguages < ActiveRecord::Migration
   def self.up
-    create_table :languages do |t|
-      t.string :language_code, :limit => 5, :null => false
+    create_table :languages, innodb_table do |t|
+      t.string :code, :limit => 5, :null => false
       t.string :name, :null => false
       t.boolean :translation_available, :null => false, :default => false
-
-      t.timestamps
     end
-    
-    add_index :languages, [:language_code], :unique => true
 
-    Language.create(:language_code => 'en', :name => 'English', :translation_available => true)
-    
-    add_foreign_key :users, [:locale], :languages, [:language_code]
-    add_foreign_key :diary_entries, [:language], :languages, [:language_code]    
+    add_primary_key :languages, [:code]
+
+    Language.create(:code => 'en', :name => 'English', :translation_available => true)
+
+    add_foreign_key :users, [:locale], :languages, [:code]
+    add_foreign_key :diary_entries, [:language], :languages, [:code]    
   end
 
   def self.down
     raise IrreversibleMigration.new
   end
 end
+
