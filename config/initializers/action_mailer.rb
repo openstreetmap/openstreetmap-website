2b7f92029f678417025f57c8f6d28e9aea82ad98
Tom Hughes <tom@compton.nu>
2009-05-30 15:51:35 +0000

    Remove pointless bit of setup that used an undefined app config key.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -5,9 +5,6 @@ ActionMailer::Base.smtp_settings = {
   :domain => 'localhost',
 }
 
-# This will let you more easily use helpers based on url_for in your mailers.
-ActionMailer::Base.default_url_options[:host] = APP_CONFIG['host']
-
 # Monkey patch to fix return-path bug in ActionMailer 2.2.2
 # Can be removed once we go to 2.3
 module Net
