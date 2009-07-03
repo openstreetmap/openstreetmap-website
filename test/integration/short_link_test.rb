feae60fb6940ee8c6983d29c0f23c2b55fefebc7
Matt Amos <zerebubuth@gmail.com>
2009-07-03 11:19:23 +0000

    Fixed short link integration test so that it would have caught a bug in shortlink charset.

--- test/integration/short_link_test.rb
+++ test/integration/short_link_test.rb
@@ -6,7 +6,7 @@ class ShortLinkTest < ActionController::IntegrationTest
   # kept in the redirect.
   def test_short_link_params
     assert_short_link_redirect('1N8H@P_5W')
-    assert_short_link_redirect('euu4oTas==')
+    assert_short_link_redirect(ShortLink::encode(-0.107846, 51.50771, 18))
   end
 
   ##
