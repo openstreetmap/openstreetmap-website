daf8c801614f0bcc538ab76c16e6d23bcbdec77a
Tom Hughes <tom@compton.nu>
2009-11-09 09:28:46 +0000

    Use the bbox to build the URL if there is one.

--- app/helpers/geocoder_helper.rb
+++ app/helpers/geocoder_helper.rb
@@ -2,7 +2,12 @@ module GeocoderHelper
   def result_to_html(result)
     html_options = {}
     #html_options[:title] = strip_tags(result[:description]) if result[:description]
-    html_options[:href] = "?mlat=#{result[:lat]}&mlon=#{result[:lon]}&zoom=#{result[:zoom]}"
+    if result[:min_lon] and result[:min_lat] and result[:max_lon] and result[:max_lat]
+      html_options[:href] = "?minlon=#{result[:min_lon]}&minlat=#{result[:min_lat]}&maxlon=#{result[:max_lon]}&maxlat=#{result[:max_lat]}"
+    else
+      html_options[:href] = "?mlat=#{result[:lat]}&mlon=#{result[:lon]}&zoom=#{result[:zoom]}"
+    end
+
     html = ""
     html << result[:prefix] if result[:prefix]
     html << " " if result[:prefix] and result[:name]
