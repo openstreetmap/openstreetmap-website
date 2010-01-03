6e5f284135aa2aabd3d400921caec8c5a9073c68
Tom Hughes <tom@compton.nu>
2010-01-03 11:56:41 +0000

    Add some indexes to diary entries to let recent entries be found efficiently for the various diary entry lists and feeds.

---
+++ db/migrate/048_add_diary_creation_indexes.rb
@@ -0,0 +1,13 @@
+class AddDiaryCreationIndexes < ActiveRecord::Migration
+  def self.up
+    add_index :diary_entries, [:created_at], :name => "diary_entry_created_at_index"
+    add_index :diary_entries, [:user_id, :created_at], :name => "diary_entry_user_id_created_at_index"
+    add_index :diary_entries, [:language_code, :created_at], :name => "diary_entry_language_code_created_at_index"
+  end
+
+  def self.down
+    remove_index :diary_entries, :name => "diary_entry_language_code_created_at_index"
+    remove_index :diary_entries, :name => "diary_entry_user_id_created_at_index"
+    remove_index :diary_entries, :name => "diary_entry_created_at_index"
+  end
+end
