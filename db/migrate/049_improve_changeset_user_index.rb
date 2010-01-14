8de014935a2946fb36a8dcde0cd5ee409c54e793
Tom Hughes <tom@compton.nu>
2010-01-14 18:37:52 +0000

    Add the changeset id to the user index so the next/previous user links can be resolved quickly.

---
+++ db/migrate/049_improve_changeset_user_index.rb
@@ -0,0 +1,11 @@
+class ImproveChangesetUserIndex < ActiveRecord::Migration
+  def self.up
+    add_index :changesets, [:user_id, :id], :name => "changesets_user_id_id_idx"
+    remove_index :changesets, :name => "changesets_user_id_idx"
+  end
+
+  def self.down
+    add_index :changesets, [:user_id], :name => "changesets_user_id_idx"
+    remove_index :changesets, [:user_id, :id], :name => "changesets_user_id_id_idx"
+  end
+end
