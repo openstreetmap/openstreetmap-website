8eead8a8707b478b342a6e4c13c4e8b4ef143b98
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-04-20 12:55:14 +0000

    Ticket #2245: Link from /browse/* pages to the wiki key/value pages

--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -15,4 +15,42 @@ module BrowseHelper
     end
     return name
   end
+
+  def wiki_link(type, key, tag)
+    wiki_data = YAML.load_file("#{RAILS_ROOT}/config/wiki-tag-and-key-description.yml")
+    my_locale = I18n.locale.to_s
+
+    if type == "key"
+      ret = key
+      lookup = key
+    else
+      ret = tag
+      lookup = key + "=" + tag
+    end
+
+    # Try our native language
+    has_primary = wiki_data[my_locale][type][lookup] rescue false
+    if has_primary
+      ret = wikify(type, key, tag, lookup, wiki_data[my_locale][type][lookup])
+    else
+      # Fall back on English
+      has_fallback = wiki_data["en"][type][lookup] rescue false
+      if has_fallback
+        ret = wikify(type, key, tag, lookup, wiki_data["en"][type][lookup])
+      end
+    end
+
+    return ret
+  end
+
+  def wikify(type, key, tag, text, wiki)
+    my_locale = I18n.locale
+    url = "http://wiki.openstreetmap.org/index.php?title=#{wiki}&uselang=#{my_locale}"
+    
+    if type == "key"
+      return '<a href="' + url + '" title="' + h(t('browse.tag_details.wiki_link.key', :key => key)) + '">' + h(text) + '</a>'
+    else
+      return '<a href="' + url + '" title="' + h(t('browse.tag_details.wiki_link.tag', :key => key, :value => tag)) + '">' + h(tag) + '</a>'
+    end
+  end
 end
