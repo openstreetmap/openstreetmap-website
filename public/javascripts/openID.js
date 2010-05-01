2b21b7f841faa4f4387e500895b921c8b1941f4e
Kai Krueger <kai@aiputerlx.(none)>
2010-05-01 19:03:31 +0100

    Forgott to commit a javascript file

---
+++ public/javascripts/openID.js
@@ -0,0 +1,27 @@
+function openid_signin(provider)
+{
+    if (provider == 'google') {
+        document.forms[0].user_openid_url.value="gmail.com";
+        document.forms[0].submit();
+    } else if (provider == 'yahoo') {
+        document.forms[0].user_openid_url.value="yahoo.com";
+        document.forms[0].submit();
+    } else if (provider == 'myopenid') {
+        document.forms[0].user_openid_url.value="myopenid.com";
+        document.forms[0].submit();
+    } else if (provider == 'wordpress') {
+        document.forms[0].user_openid_url.value="wordpress.com";
+        document.forms[0].submit();
+    } else if (provider == 'myspace') {
+        document.forms[0].user_openid_url.value="myspace.com";
+        document.forms[0].submit();
+    } else if (provider == 'openid') {
+        document.forms[0].user_openid_url.value="http://";
+    }
+
+}
+
+function unhideopenID() {
+	document.getElementById("openID_row-1").style.display = "";
+	document.getElementById("openID_row-2").style.display = "";
+}
