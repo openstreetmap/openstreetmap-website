ebd109c946d71b3ccc616ea3966e56ae019638d7
Tom Hughes <tom@compton.nu>
2010-05-09 12:25:50 +0100

    Replace openID.js with funky rails generated javascript

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,12 @@
 module UserHelper
+  def openid_button(name, url)
+    link_to_function(
+      image_tag("#{name}.png", :alt => t("user.login.openid_providers.#{name}.alt")),
+      nil,
+      :title => t("user.login.openid_providers.#{name}.title")      
+    ) do |page|
+      page[:login_form][:user_openid_url][:value] = url
+      page[:login_form].submit()
+    end
+  end
 end
