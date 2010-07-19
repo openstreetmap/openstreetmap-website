5e2b8d087c868107e6013f1745a09b0c1dffa4e9
Tom Hughes <tom@compton.nu>
2010-07-19 18:01:42 +0100

    Make memory limits configurable and make them work with passenger

--- config/initializers/limits.rb
+++
@@ -1,4 +0,0 @@
-# Set a hard limit of 1Gb on the virtual size of the process
-if Process.const_defined?(:RLIMIT_AS)
-  Process.setrlimit Process::RLIMIT_AS, 1024*1024*1024, Process::RLIM_INFINITY
-end
