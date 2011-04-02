452879ee03951f017ef356f47bda79a7fc7b3cf4
Tom Hughes <tom@compton.nu>
2011-04-02 16:35:20 +0100

    Don't run the user terms tests unless REQUIRE_TERMS_SEEN is set

--- test/integration/user_terms_seen_test.rb
+++ test/integration/user_terms_seen_test.rb
@@ -8,36 +8,40 @@ class UserTermsSeenTest < ActionController::IntegrationTest
   end
 
   def test_api_blocked
-    user = users(:terms_not_seen_user)
+    if REQUIRE_TERMS_SEEN
+      user = users(:terms_not_seen_user)
 
-    get "/api/#{API_VERSION}/user/details", nil, auth_header(user.display_name, "test")
-    assert_response :forbidden
+      get "/api/#{API_VERSION}/user/details", nil, auth_header(user.display_name, "test")
+      assert_response :forbidden
 
-    # touch it so that the user has seen the terms
-    user.terms_seen = true
-    user.save
+      # touch it so that the user has seen the terms
+      user.terms_seen = true
+      user.save
 
-    get "/api/#{API_VERSION}/user/details", nil, auth_header(user.display_name, "test")
-    assert_response :success
+      get "/api/#{API_VERSION}/user/details", nil, auth_header(user.display_name, "test")
+      assert_response :success
+    end
   end
 
   def test_terms_presented_at_login
-    user = users(:terms_not_seen_user)
+    if REQUIRE_TERMS_SEEN
+      user = users(:terms_not_seen_user)
 
-    # try to log in
-    get_via_redirect "/login"
-    assert_response :success
-    assert_template 'user/login'
-    post "/login", {'user[email]' => user.email, 'user[password]' => 'test', :referer => "/"}
-    assert_response :redirect
-    # but now we need to look at the terms
-    assert_redirected_to "controller" => "user", "action" => "terms", :referer => "/"
-    follow_redirect!
-    assert_response :success
+      # try to log in
+      get_via_redirect "/login"
+      assert_response :success
+      assert_template 'user/login'
+      post "/login", {'user[email]' => user.email, 'user[password]' => 'test', :referer => "/"}
+      assert_response :redirect
+      # but now we need to look at the terms
+      assert_redirected_to "controller" => "user", "action" => "terms", :referer => "/"
+      follow_redirect!
+      assert_response :success
 
-    # don't agree to the terms, but hit decline
-    
-    # should be carried through to a normal login
+      # don't agree to the terms, but hit decline
+
+      # should be carried through to a normal login
+    end
   end
 
 end
