5f7855fa4d07fbee05de4f67d0aa370c8131ff2f
Tom Hughes <tom@compton.nu>
2011-04-06 21:22:47 +0100

    Tell Potlatch 2 which locale to use

---
+++ lib/potlatch2.rb
@@ -0,0 +1,13 @@
+module Potlatch2
+  LOCALES = {
+    "de" => "de_DE",
+    "de-DE" => "de_DE",
+    "en" => "en_US",
+    "en-GB" => "en_GB",
+    "en-US" => "en_US",
+    "fr" => "fr_FR",
+    "fr-FR" => "fr_FR",
+    "pl" => "pl_PL",
+    "pl-PL" => "pl_PL"
+  }
+end
