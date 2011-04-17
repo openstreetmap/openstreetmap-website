2fd52ddf6024ebdbebf451524836e67f7b4f07a2
Tom Hughes <tom@compton.nu>
2011-04-17 18:56:50 +0100

    Add memcache support

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,9 @@ Rails::Initializer.run do |config|
   config.gem 'SystemTimer', :version => '>= 1.1.3', :lib => 'system_timer'
   config.gem 'sanitize'
   config.gem 'i18n', :version => '>= 0.5.0'
+  if defined?(MEMCACHE_SERVERS)
+    config.gem 'memcached'
+  end
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
