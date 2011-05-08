7bba7989909c9137561d9e330abcfadbf5212e44
Tom Hughes <tom@compton.nu>
2011-05-08 12:34:53 +0100

    Add a C implementation of QuadTile.iterate_tiles_for_area

--- lib/quad_tile.rb
+++ lib/quad_tile.rb
@@ -39,6 +39,25 @@ module QuadTile
 
       return t
     end
+
+    def self.iterate_tiles_for_area(minlat, minlon, maxlat, maxlon)
+      tiles = tiles_for_area(minlat, minlon, maxlat, maxlon)
+      first = last = nil
+
+      tiles.sort.each do |tile|
+        if last.nil?
+          first = last = tile
+        elsif tile == last + 1
+          last = tile
+        else
+          yield first, last
+
+          first = last = tile
+        end
+      end
+
+      yield first, last unless last.nil?
+    end
   end
 
   def self.sql_for_area(minlat, minlon, maxlat, maxlon, prefix)
@@ -56,25 +75,6 @@ module QuadTile
     sql.push("#{prefix}tile IN (#{single.join(',')})") if single.size > 0
 
     return "( " + sql.join(" OR ") + " )"
-  end
-
-  def self.iterate_tiles_for_area(minlat, minlon, maxlat, maxlon)
-    tiles = tiles_for_area(minlat, minlon, maxlat, maxlon)
-    first = last = nil
-
-    tiles.sort.each do |tile|
-      if last.nil?
-        first = last = tile
-      elsif tile == last + 1
-        last = tile
-      else
-        yield first, last
-
-        first = last = tile
-      end
-    end
-
-    yield first, last unless last.nil?
   end
 
   private_class_method :tile_for_xy, :iterate_tiles_for_area
