93fded964161e190eda2ff220c2b2229e10d92da
Tom Hughes <tom@compton.nu>
2011-09-30 01:02:09 +0100

    Drop the sql_session_store plugin
    The sql_session_store plugin doesn't work with rails 3 and doesn't
    seem to be supported anymore, so drop it in favour of the builtin
    database backed session storage.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-OpenStreetMap::Application.config.session_store :cookie_store, :key => '_osm_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# OpenStreetMap::Application.config.session_store :active_record_store
+if STATUS == :database_offline or STATUS == :database_readonly
+  OpenStreetMap::Application.config.session_store :cookie_store, :key => '_osm_session'
+else
+  ActiveRecord::SessionStore.session_class = ActiveRecord::SessionStore::SqlBypass
+  OpenStreetMap::Application.config.session_store :active_record_store, :key => '_osm_session'
+end
--- config/initializers/sql_session_store.rb
+++
@@ -1,8 +0,0 @@
-# Work out which session store adapter to use
-adapter = Rails.configuration.database_configuration[Rails.env]["adapter"]
-session_class = adapter + "_session"
-
-# Configure SqlSessionStore
-unless STATUS == :database_offline
-  SqlSessionStore.session_class = session_class.camelize.constantize
-end
