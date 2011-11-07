0e33245b1d073afadbb178ac3787e3399742b527
Tom Hughes <tom@compton.nu>
2011-11-07 10:57:38 +0000

    Disable rack caching in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ OpenStreetMap::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Disable rack caching
+  config.action_dispatch.rack_cache = false
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
