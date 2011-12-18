d5561dd27a3affe7f395febf4880110f66e2af74
Tom Hughes <tom@compton.nu>
2011-12-18 16:51:14 +0000

    Use normal fallbacks to handle the no/nb confusion
    This requires (for now) a monkey patch to the i18n fallback code, which
    has been offered upstream as https://github.com/svenfuchs/i18n/pull/130.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,21 +1,5 @@
 module I18n
   module Backend
-    class Simple
-      module Implementation
-        protected
-        alias_method :old_init_translations, :init_translations
-      
-        def init_translations
-          old_init_translations
-
-          store_translations(:nb, translations[:no])
-          translations[:no] = translations[:nb]
-
-          @skip_syntax_deprecation = true
-        end
-      end
-    end
-
     module Fallbacks
       def find_first_string_or_lambda_default(defaults)
         defaults.each_with_index { |default, ix| return ix if default && !default.is_a?(Symbol) }
@@ -32,6 +16,20 @@ module I18n
       end
     end
   end
+
+  module Locale
+    class Fallbacks
+      def compute(tags, include_defaults = true, exclude = [])
+        result = Array(tags).collect do |tag|
+          tags = I18n::Locale::Tag.tag(tag).self_and_parents.map! { |t| t.to_sym } - exclude
+          tags.each { |_tag| tags += compute(@map[_tag], false, exclude + tags) if @map[_tag] }
+          tags
+        end.flatten
+        result.push(*defaults) if include_defaults
+        result.uniq.compact
+      end
+    end
+  end
 end
 
 I18n::Backend::Simple.include(I18n::Backend::Pluralization)
@@ -39,6 +37,9 @@ I18n::Backend::Simple.include(I18n::Backend::PluralizationFallback)
 I18n.load_path << "#{Rails.root}/config/pluralizers.rb"
 
 I18n::Backend::Simple.include(I18n::Backend::Fallbacks)
+
+I18n.fallbacks.map("nb" => "no")
+I18n.fallbacks.map("no" => "nb")
 
 Rails.configuration.after_initialize do
   I18n.reload!
