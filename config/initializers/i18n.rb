5f0868bd49fcc2707f3758094c8356d2655e993d
Tom Hughes <tom@compton.nu>
2011-12-18 15:51:03 +0000

    Monkey patch in an upstream fix for I18n::Backend::Fallbacks
    This can be removed when a new i18n gem release happens. It also allows
    us to remvoe an old monkey patch that worked around the problem for one
    specific translation key.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -11,16 +11,15 @@ module I18n
           store_translations(:nb, translations[:no])
           translations[:no] = translations[:nb]
 
-          friendly = translate('en', 'time.formats.friendly')
-
-          available_locales.each do |locale|
-            unless lookup(locale, 'time.formats.friendly')
-              store_translations(locale, :time => { :formats => { :friendly => friendly } })
-            end
-          end
-
           @skip_syntax_deprecation = true
         end
+      end
+    end
+
+    module Fallbacks
+      def find_first_string_or_lambda_default(defaults)
+        defaults.each_with_index { |default, ix| return ix if default && !default.is_a?(Symbol) }
+        nil
       end
     end
 
