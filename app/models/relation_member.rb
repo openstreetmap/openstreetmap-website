9930b03388a89f147d0e7e8077651008918da160
Tom Hughes <tom@compton.nu>
2012-01-24 00:26:54 +0000

    Remove remnants of old lower case relation member types

--- app/models/relation_member.rb
+++ app/models/relation_member.rb
@@ -4,23 +4,4 @@ class RelationMember < ActiveRecord::Base
 
   belongs_to :relation
   belongs_to :member, :polymorphic => true
-
-  after_find :set_class_from_type
-  after_initialize :set_class_from_type
-  before_save :set_type_from_class
-
-  def member_type=(type)
-    self[:member_type] = type
-    self[:member_class] = type.capitalize
-  end
-
-private
-
-  def set_class_from_type
-    self[:member_class] = self.member_type.classify unless self.member_type.nil?
-  end
-
-  def set_type_from_class
-    self.member_type = self[:member_class].classify
-  end
 end
