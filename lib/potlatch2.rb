ca74f1c094bacc41beed816d8139d7921d8def22
Tom Hughes <tom@compton.nu>
2012-02-26 16:24:02 +0000

    Update Potlatch 2 to 2.3-142-gecedc05 build

--- lib/potlatch2.rb
+++ lib/potlatch2.rb
@@ -5,6 +5,9 @@ module Potlatch2
     "en-GB" => "en_GB",
     "fr" => "fr_FR",
     "ja" => "ja_JP",
+    "nb" => "nb_NO",
+    "nn" => "nn_NO",
+    "no" => "nb_NO",
     "pl" => "pl_PL",
     "vi" => "vi_VN"
   }
