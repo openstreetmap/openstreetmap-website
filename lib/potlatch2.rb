e6af712eae1e2b8e515571ddc0245256938036fb
Tom Hughes <tom@compton.nu>
2012-03-04 10:48:24 +0000

    Update Potlatch 2 to 2.3-153-gf0c9bef build

--- lib/potlatch2.rb
+++ lib/potlatch2.rb
@@ -1,14 +1,28 @@
 module Potlatch2
   LOCALES = {
+    "ast" => "ast",
+    "br" => "br",
     "de" => "de_DE",
     "en" => "en_US",
     "en-GB" => "en_GB",
+    "es" => "es_ES",
+    "fi" => "fi",
     "fr" => "fr_FR",
+    "he" => "he",
+    "hsb" => "hsb",
     "ja" => "ja_JP",
+    "mk" => "mk",
+    "ms" => "ms",
     "nb" => "nb_NO",
+    "nl" => "nl_NL",
     "nn" => "nn_NO",
     "no" => "nb_NO",
     "pl" => "pl_PL",
-    "vi" => "vi_VN"
+    "ru" => "ru",
+    "sq" => "sq",
+    "tr" => "tr",
+    "vi" => "vi",
+    "vi-VN" => "vi_VN",
+    "zh" => "zh_VN"
   }
 end
