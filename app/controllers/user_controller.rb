a87540a5ddb4d4f7863472f5b2bb1f8d2f8b62c4
Tom Hughes <tom@compton.nu>
2012-03-10 12:46:10 +0000

    Fix layout selection to detect OAuth correctly

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -718,7 +718,7 @@ private
   # Choose the layout to use. See
   # https://rails.lighthouseapp.com/projects/8994/tickets/5371-layout-with-onlyexcept-options-makes-other-actions-render-without-layouts
   def choose_layout
-    oauth_url = url_for(:controller => :oauth, :action => :oauthorize, :only_path => true)
+    oauth_url = url_for(:controller => :oauth, :action => :authorize, :only_path => true)
 
     if [ 'api_details' ].include? action_name
       nil
