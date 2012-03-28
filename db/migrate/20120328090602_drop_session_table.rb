19b6221b32fbc49e0caa0c1942e874961c8b1b24
Tom Hughes <tom@compton.nu>
2012-03-28 10:07:58 +0100

    Drop the sessions table

---
+++ db/migrate/20120328090602_drop_session_table.rb
@@ -0,0 +1,17 @@
+require 'migrate'
+
+class DropSessionTable < ActiveRecord::Migration
+  def up
+    drop_table "sessions"
+  end
+
+  def down
+    create_table "sessions", :options => innodb_option do |t|
+      t.column "session_id", :string
+      t.column "data",       :text
+      t.column "created_at", :timestamp
+      t.column "updated_at", :timestamp
+    end
+    add_index "sessions", ["session_id"], :name => "sessions_session_id_idx", :unique => true
+  end
+end
