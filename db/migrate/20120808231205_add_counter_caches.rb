ea05dbf84b16da2fea439ce72a5b9dca20f405cc
Tom MacWright <tom@macwright.org>
2012-08-08 19:13:52 -0400

    Make migration with generate script and use faster code

--- db/migrate/20120808231205_add_counter_caches.rb
+++ db/migrate/20120808231205_add_counter_caches.rb
@@ -1,17 +1,12 @@
-require 'migrate'
-
 class AddCounterCaches < ActiveRecord::Migration
   def self.up
     add_column :users, :changesets_count, :integer, :null => false, :default => 0
     add_column :users, :traces_count, :integer, :null => false, :default => 0
     # reset cached counts for nominations with comments only
-    ids = Set.new
-
-    Changeset.all.each {|c| ids << c.user_id}
-    Trace.all.each {|c| ids << c.user_id}
-
-    ids.each do |user_id|
+    Changeset.group(:user_id).pluck(:user_id).each do |user_id|
       User.reset_counters(user_id, :changesets)
+    end
+    Trace.group(:user_id).pluck(:user_id).each do |user_id|
       User.reset_counters(user_id, :traces)
     end
   end
