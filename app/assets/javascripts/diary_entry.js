64ca2a826725b897fa19bca66bfe0e72f6ec8ac1
John Firebaugh <john.firebaugh@gmail.com>
2012-10-05 11:58:45 -0700

    Move diary entry JS to page-specific script file

---
+++ app/assets/javascripts/diary_entry.js
@@ -0,0 +1,33 @@
+var marker;
+
+function setLocation( e ) {
+  closeMapPopup();
+
+  var lonlat = getEventPosition(e);
+
+  $("#latitude").val(lonlat.lat);
+  $("#longitude").val(lonlat.lon);
+
+  if (marker) {
+    removeMarkerFromMap(marker);
+  }
+
+  marker = addMarkerToMap(lonlat, null, I18n.t('diary_entry.edit.marker_text'));
+}
+
+function openMap() {
+  $("#map").show();
+  $("#usemap").hide();
+
+  var params = $("#map").data();
+  var centre = new OpenLayers.LonLat(params.lon, params.lat);
+  var map = createMap("map");
+
+  setMapCenter(centre, params.zoom);
+
+  if ($("#latitude").val() && $("#longitude").val()) {
+    marker = addMarkerToMap(centre, null, I18n.t('diary_entry.edit.marker_text'));
+  }
+
+  map.events.register("click", map, setLocation);
+}
