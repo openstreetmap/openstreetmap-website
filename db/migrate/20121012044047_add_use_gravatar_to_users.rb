5eea38aa2856938b66e76244d6cd1c93ce80a53b
Paweł Paprota <ppawel@fastmail.fm>
2012-10-12 21:45:34 +0200

    Changed migration - for people who don't have images on osm.org, enable Gravatar.

--- db/migrate/20121012044047_add_use_gravatar_to_users.rb
+++ db/migrate/20121012044047_add_use_gravatar_to_users.rb
@@ -1,6 +1,12 @@
 class AddUseGravatarToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :use_gravatar, :boolean, :null => false, :default => false
+
+    User.all.each do |user|
+      # For people who don't have images on osm.org, enable Gravatar.
+      user.use_gravatar = !user.image.file?
+      user.save
+    end
   end
 
   def self.down
