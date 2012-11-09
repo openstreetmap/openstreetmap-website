ed84516db2ccf634c8639d15fc39b1896f66cd18
John Firebaugh <john.firebaugh@gmail.com>
2012-11-09 12:11:35 -0800

    Fix user popup styles

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
       var user = $(this).data('user');
       if (user.lon && user.lat) {
         L.marker([user.lat, user.lon], {icon: getUserIcon(user.icon)}).addTo(map)
-          .bindPopup(user.description);
+          .bindPopup(user.description, {offset: L.point(-1, -20)});
       }
     });
   }
