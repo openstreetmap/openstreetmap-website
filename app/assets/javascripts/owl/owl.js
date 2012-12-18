94e227acf0fda49f45435a2667c302fb51178587
Paweł Paprota <ppawel@fastmail.fm>
2012-12-18 21:39:06 +0100

    Work on the history sidebar UI.

--- app/assets/javascripts/owl/owl.js
+++ app/assets/javascripts/owl/owl.js
@@ -6,7 +6,56 @@ var owlObjectLayers = {};
 var initialized = false;
 var pageSize = 15;
 var currentOffset = 0;
-var geoJSONLayer;
+var geoJSONLayerGroup;
+
+var geoJSONStyle_active = {
+  "color": "green",
+  "fillColor": "green",
+  "weight": 5,
+  "opacity": 0.5,
+  "fillOpacity": 0.5
+};
+
+// Inactive means deleted or previous geometry.
+var geoJSONStyle_inactive = {
+  "color": "red",
+  "fillColor": "red",
+  "weight": 5,
+  "opacity": 0.5,
+  "fillOpacity": 0.5
+};
+
+var geoJSONStyle_hover = {
+  "opacity": 0.25,
+  "fillOpacity": 0.25
+};
+
+var ICON_TAGS = [
+  "aeroway", "amenity", "barrier", "building", "highway", "historic", "landuse",
+  "leisure", "man_made", "natural", "railway", "shop", "tourism", "waterway"
+];
+
+function iconTags(tags) {
+  var a = [];
+  $.each (ICON_TAGS, function (index, tag) {
+    if (tag in tags) {
+      a.push(tags[tag]);
+      a.push(tag);
+    }
+  });
+  return a;
+}
+
+function classForChange(el_type, tags) {
+  var cls;
+  if (el_type == 'W') {
+    cls = 'way ';
+  } else if (el_type == 'N') {
+    cls = 'node ';
+  }
+  cls += iconTags(tags).join(' ');
+  return cls;
+}
 
 function initOwlLayer() {
   if (initialized) {
@@ -17,40 +66,15 @@ function initOwlLayer() {
   initialized = true;
   removeObjectLayers();
   map.on('moveend', handleMapChange);
-  geoJSONStyle = {
-        "color": "yellow",
-        "fillColor": "yellow",
-        "weight": 10,
-        "opacity": 0.75,
-        "fillOpacity": 0.75
-  };
-  geoJSONLayer = new L.GeoJSON(null, {
-      /*pointToLayer: function(featureData, latlng) {
-          return new L.Circle(latlng, 2);
-      },
-      onEachFeature: function (feature, layer) {
-          layer.setStyle(geoJSONStyle);
-          layer.on('mouseover', function() {
-              layer.setStyle({
-                  "color": "blue",
-                  "opacity": 0.05,
-                  "fillOpacity": 0.05
-              });
-              highlightChangeset(getChangesetIdFromFeatureId(feature.id));
-          });
-          layer.on('mouseout', function() {
-              layer.setStyle(geoJSONStyle);
-              unhighlightChangeset(getChangesetIdFromFeatureId(feature.id));
-          });
-      }*/
-  });
-  map.addLayer(geoJSONLayer);
+
+  geoJSONLayerGroup = L.layerGroup();
+  map.addLayer(geoJSONLayerGroup);
 }
 
 function destroyOwlLayer() {
   map.off('moveend', handleMapChange);
   removeObjectLayers();
-  map.removeLayer(geoJSONLayer);
+  //map.removeLayer(geoJSONLayer);
   geoJSONLayer = null;
   owlLayer = null;
   initialized = false;
@@ -69,34 +93,32 @@ function sortChangesets(changesets) {
   return changesets;
 }
 
-function highlightChangeset(changeset_id) {
+function highlightChangesetFeatures(changeset_id) {
   if (changeset_id in owlObjectLayers) {
     $.each(owlObjectLayers[changeset_id], function(index, obj) {
       if ('setStyle' in obj) {
-        obj.setStyle({
-          color: 'black',
-          opacity: 1,
-          weight: 1,
-          fillColor: 'green',
-          fillOpacity: 0.25});
+        obj.setStyle(geoJSONStyle_hover);
       }
     });
   }
 }
 
-function unhighlightChangeset(changeset_id) {
+function unhighlightChangesetFeatures(changeset_id) {
   if (changeset_id in owlObjectLayers) {
     $.each(owlObjectLayers[changeset_id], function(index, obj) {
-      if ('setStyle' in obj) {
-        obj.setStyle({
-          color: 'black',
-          opacity: 1,
-          weight: 1,
-          fillColor: 'red',
-          fillOpacity: 0.25});
+      if ('resetStyle' in obj) {
+        obj.setStyle(obj.options.style);
       }
     });
   }
+}
+
+function highlightChangesetItem(changeset_id) {
+  $('li[data-changeset-id=' + changeset_id + ']').addClass('changeset-item-highlight');
+}
+
+function unhighlightChangesetItem(changeset_id) {
+  $('li[data-changeset-id=' + changeset_id + ']').removeClass('changeset-item-highlight');
 }
 
 function refreshHistoryData() {
@@ -120,11 +142,11 @@ function refreshHistoryData() {
       $('.changeset-item').hover(
         function (e) {
           var changeset_id = parseInt($(e.target).data('changeset-id'));
-          highlightChangeset(changeset_id);
+          highlightChangesetFeatures(changeset_id);
         },
         function (e) {
           var changeset_id = parseInt($(e.target).data('changeset-id'));
-          unhighlightChangeset(changeset_id);
+          unhighlightChangesetFeatures(changeset_id);
         }
       );
     },
@@ -147,16 +169,25 @@ function loadHistoryForCurrentViewport() {
 }
 
 function getUrlForTilerange() {
+  var tileSize;
+  if (map.getZoom() > 16) {
+    // Modified tile size: ZL17 -> 512, ZL19 -> 1024
+    tileSize = Math.pow(2, 8 - (16 - map.getZoom()));
+  } else {
+    // Regular settings.
+    tileSize = 256;
+  }
   var bounds = map.getPixelBounds(),
     nwTilePoint = new L.Point(
-      Math.floor(bounds.min.x / 256),
-      Math.floor(bounds.min.y / 256)),
+      Math.floor(bounds.min.x / tileSize),
+      Math.floor(bounds.min.y / tileSize)),
     seTilePoint = new L.Point(
-      Math.floor(bounds.max.x / 256),
-      Math.floor(bounds.max.y / 256));
-
+      Math.floor(bounds.max.x / tileSize),
+      Math.floor(bounds.max.y / tileSize));
+  //console.log(bounds);
+  //console.log(seTilePoint);
   return OSM.OWL_API_URL + 'changesets/'
-      + map.getZoom() + '/'
+      + 16 + '/'
       + nwTilePoint.x + '/' + nwTilePoint.y + '/'
       + seTilePoint.x + '/' + seTilePoint.y + '.geojson?limit=' + pageSize + '&offset=' + currentOffset;
 }
@@ -183,10 +214,41 @@ function addObjectLayers(changesets) {
   });
 }
 
-// Add bounding boxes and point markers for changesets.
+// Add GeoJSON features.
 function addGeoJSON(geojson) {
-  geoJSONLayer.clearLayers();
-  geoJSONLayer.addData(geojson);
+  geoJSONLayerGroup.clearLayers();
+  $.each(geojson['features'], function (index, changeset) {
+    owlObjectLayers[changeset.properties.id] = [];
+    $.each(changeset['features'], function (index, change) {
+      addChangeFeatureLayer(change, change.features[0]);
+      if (change.features.length > 1) {
+        addChangeFeatureLayer(change, change.features[1]);
+      }
+    });
+  });
+}
+
+// Prepares a GeoJSON layer for a given change feature and adds it to the map.
+function addChangeFeatureLayer(change, geojson) {
+  var active = geojson.properties.type == 'current';
+  var style = active ? geoJSONStyle_active : geoJSONStyle_inactive;
+  var layer = new L.GeoJSON(geojson, {style: style});
+  layer.on('mouseover', function (e) {
+      e.target.setStyle(geoJSONStyle_hover);
+      highlightChangesetItem(change.properties.changeset_id);
+  });
+  layer.on('mouseout', function (e) {
+      e.target.setStyle(style);
+      unhighlightChangesetItem(change.properties.changeset_id);
+  });
+  layer.on('click', function (e) {
+    L.popup()
+      .setLatLng(e.latlng)
+      .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+      .openOn(map);
+  });
+  owlObjectLayers[change.properties.changeset_id].push(layer);
+  geoJSONLayerGroup.addLayer(layer);
 }
 
 function changesetsFromGeoJSON(geojson) {
