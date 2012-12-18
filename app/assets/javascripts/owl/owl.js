11d12291c4be7cdad94a7b26e8a2bbda9e4c653c
Paweł Paprota <ppawel@fastmail.fm>
2012-12-18 15:54:29 +0100

    Work on the history sidebar UI.

--- app/assets/javascripts/owl/owl.js
+++ app/assets/javascripts/owl/owl.js
@@ -6,7 +6,50 @@ var owlObjectLayers = {};
 var initialized = false;
 var pageSize = 15;
 var currentOffset = 0;
-var geoJSONLayer;
+var geoJSONLayerGroup;
+
+var geoJSONStyle = {
+        "color": "green",
+        "fillColor": "red",
+        "weight": 5,
+        "opacity": 0.5,
+        "fillOpacity": 0.5
+  };
+
+var geoJSONStyle_hover = {
+        "color": "yellow",
+        "fillColor": "yellow",
+        "weight": 5,
+        "opacity": 0.25,
+        "fillOpacity": 0.25
+  };
+
+var ICON_TAGS = [
+  "aeroway", "amenity", "barrier", "building", "highway", "historic", "landuse",
+  "leisure", "man_made", "natural", "railway", "shop", "tourism", "waterway"
+];
+
+function iconTags(tags) {
+  var a = [];
+  $.each (ICON_TAGS, function (index, tag) {
+    if (tag in tags) {
+      a.push(tags[tag]);
+      a.push(tag);
+    }
+  });
+  return a;
+}
+
+function classForChange(el_type, tags) {
+  var cls;
+  if (el_type == 'W') {
+    cls = 'way ';
+  } else if (el_type == 'N') {
+    cls = 'node ';
+  }
+  cls += iconTags(tags).join(' ');
+  return cls;
+}
 
 function initOwlLayer() {
   if (initialized) {
@@ -17,40 +60,15 @@ function initOwlLayer() {
   initialized = true;
   removeObjectLayers();
   map.on('moveend', handleMapChange);
-  geoJSONStyle = {
-        "color": "yellow",
-        "fillColor": "yellow",
-        "weight": 10,
-        "opacity": 0.75,
-        "fillOpacity": 0.75
-  };
-  geoJSONLayer = new L.GeoJSON(null, {
-      /*pointToLayer: function(featureData, latlng) {
-          return new L.Circle(latlng, 2);
-      },
-      onEachFeature: function (feature, layer) {
-          layer.setStyle(geoJSONStyle);
-          layer.on('mouseover', function() {
-              layer.setStyle({
-                  "color": "blue",
-                  "opacity": 0.05,
-                  "fillOpacity": 0.05
-              });
-              highlightChangeset(getChangesetIdFromFeatureId(feature.id));
-          });
-          layer.on('mouseout', function() {
-              layer.setStyle(geoJSONStyle);
-              unhighlightChangeset(getChangesetIdFromFeatureId(feature.id));
-          });
-      }*/
-  });
-  map.addLayer(geoJSONLayer);
+
+  geoJSONLayerGroup = L.layerGroup();
+  map.addLayer(geoJSONLayerGroup);
 }
 
 function destroyOwlLayer() {
   map.off('moveend', handleMapChange);
   removeObjectLayers();
-  map.removeLayer(geoJSONLayer);
+  //map.removeLayer(geoJSONLayer);
   geoJSONLayer = null;
   owlLayer = null;
   initialized = false;
@@ -147,16 +165,25 @@ function loadHistoryForCurrentViewport() {
 }
 
 function getUrlForTilerange() {
+  var tileSize;
+  if (map.getZoom() > 16) {
+    // Modified tile size: ZL17 -> 512, ZL19 -> 1024
+    tileSize = Math.pow(2, 8 - (16 - map.getZoom()));
+  } else {
+    // Regular settings.
+    tileSize = 256;
+  }
   var bounds = map.getPixelBounds(),
     nwTilePoint = new L.Point(
-      Math.floor(bounds.min.x / 256),
-      Math.floor(bounds.min.y / 256)),
+      Math.floor(bounds.min.x / tileSize),
+      Math.floor(bounds.min.y / tileSize)),
     seTilePoint = new L.Point(
-      Math.floor(bounds.max.x / 256),
-      Math.floor(bounds.max.y / 256));
-
+      Math.floor(bounds.max.x / tileSize),
+      Math.floor(bounds.max.y / tileSize));
+  //console.log(bounds);
+  //console.log(seTilePoint);
   return OSM.OWL_API_URL + 'changesets/'
-      + map.getZoom() + '/'
+      + 16 + '/'
       + nwTilePoint.x + '/' + nwTilePoint.y + '/'
       + seTilePoint.x + '/' + seTilePoint.y + '.geojson?limit=' + pageSize + '&offset=' + currentOffset;
 }
@@ -183,10 +210,27 @@ function addObjectLayers(changesets) {
   });
 }
 
-// Add bounding boxes and point markers for changesets.
+// Add GeoJSON features.
 function addGeoJSON(geojson) {
-  geoJSONLayer.clearLayers();
-  geoJSONLayer.addData(geojson);
+  geoJSONLayerGroup.clearLayers();
+  $.each(geojson['features'], function (index, changeset) {
+    $.each(changeset['features'], function (index, change) {
+      l = new L.GeoJSON(change, {style: geoJSONStyle});
+      l.on('mouseover', function (e) {
+          e.target.setStyle(geoJSONStyle_hover);
+      });
+      l.on('mouseout', function (e) {
+          e.target.setStyle(geoJSONStyle);
+      });
+      l.on('click', function (e) {
+        L.popup()
+          .setLatLng(e.latlng)
+          .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+          .openOn(map);
+      });
+      geoJSONLayerGroup.addLayer(l);
+    });
+  });
 }
 
 function changesetsFromGeoJSON(geojson) {
