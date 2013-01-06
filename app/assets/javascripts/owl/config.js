3c06cc2b956374d438dc9313acd58c9e244790d8
Paweł Paprota <ppawel@fastmail.fm>
2013-01-06 19:57:00 +0100

    Adjusted colors.

--- app/assets/javascripts/owl/config.js
+++ app/assets/javascripts/owl/config.js
@@ -5,54 +5,54 @@ var OWL = {
       fill: true,
       stroke: false,
       fillColor: "indigo",
-      fillOpacity: 0.85,
+      fillOpacity: 0.50,
       radius: 8
     },
     'node_modify': {
       fill: true,
       stroke: false,
       fillColor: "blue",
-      fillOpacity: 0.85,
+      fillOpacity: 0.50,
       radius: 8
     },
     'node_delete': {
       fill: true,
       stroke: false,
       fillColor: "red",
-      fillOpacity: 0.85,
+      fillOpacity: 0.50,
       radius: 8
     },
     'way_create': {
       color: "indigo",
       fillColor: "indigo",
       weight: 5,
-      opacity: 0.85,
-      fillOpacity: 0.85
+      opacity: 0.50,
+      fillOpacity: 0.50
     },
     'way_modify': {
       color: "blue",
       fillColor: "blue",
       weight: 5,
-      opacity: 0.85,
-      fillOpacity: 0.85
+      opacity: 0.50,
+      fillOpacity: 0.50
     },
     'way_delete': {
       color: "red",
       fillColor: "red",
       weight: 5,
-      opacity: 0.85,
-      fillOpacity: 0.85
+      opacity: 0.50,
+      fillOpacity: 0.50
     },
     'way_affect': {
       color: "blue",
       fillColor: "lightblue",
       weight: 5,
-      opacity: 0.85,
-      fillOpacity: 0.85
+      opacity: 0.50,
+      fillOpacity: 0.50
     },
     'hover': {
-      opacity: 0.25,
-      fillOpacity: 0.25
+      opacity: 0.75,
+      fillOpacity: 0.75
     }
   },
 
