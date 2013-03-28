00a4d9fe49f2118cd0fa798edfcc41fab3388025
Tom Hughes <tom@compton.nu>
2013-03-28 11:11:51 +0000

    Tidy up javascript OAuth code

--- app/assets/javascripts/oauth.js
+++ app/assets/javascripts/oauth.js
@@ -1,32 +1,25 @@
 //= require sha
 //= require ohauth
 
-$(document).ready(function () {
-  $.ajaxPrefilter(function(options, jqxhr) {
-    if (options.oauth) {
-      var ohauth = window.ohauth;
-      var url = options.url.replace(/\?$/, "");
-      var params = {
-        oauth_consumer_key: OSM.oauth_consumer_key,
-        oauth_token: OSM.oauth_token,
-        oauth_signature_method: "HMAC-SHA1",
-        oauth_timestamp: ohauth.timestamp(),
-        oauth_nonce: ohauth.nonce()
-      };
+$.ajaxPrefilter(function(options, jqxhr) {
+  if (options.oauth) {
+    var ohauth = window.ohauth;
+    var url = options.url.replace(/\?$/, "");
+    var params = {
+      oauth_consumer_key: OSM.oauth_consumer_key,
+      oauth_token: OSM.oauth_token,
+      oauth_signature_method: "HMAC-SHA1",
+      oauth_timestamp: ohauth.timestamp(),
+      oauth_nonce: ohauth.nonce()
+    };
 
-      for (var name in jqxhr.data) {
-        params[name] = jqxhr.data[name];
-      }
+    params.oauth_signature = ohauth.signature(
+      OSM.oauth_consumer_secret,
+      OSM.oauth_token_secret,
+      ohauth.baseString(options.type, url, $.extend({}, params, jqxhr.data))
+    );
 
-      params.oauth_signature = ohauth.signature(
-        OSM.oauth_consumer_secret,
-        OSM.oauth_token_secret,
-        ohauth.baseString(options.type, url, params)
-      );
-
-      options.headers = {
-        Authorization: "OAuth " + ohauth.authHeader(params)
-      };
-    }
-  });
+    options.headers = options.headers || {};
+    options.headers.Authorization = "OAuth " + ohauth.authHeader(params);
+  }
 });
