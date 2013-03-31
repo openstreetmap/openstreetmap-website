9807bb7876c1069ad31bd6a5f52d6e3b0472b2b9
Eric Theise <erictheise@gmail.com>
2013-03-31 14:49:30 -0700

    maybe I should stub locale, but I'm not sure how to do that

--- test/unit/helpers/browse_helper_test.rb
+++ test/unit/helpers/browse_helper_test.rb
@@ -7,6 +7,7 @@ class BrowseHelperTest < ActionView::TestCase
     @key = 'wikipedia'
     @language = 'zh-classical'
     @title = 'Test_method'
+    @locale = "#{I18n.locale}"
   end
   
   def test_wikipedia_link_helper_when_key_is_nil
@@ -16,19 +17,19 @@ class BrowseHelperTest < ActionView::TestCase
 
   def test_wikipedia_link_helper_when_key_is_wikipedia_value_is_simple_string
     key, value = @key, @title
-    expected_hash = { url: "http://en.wikipedia.org/wiki/#{@title}?uselang=en", title: @title }
+    expected_hash = { url: "http://en.wikipedia.org/wiki/#{@title}?uselang=#{@locale}", title: @title }
     assert_equal expected_hash, wikipedia_link(key, value)
   end
 
   def test_wikipedia_link_helper_when_key_is_wikipedia_including_language_string
     key, value = "#{@key}:#{@language}", "#{@title}"
-    expected_hash = { url: "http://#{@language}.wikipedia.org/wiki/#{@title}?uselang=en", title: "#{@title}" }
+    expected_hash = { url: "http://#{@language}.wikipedia.org/wiki/#{@title}?uselang=#{@locale}", title: "#{@title}" }
     assert_equal expected_hash, wikipedia_link(key, value)
   end
 
   def test_wikipedia_link_helper_when_key_is_wikipedia_value_includes_language_string
     key, value = @key, "#{@language}:#{@title}"
-    expected_hash = { url: "http://#{@language}.wikipedia.org/wiki/#{@language}:#{@title}?uselang=en", title: "#{@language}:#{@title}" }
+    expected_hash = { url: "http://#{@language}.wikipedia.org/wiki/#{@language}:#{@title}?uselang=#{@locale}", title: "#{@language}:#{@title}" }
     assert_equal expected_hash, wikipedia_link(key, value)
   end
 
