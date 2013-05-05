2b3cec0cf6ac2ccd5c24fa193ffb9ea8b57252cf
Leo Koppelkamm <diebuche@gmail.com>
2013-05-05 20:21:22 +0200

    Also close sidebar when clicking on "View".

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -7,15 +7,19 @@ function openSidebar(options) {
 
   $("#sidebar").width(options.width || "30%");
   $("#sidebar").css("display", "block").trigger("opened");
+  $('#viewanchor').on('click', handleCloseClick);
 }
 
 function closeSidebar() {
   $("#sidebar").css("display", "none").trigger("closed");
+  $('#viewanchor').off('click', handleCloseClick);
+}
+
+function handleCloseClick(e) {
+  closeSidebar();
+  e.preventDefault();
 }
 
 $(document).ready(function () {
-  $(".sidebar_close").click(function (e) {
-    closeSidebar();
-    e.preventDefault();
-  });
+  $(".sidebar_close, #viewanchor").click(handleCloseClick);
 });
