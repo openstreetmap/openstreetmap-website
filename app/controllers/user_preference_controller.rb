c809f79912a3b998ddf3a21973582254db564183
Tom Hughes <tom@compton.nu>
2013-05-10 16:03:37 +0100

    Tidy up user preferences controller

--- app/controllers/user_preference_controller.rb
+++ app/controllers/user_preference_controller.rb
@@ -6,39 +6,8 @@ class UserPreferenceController < ApplicationController
   before_filter :require_allow_write_prefs, :except => [:read_one, :read]
   around_filter :api_call_handle_error
 
-  def read_one
-    pref = UserPreference.find(@user.id, params[:preference_key])
-
-    render :text => pref.v.to_s, :content_type => "text/plain"
-  rescue ActiveRecord::RecordNotFound => ex
-    render :text => 'OH NOES! PREF NOT FOUND!', :status => :not_found
-  end
-
-  def update_one
-    begin
-      pref = UserPreference.find(@user.id, params[:preference_key])
-      pref.v = request.raw_post.chomp
-      pref.save
-    rescue ActiveRecord::RecordNotFound 
-      pref = UserPreference.new
-      pref.user = @user
-      pref.k = params[:preference_key]
-      pref.v = request.raw_post.chomp
-      pref.save
-    end
-
-    render :nothing => true, :content_type => "text/plain"
-  end
-
-  def delete_one
-    UserPreference.find(@user.id, params[:preference_key]).delete
-
-    render :nothing => true, :content_type => "text/plain"
-  rescue ActiveRecord::RecordNotFound => ex
-    render :text => "param: #{params[:preference_key]} not found", :status => :not_found
-  end
-
-  # print out all the preferences as a big xml block
+  ##
+  # return all the preferences as an XML document
   def read
     doc = OSM::API.new.get_xml_doc
 
@@ -54,42 +23,69 @@ class UserPreferenceController < ApplicationController
     render :text => doc.to_s, :content_type => "text/xml"
   end
 
+  ##
+  # return the value for a single preference
+  def read_one
+    pref = UserPreference.find(@user.id, params[:preference_key])
+
+    render :text => pref.v.to_s, :content_type => "text/plain"
+  end
+
   # update the entire set of preferences
   def update
+    old_preferences = @user.preferences.reduce({}) do |preferences,preference|
+      preferences[preference.k] = preference
+      preferences
+    end
+
+    new_preferences = {}
+
     doc = XML::Parser.string(request.raw_post).parse
 
-    prefs = []
-
-    keyhash = {}
-
     doc.find('//preferences/preference').each do |pt|
-      pref = UserPreference.new
-
-      unless keyhash[pt['k']].nil? # already have that key
-        render :text => 'OH NOES! CAN HAS UNIQUE KEYS?', :status => :not_acceptable, :content_type => "text/plain"
-        return
+      if preference = old_preferences.delete(pt["k"])
+        preference.v = pt["v"]
+      elsif new_preferences.include?(pt["k"])
+        raise OSM::APIDuplicatePreferenceError.new(pt["k"])
+      else
+        preference = @user.preferences.build(:k => pt["k"], :v => pt["v"])
       end
 
-      keyhash[pt['k']] = 1
-
-      pref.k = pt['k']
-      pref.v = pt['v']
-      pref.user_id = @user.id
-      prefs << pref
+      new_preferences[preference.k] = preference
     end
 
-    if prefs.size > 150
-      render :text => 'Too many preferences', :status => :request_entity_too_large, :content_type => "text/plain"
-      return
+    old_preferences.each_value do |preference|
+      preference.delete
     end
 
-    # kill the existing ones
-    UserPreference.delete_all(['user_id = ?', @user.id])
-
-    # save the new ones
-    prefs.each do |pref|
-      pref.save!
+    new_preferences.each_value do |preference|
+      preference.save!
     end
+
+    render :nothing => true, :content_type => "text/plain"
+  end
+
+  ##
+  # update the value of a single preference
+  def update_one
+    begin
+      pref = UserPreference.find(@user.id, params[:preference_key])
+    rescue ActiveRecord::RecordNotFound 
+      pref = UserPreference.new
+      pref.user = @user
+      pref.k = params[:preference_key]
+    end
+
+    pref.v = request.raw_post.chomp
+    pref.save!
+
+    render :nothing => true, :content_type => "text/plain"
+  end
+
+  ##
+  # delete a single preference
+  def delete_one
+    UserPreference.find(@user.id, params[:preference_key]).delete
 
     render :nothing => true, :content_type => "text/plain"
   end
