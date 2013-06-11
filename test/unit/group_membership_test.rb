d1f9f6aac6029613e4827ab3b012fe217a0a3dae
Drew Dara-Abrams <dda@dara-abrams.com>
2013-06-11 08:03:48 -0700

    Groups: adding some test coverage

--- test/unit/group_membership_test.rb
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-require File.dirname(__FILE__) + '/../test_helper'
-
-class GroupMembershipTest < ActiveSupport::TestCase
-  api_fixtures
-  # TODO: write some tests!
-end
--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -3,5 +3,31 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class GroupTest < ActiveSupport::TestCase
   api_fixtures
-  # TODO: write some tests!
+  fixtures :groups, :users
+
+  def test_group_count
+    assert_equal 2, Group.count
+  end
+
+  def test_group_membership
+    uk_group = Group.find(1)
+    us_group = Group.find(2)
+    first_user = User.find(1)
+    second_user = User.find(2)
+
+    uk_group.users << first_user
+    uk_group.users << second_user
+    us_group.users << second_user
+
+    assert arrays_are_equal?(uk_group.users.map(&:id), [first_user.id, second_user.id])
+    assert arrays_are_equal?(us_group.users.map(&:id), [second_user.id])
+    assert arrays_are_equal?(first_user.groups.map(&:id), [uk_group.id])
+    assert arrays_are_equal?(second_user.groups.map(&:id), [uk_group.id, us_group.id])
+  end
+
+private
+
+  def arrays_are_equal?(a, b)
+    a.sort.uniq == b.sort.uniq
+  end
 end
