9b4c48bdb2ddce41ff31a3615499beb6b1091568
Drew Dara-Abrams <dda@dara-abrams.com>
2013-06-18 08:07:09 -0700

    Groups: each group's map now displays the bounding box of group members

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -3,14 +3,21 @@ $(document).ready(function () {
     panZoomControl: true
   });
 
-  /* TODO: display bounding box of group members */
-  map.setView([OSM.home.lat, OSM.home.lon], 12);
+  var userMarkers = []
 
   $("[data-user]").each(function () {
     var user = $(this).data('user');
     if (user.lon && user.lat) {
-      L.marker([user.lat, user.lon], {icon: getUserIcon(user.icon)}).addTo(map)
-        .bindPopup(user.description);
+      userMarkers.push(
+        L.marker([user.lat, user.lon], {icon: getUserIcon(user.icon)})
+          .addTo(map)
+          .bindPopup(user.description)
+      );
     }
   });
+
+  if (userMarkers.length > 0) {
+    var userLayer = L.featureGroup(userMarkers);
+    map.fitBounds(userLayer.getBounds());
+  }
 });
