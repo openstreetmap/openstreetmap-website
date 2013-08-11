9d68b65cd30741e5c0aa492c95d0739ca2deddae
Tom Hughes <tom@compton.nu>
2013-08-11 15:58:42 +0100

    Fix "where am I" on the edit page

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -7,12 +7,25 @@ function minimiseMap() {
 }
 
 $(document).ready(function () {
-  $("#search_form").submit(function () {
+  $("#search_form").submit(function (e) {
+    e.preventDefault();
+
     $("#sidebar_title").html(I18n.t('site.sidebar.search_results'));
     $("#sidebar_content").load($(this).attr("action"), {
       query: $("#query").val()
     }, openSidebar);
+  });
 
-    return false;
+  $("#describe_location").click(function (e) {
+    e.preventDefault();
+
+    var mapParams = OSM.mapParams();
+
+    $("#sidebar_title").html(I18n.t('site.sidebar.search_results'));
+    $("#sidebar_content").load($(this).attr("href"), {
+      lat: mapParams.lat,
+      lon: mapParams.lng,
+      zoom: mapParams.zoom
+    }, openSidebar);    
   });
 });
