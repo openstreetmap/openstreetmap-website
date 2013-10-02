ad7bc7415bd3d2dfde8eec3800c3936e6a3db31e
John Firebaugh <john.firebaugh@gmail.com>
2013-10-02 11:17:55 -0700

    Remove unused

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -5,25 +5,3 @@ function maximiseMap() {
 function minimiseMap() {
   $("#content").removeClass("maximised");
 }
-
-$(document).ready(function () {
-  $("#search_form").submit(function (e) {
-    e.preventDefault();
-
-    $("#sidebar_content").load($(this).attr("action"), {
-      query: $("#query").val()
-    });
-  });
-
-  $("#describe_location").click(function (e) {
-    e.preventDefault();
-
-    var mapParams = OSM.mapParams();
-
-    $("#sidebar_content").load($(this).attr("href"), {
-      lat: mapParams.lat,
-      lon: mapParams.lon,
-      zoom: mapParams.zoom
-    });
-  });
-});
