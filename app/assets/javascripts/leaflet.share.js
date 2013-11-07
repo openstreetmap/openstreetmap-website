3689798a04194d5098ce3dc84aab7967b27b7239
Eden Halperin <eden.halperin@gmail.com>
2013-11-07 16:57:53 -0500

    Return map-ui sidebars in mobile

--- app/assets/javascripts/leaflet.share.js
+++ app/assets/javascripts/leaflet.share.js
@@ -11,7 +11,7 @@ L.OSM.share = function (options) {
       .attr('class', 'control-share');
 
     var button = $('<a>')
-      .attr('class', 'control-button mobile-hide')
+      .attr('class', 'control-button')
       .attr('href', '#')
       .attr('title', I18n.t('javascripts.share.title'))
       .html('<span class="icon share"></span>')
