b56fb250680e95146256161e8adb5f1a327859fd
Aaron Lidman <aaronlidman@gmail.com>
2013-11-09 23:39:02 -0800

    Restructured notes page

--- app/helpers/note_helper.rb
+++ app/helpers/note_helper.rb
@@ -1,9 +1,16 @@
 module NoteHelper
-  def note_event(at, by)
+  def note_event(event, at, by)
     if by.nil?
-      I18n.t("browse.note.at_html", :when => friendly_date(at)).html_safe
+      I18n.t("browse.note." + event + "_by_anonymous",
+        :when => friendly_date(at),
+        :exact_time => l(at)
+      ).html_safe
     else
-      I18n.t("browse.note.at_by_html", :when => friendly_date(at), :user => note_author(by)).html_safe
+      I18n.t("browse.note." + event + "_by",
+        :when => friendly_date(at),
+        :exact_time => l(at),
+        :user => note_author(by)
+      ).html_safe
     end
   end
 
@@ -14,4 +21,5 @@ module NoteHelper
       link_to h(author.display_name), link_options.merge({:controller => "user", :action => "view", :display_name => author.display_name})
     end
   end
+
 end
