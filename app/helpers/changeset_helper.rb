4cb9b6254f2265a38fbda609d8a6b775129bfea7
John Firebaugh <john.firebaugh@gmail.com>
2013-11-12 14:46:47 -0800

    Fix lego translations

--- app/helpers/changeset_helper.rb
+++ app/helpers/changeset_helper.rb
@@ -1,33 +1,32 @@
 module ChangesetHelper
-  def changeset_details(changeset)
-    out = ''
-    created_at = distance_of_time_in_words_to_now(changeset.created_at)
-    closed_at = distance_of_time_in_words_to_now(changeset.closed_at)
-    date = ''
-    if changeset.closed_at > DateTime.now
-      date << t('browse.created') + ' '
-      date << content_tag(:abbr, t('browse.ago', :ago => created_at), title: l(changeset.created_at))
+  def changeset_user_link(changeset)
+    if changeset.user.data_public?
+      link_to(changeset.user.display_name, user_path(changeset.user.display_name))
     else
-      date << t('browse.closed') + ' '
-      both_times = t('browse.created') + ': ' + l(changeset.created_at)
-      both_times << '&#10;'
-      both_times << t('browse.closed') + ': ' + l(changeset.closed_at)
-      date << content_tag(:abbr, t('browse.ago', :ago => created_at), title: both_times.html_safe)
+      t('browse.anonymous')
     end
-    out << content_tag(:span, date.html_safe, class: 'date')
-    unless params.key?(:display_name)
-      userspan = ''
-      if changeset.user.data_public?
-        userspan << ' ' + t('browse.by') + ' '
-        if changeset.user.data_public?
-          user = link_to changeset.user.display_name, user_path(changeset.user.display_name)
-        else
-          user = t('changeset.changeset.anonymous')
-        end
-        userspan << content_tag(:span, user, class: 'user')
-      end
-      out << content_tag(:span, userspan.html_safe, class: 'user')
+  end
+
+  def changeset_details(changeset)
+    if changeset.closed_at > DateTime.now
+      action = :created
+      time = distance_of_time_in_words_to_now(changeset.created_at)
+      title = l(changeset.created_at)
+    else
+      action = :closed
+      time = distance_of_time_in_words_to_now(changeset.closed_at)
+      title = "#{t('browse.created')}: #{l(changeset.created_at)}&#10;#{t('browse.closed')}: #{l(changeset.closed_at)}".html_safe
     end
-    return out.html_safe
+
+    if params.key?(:display_name)
+      t "browse.#{action}_html",
+        :time => time,
+        :title => title
+    else
+      t "browse.#{action}_by_html",
+        :time => time,
+        :title => title,
+        :user => changeset_user_link(changeset)
+    end
   end
 end
