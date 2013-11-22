954261fb1c5c328bba388a70e4ea013ad9667c23
John Firebaugh <john.firebaugh@gmail.com>
2013-11-22 12:13:12 -0800

    Restore prev/next changeset by user links

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -60,6 +60,10 @@ class BrowseController < ApplicationController
     @node_pages, @nodes = paginate(:old_nodes, :conditions => {:changeset_id => @changeset.id}, :per_page => 20, :parameter => 'node_page')
     @way_pages, @ways = paginate(:old_ways, :conditions => {:changeset_id => @changeset.id}, :per_page => 20, :parameter => 'way_page')
     @relation_pages, @relations = paginate(:old_relations, :conditions => {:changeset_id => @changeset.id}, :per_page => 20, :parameter => 'relation_page')
+    if @changeset.user.data_public?
+      @next_by_user = @changeset.user.changesets.where("id > ?", @changeset.id).reorder(:id => :asc).first
+      @prev_by_user = @changeset.user.changesets.where("id < ?", @changeset.id).reorder(:id => :desc).first
+    end
   rescue ActiveRecord::RecordNotFound
     render :action => "not_found", :status => :not_found
   end
