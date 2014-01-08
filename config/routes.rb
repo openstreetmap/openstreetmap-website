d8a90a748ff9d57290905a20934af9fabff3bdd2
simonpoole <simonpoole@users.noreply.github.com>
2014-01-09 00:32:40 +0100

    Fix copy paste error

--- config/routes.rb
+++ config/routes.rb
@@ -139,7 +139,7 @@ OpenStreetMap::Application.routes.draw do
   match '/copyright/:copyright_locale' => 'site#copyright', :via => :get
   match '/copyright' => 'site#copyright', :via => :get
   match '/welcome' => 'site#welcome', :via => :get, :as => :welcome
-  match '/welcome' => 'site#welcome', :via => :get, :as => :welcome
+  match '/landing' => 'site#landing', :via => :get, :as => :landing
   match '/help' => 'site#help', :via => :get, :as => :help
   match '/about' => 'site#about', :via => :get, :as => :about
   match '/history' => 'changeset#list', :via => :get
