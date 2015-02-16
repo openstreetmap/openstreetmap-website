816404ffe63a6b83340c130c53aa1772873043c6
Tom Hughes <tom@compton.nu>
2015-02-16 22:53:52 +0000

    Avoid deprecation warning in migration

--- db/migrate/20150111192335_subscribe_old_changesets.rb
+++ db/migrate/20150111192335_subscribe_old_changesets.rb
@@ -1,7 +1,7 @@
 class SubscribeOldChangesets < ActiveRecord::Migration
   def up
     Changeset.find_each do |changeset|
-      changeset.subscribers << changeset.user unless changeset.subscribers.exists?(changeset.user)
+      changeset.subscribers << changeset.user unless changeset.subscribers.exists?(changeset.user.id)
     end
   end
 
