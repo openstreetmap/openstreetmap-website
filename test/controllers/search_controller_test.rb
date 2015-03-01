38a74cf6a38830860a75c1993bfeca28ded8c1fe
Tom Hughes <tom@compton.nu>
2015-03-01 14:07:25 +0000

    Fix some tests

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -24,65 +24,65 @@ class SearchControllerTest < ActionController::TestCase
 
   ##
   # test searching nodes
-  def search_nodes
+  def test_search_nodes
     get :search_nodes, :type => "test"
-    assert_response :status => :service_unavailable
+    assert_response :service_unavailable
     assert_equal "Searching of nodes is currently unavailable", response.headers["Error"]
 
     get :search_nodes, :type => "test", :value => "yes"
-    assert_response :status => :service_unavailable
+    assert_response :service_unavailable
     assert_equal "Searching of nodes is currently unavailable", response.headers["Error"]
 
     get :search_nodes, :name => "Test Node"
-    assert_response :status => :service_unavailable
+    assert_response :service_unavailable
     assert_equal "Searching of nodes is currently unavailable", response.headers["Error"]
   end
 
   ##
   # test searching ways
-  def search_ways
+  def test_search_ways
     get :search_ways, :type => "test"
-    assert_response :status => :service_unavailable
+    assert_response :service_unavailable
     assert_equal "Searching for a key without value is currently unavailable", response.headers["Error"]
 
     get :search_ways, :type => "test", :value => "yes"
-    assert_response :status => :success
+    assert_response :success
     assert_select "way", 3
 
     get :search_ways, :name => "Test Way"
-    assert_response :status => :success
+    assert_response :success
     assert_select "way", 1
   end
 
   ##
   # test searching relations
-  def search_relations
+  def test_search_relations
     get :search_relations, :type => "test"
-    assert_response :status => :service_unavailable
+    assert_response :service_unavailable
     assert_equal "Searching for a key without value is currently unavailable", response.headers["Error"]
 
     get :search_relations, :type => "test", :value => "yes"
-    assert_response :status => :success
+    assert_response :success
     assert_select "relation", 3
 
     get :search_relations, :name => "Test Relation"
-    assert_response :status => :success
+    assert_response :success
     assert_select "relation", 1
   end
 
   ##
   # test searching nodes, ways and relations
-  def search_all
+  def test_search_all
     get :search_all, :type => "test"
-    assert_response :status => :service_unavailable
+    assert_response :service_unavailable
     assert_equal "Searching of nodes is currently unavailable", response.headers["Error"]
 
     get :search_all, :type => "test", :value => "yes"
-    assert_response :status => :service_unavailable
+    assert_response :service_unavailable
     assert_equal "Searching of nodes is currently unavailable", response.headers["Error"]
 
     get :search_all, :name => "Test"
-    assert_response :status => :service_unavailable
+    assert_response :service_unavailable
     assert_equal "Searching of nodes is currently unavailable", response.headers["Error"]
   end
 end
--- test/controllers/swf_controller_test.rb
+++ test/controllers/swf_controller_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class SwfControllerTest < ActionController::TestCase
+  api_fixtures
+
   ##
   # test all routes which lead to this controller
   def test_routes
@@ -13,9 +15,22 @@ class SwfControllerTest < ActionController::TestCase
   ##
   # basic test that trackpoints at least returns some sort of flash movie
   def test_trackpoints
-    get :trackpoints, :xmin => 51, :xmax => 52, :ymin => -1, :ymax => 1, :baselong => 0, :basey => 0, :masterscale => 1
+    get :trackpoints, :xmin => -1, :xmax => 1, :ymin => 51, :ymax => 52, :baselong => 0, :basey => 0, :masterscale => 1
     assert_response :success
     assert_equal "application/x-shockwave-flash", response.content_type
     assert_match /^FWS/, response.body
+    assert_equal 80, response.body.length
+
+    get :trackpoints, :xmin => -1, :xmax => 1, :ymin => 51, :ymax => 52, :baselong => 0, :basey => 0, :masterscale => 1, :token => users(:normal_user).tokens.create.token
+    assert_response :success
+    assert_equal "application/x-shockwave-flash", response.content_type
+    assert_match /^FWS/, response.body
+    assert_equal 67, response.body.length
+
+    get :trackpoints, :xmin => -1, :xmax => 1, :ymin => 51, :ymax => 52, :baselong => 0, :basey => 0, :masterscale => 1, :token => users(:public_user).tokens.create.token
+    assert_response :success
+    assert_equal "application/x-shockwave-flash", response.content_type
+    assert_match /^FWS/, response.body
+    assert_equal 74, response.body.length
   end
 end
