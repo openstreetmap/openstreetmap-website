6ce635d5d32d5e583c4a271af2309c5af6ac9424
Tom Hughes <tom@compton.nu>
2015-03-07 11:12:17 +0000

    Reduce duplication in the oauth tests

--- test/integration/oauth_test.rb
+++ test/integration/oauth_test.rb
@@ -9,127 +9,56 @@ class OAuthTest < ActionDispatch::IntegrationTest
   def test_oauth10_web_app
     client = client_applications(:oauth_web_app)
 
-    post_via_redirect "/login",
-                      :username => client.user.email, :password => "test"
+    post_via_redirect "/login", :username => client.user.email, :password => "test"
     assert_response :success
 
-    signed_get "/oauth/request_token", :consumer => client
-    assert_response :success
-    token = parse_token(response)
-    assert_instance_of RequestToken, token
-    assert_not_nil token.created_at
-    assert_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, client.permissions
-
-    post "/oauth/authorize",
-         :oauth_token => token.token,
-         :allow_read_prefs => true, :allow_write_prefs => true
-    assert_response :redirect
-    assert_redirected_to "http://some.web.app.org/callback?oauth_token=#{token.token}"
-    token.reload
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, [:allow_read_prefs]
-
-    signed_get "/oauth/access_token", :consumer => client, :token => token
-    assert_response :success
-    token.reload
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_not_nil token.invalidated_at
-    token = parse_token(response)
-    assert_instance_of AccessToken, token
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, [:allow_read_prefs]
-
-    signed_get "/api/0.6/user/preferences", :consumer => client, :token => token
-    assert_response :success
-
-    signed_get "/api/0.6/gpx/2", :consumer => client, :token => token
-    assert_response :forbidden
-
-    post "/oauth/revoke", :token => token.token
-    assert_redirected_to oauth_clients_url(token.user.display_name)
-    token = OauthToken.find_by_token(token.token)
-    assert_not_nil token.invalidated_at
-
-    signed_get "/api/0.6/user/preferences", :consumer => client, :token => token
-    assert_response :unauthorized
-
-    signed_get "/oauth/request_token", :consumer => client
-    assert_response :success
-    token = parse_token(response)
-    assert_instance_of RequestToken, token
-    assert_not_nil token.created_at
-    assert_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, client.permissions
-
-    post "/oauth/authorize",
-         :oauth_token => token.token,
-         :oauth_callback => "http://another.web.app.org/callback",
-         :allow_write_api => true, :allow_read_gpx => true
-    assert_response :redirect
-    assert_redirected_to "http://another.web.app.org/callback?oauth_token=#{token.token}"
-    token.reload
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, [:allow_write_api, :allow_read_gpx]
-
-    signed_get "/oauth/access_token", :consumer => client, :token => token
-    assert_response :success
-    token.reload
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_not_nil token.invalidated_at
-    token = parse_token(response)
-    assert_instance_of AccessToken, token
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, [:allow_write_api, :allow_read_gpx]
-
-    signed_get "/api/0.6/gpx/2", :consumer => client, :token => token
-    assert_response :success
-
-    signed_get "/api/0.6/user/details", :consumer => client, :token => token
-    assert_response :forbidden
-
-    post "/oauth/revoke", :token => token.token
-    assert_redirected_to oauth_clients_url(token.user.display_name)
-    token = OauthToken.find_by_token(token.token)
-    assert_not_nil token.invalidated_at
-
-    signed_get "/api/0.6/gpx/2", :consumer => client, :token => token
-    assert_response :unauthorized
+    oauth10_without_callback(client)
+    oauth10_with_callback(client, "http://another.web.app.org/callback")
   end
 
   def test_oauth10_desktop_app
     client = client_applications(:oauth_desktop_app)
 
-    post_via_redirect "/login",
-                      :username => client.user.email, :password => "test"
+    post_via_redirect "/login", :username => client.user.email, :password => "test"
     assert_response :success
 
-    signed_get "/oauth/request_token", :consumer => client
+    oauth10_without_callback(client)
+  end
+
+  def test_oauth10a_web_app
+    client = client_applications(:oauth_web_app)
+
+    post_via_redirect "/login", :username => client.user.email, :password => "test"
     assert_response :success
-    token = parse_token(response)
-    assert_instance_of RequestToken, token
-    assert_not_nil token.created_at
-    assert_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, client.permissions
+
+    oauth10a_without_callback(client)
+    oauth10a_with_callback(client, "http://another.web.app.org/callback")
+  end
+
+  def test_oauth10a_desktop_app
+    client = client_applications(:oauth_desktop_app)
+
+    post_via_redirect "/login", :username => client.user.email, :password => "test"
+    assert_response :success
+
+    oauth10a_without_callback(client)
+  end
+
+  private
+
+  def oauth10_without_callback(client)
+    token = get_request_token(client)
 
     post "/oauth/authorize",
          :oauth_token => token.token,
          :allow_read_prefs => true, :allow_write_prefs => true
-    assert_response :success
-    assert_template "authorize_success"
+    if client.callback_url
+      assert_response :redirect
+      assert_redirected_to "#{client.callback_url}?oauth_token=#{token.token}"
+    else
+      assert_response :success
+      assert_template "authorize_success"
+    end
     token.reload
     assert_not_nil token.created_at
     assert_not_nil token.authorized_at
@@ -164,29 +93,65 @@ class OAuthTest < ActionDispatch::IntegrationTest
     assert_response :unauthorized
   end
 
-  def test_oauth10a_web_app
-    client = client_applications(:oauth_web_app)
+  def oauth10_with_callback(client, callback_url)
+    token = get_request_token(client)
 
-    post_via_redirect "/login",
-                      :username => client.user.email, :password => "test"
-    assert_response :success
-
-    signed_get "/oauth/request_token",
-               :consumer => client, :oauth_callback => "oob"
-    assert_response :success
-    token = parse_token(response)
-    assert_instance_of RequestToken, token
+    post "/oauth/authorize",
+         :oauth_token => token.token, :oauth_callback => callback_url,
+         :allow_write_api => true, :allow_read_gpx => true
+    assert_response :redirect
+    assert_redirected_to "#{callback_url}?oauth_token=#{token.token}"
+    token.reload
     assert_not_nil token.created_at
-    assert_nil token.authorized_at
+    assert_not_nil token.authorized_at
     assert_nil token.invalidated_at
-    assert_allowed token, client.permissions
+    assert_allowed token, [:allow_write_api, :allow_read_gpx]
+
+    signed_get "/oauth/access_token", :consumer => client, :token => token
+    assert_response :success
+    token.reload
+    assert_not_nil token.created_at
+    assert_not_nil token.authorized_at
+    assert_not_nil token.invalidated_at
+    token = parse_token(response)
+    assert_instance_of AccessToken, token
+    assert_not_nil token.created_at
+    assert_not_nil token.authorized_at
+    assert_nil token.invalidated_at
+    assert_allowed token, [:allow_write_api, :allow_read_gpx]
+
+    signed_get "/api/0.6/gpx/2", :consumer => client, :token => token
+    assert_response :success
+
+    signed_get "/api/0.6/user/details", :consumer => client, :token => token
+    assert_response :forbidden
+
+    post "/oauth/revoke", :token => token.token
+    assert_redirected_to oauth_clients_url(token.user.display_name)
+    token = OauthToken.find_by_token(token.token)
+    assert_not_nil token.invalidated_at
+
+    signed_get "/api/0.6/gpx/2", :consumer => client, :token => token
+    assert_response :unauthorized
+  end
+
+  def oauth10a_without_callback(client)
+    token = get_request_token(client, :oauth_callback => "oob")
 
     post "/oauth/authorize",
          :oauth_token => token.token,
          :allow_read_prefs => true, :allow_write_prefs => true
-    assert_response :redirect
-    verifier = parse_verifier(response)
-    assert_redirected_to "http://some.web.app.org/callback?oauth_token=#{token.token}&oauth_verifier=#{verifier}"
+    if client.callback_url
+      assert_response :redirect
+      verifier = parse_verifier(response)
+      assert_redirected_to "http://some.web.app.org/callback?oauth_token=#{token.token}&oauth_verifier=#{verifier}"
+    else
+      assert_response :success
+      assert_template "authorize_success"
+      m = response.body.match("<p>The verification code is ([A-Za-z0-9]+).</p>")
+      assert_not_nil m
+      verifier = m[1]
+    end
     token.reload
     assert_not_nil token.created_at
     assert_not_nil token.authorized_at
@@ -223,24 +188,17 @@ class OAuthTest < ActionDispatch::IntegrationTest
 
     signed_get "/api/0.6/user/preferences", :consumer => client, :token => token
     assert_response :unauthorized
+  end
 
-    signed_get "/oauth/request_token",
-               :consumer => client,
-               :oauth_callback => "http://another.web.app.org/callback"
-    assert_response :success
-    token = parse_token(response)
-    assert_instance_of RequestToken, token
-    assert_not_nil token.created_at
-    assert_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, client.permissions
+  def oauth10a_with_callback(client, callback_url)
+    token = get_request_token(client, :oauth_callback => callback_url)
 
     post "/oauth/authorize",
          :oauth_token => token.token,
          :allow_write_api => true, :allow_read_gpx => true
     assert_response :redirect
     verifier = parse_verifier(response)
-    assert_redirected_to "http://another.web.app.org/callback?oauth_token=#{token.token}&oauth_verifier=#{verifier}"
+    assert_redirected_to "#{callback_url}?oauth_token=#{token.token}&oauth_verifier=#{verifier}"
     token.reload
     assert_not_nil token.created_at
     assert_not_nil token.authorized_at
@@ -279,15 +237,8 @@ class OAuthTest < ActionDispatch::IntegrationTest
     assert_response :unauthorized
   end
 
-  def test_oauth10a_desktop_app
-    client = client_applications(:oauth_desktop_app)
-
-    post_via_redirect "/login",
-                      :username => client.user.email, :password => "test"
-    assert_response :success
-
-    signed_get "/oauth/request_token",
-               :consumer => client, :oauth_callback => "oob"
+  def get_request_token(client, options = {})
+    signed_get "/oauth/request_token", options.merge(:consumer => client)
     assert_response :success
     token = parse_token(response)
     assert_instance_of RequestToken, token
@@ -296,53 +247,8 @@ class OAuthTest < ActionDispatch::IntegrationTest
     assert_nil token.invalidated_at
     assert_allowed token, client.permissions
 
-    post "/oauth/authorize",
-         :oauth_token => token.token,
-         :allow_read_prefs => true, :allow_write_prefs => true
-    assert_response :success
-    assert_template "authorize_success"
-    m = response.body.match("<p>The verification code is ([A-Za-z0-9]+).</p>")
-    assert_not_nil m
-    verifier = m[1]
-    token.reload
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, [:allow_read_prefs]
-
-    signed_get "/oauth/access_token", :consumer => client, :token => token
-    assert_response :unauthorized
-
-    signed_get "/oauth/access_token",
-               :consumer => client, :token => token, :oauth_verifier => verifier
-    assert_response :success
-    token.reload
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_not_nil token.invalidated_at
-    token = parse_token(response)
-    assert_instance_of AccessToken, token
-    assert_not_nil token.created_at
-    assert_not_nil token.authorized_at
-    assert_nil token.invalidated_at
-    assert_allowed token, [:allow_read_prefs]
-
-    signed_get "/api/0.6/user/preferences", :consumer => client, :token => token
-    assert_response :success
-
-    signed_get "/api/0.6/gpx/2", :consumer => client, :token => token
-    assert_response :forbidden
-
-    post "/oauth/revoke", :token => token.token
-    assert_redirected_to oauth_clients_url(token.user.display_name)
-    token = OauthToken.find_by_token(token.token)
-    assert_not_nil token.invalidated_at
-
-    signed_get "/api/0.6/user/preferences", :consumer => client, :token => token
-    assert_response :unauthorized
+    token
   end
-
-  private
 
   def signed_get(uri, options)
     uri = URI.parse(uri)
