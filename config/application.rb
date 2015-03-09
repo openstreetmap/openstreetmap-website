dbba67450a5c41dcf542167415aec7da9fd26810
Tom Hughes <tom@compton.nu>
2015-03-09 20:54:33 +0000

    Fix offline mode

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module OpenStreetMap
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true unless STATUS == :database_offline
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
