fc57778d61f0d8f8ebff31c118cec448412f9ac7
Tom Hughes <tom@compton.nu>
2015-03-31 14:03:10 +0100

    Configure CA path for OpenID

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
+require "openid/fetchers"
+
+CA_BUNDLES = ["/etc/ssl/certs/ca-certificates.crt", "/etc/pki/tls/cert.pem"]
+
+OpenID.fetcher.ca_file = CA_BUNDLES.find { |f| File.exist?(f) }
+
 OmniAuth.config.logger = Rails.logger
 OmniAuth.config.failure_raise_out_environments = []
 
