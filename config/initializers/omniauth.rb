8ad2f4b5c8f14f97c44e91a599e6e7acb6f74f0b
Tom Hughes <tom@compton.nu>
2015-03-31 14:31:51 +0100

    Send OpenID logging to rails log

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
 require "openid/fetchers"
+require "openid/util"
 
 CA_BUNDLES = ["/etc/ssl/certs/ca-certificates.crt", "/etc/pki/tls/cert.pem"]
 
 OpenID.fetcher.ca_file = CA_BUNDLES.find { |f| File.exist?(f) }
+OpenID::Util.logger = Rails.logger
 
 OmniAuth.config.logger = Rails.logger
 OmniAuth.config.failure_raise_out_environments = []
