46761798797638a4ef8c35b3bf8e5b1ee83e263f
Tom Hughes <tom@compton.nu>
2015-06-25 17:44:20 +0100

    Improve display of user block timestamps
    Display the creation time, as a friendly date with a tooltip for
    the exact time, and add a tooltip to the expiry time.
    
    Fixes #923 and #996

--- app/helpers/user_blocks_helper.rb
+++ app/helpers/user_blocks_helper.rb
@@ -7,14 +7,14 @@ module UserBlocksHelper
       if block.needs_view?
         I18n.t("user_block.helper.until_login")
       else
-        I18n.t("user_block.helper.time_future", :time => distance_of_time_in_words_to_now(block.ends_at))
+        I18n.t("user_block.helper.time_future", :time => friendly_date(block.ends_at)).html_safe
       end
     else
       # the max of the last update time or the ends_at time is when this block finished
       # either because the user viewed the block (updated_at) or it expired or was
       # revoked (ends_at)
       last_time = [block.ends_at, block.updated_at].max
-      I18n.t("user_block.helper.time_past", :time => distance_of_time_in_words_to_now(last_time))
+      I18n.t("user_block.helper.time_past", :time => friendly_date(last_time)).html_safe
     end
   end
 end
