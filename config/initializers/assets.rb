84170254e4eefe74f2a14f1e395c22ec3ca40638
Tom Hughes <tom@compton.nu>
2015-06-28 10:35:44 +0100

    Use a fixed name for the asset manifest
    Because we deploy by updating an existing environment the default
    sprockets strategy gives us multiple randomly named manifest files
    and it will then pick one arbitrarily when starting up.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
+# Location of manifest file.
+Rails.application.config.assets.manifest = Rails.root.join("tmp/manifest.json")
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
