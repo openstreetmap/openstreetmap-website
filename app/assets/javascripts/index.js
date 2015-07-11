996d5413d85e4d38d33ac3827a1fc853ba9d0149
Tom Hughes <tom@compton.nu>
2015-07-11 11:40:14 +0100

    Remove donation drive banner
    Fixes #1003

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -165,15 +165,6 @@ $(document).ready(function () {
     $.cookie("_osm_location", OSM.locationCookie(map), { expires: expiry, path: "/" });
   });
 
-  if ($.cookie('_osm_donatedone2015') === 'hide') {
-    $('#donate').hide();
-  }
-
-  $('#donate .close').on('click', function() {
-    $('#donate').hide();
-    $.cookie("_osm_donatedone2015", 'hide', { expires: expiry });
-  });
-
   if ($.cookie('_osm_welcome') === 'hide') {
     $('.welcome').hide();
   }
