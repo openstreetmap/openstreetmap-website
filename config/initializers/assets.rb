d1d784bccad962ef23ce55f166aff7f34deba72b
Tom Hughes <tom@compton.nu>
2015-09-30 19:34:14 +0100

    Ensure assets are recompiled when configuration changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.manifest = Rails.root.join("tmp/manifest.json")
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("config")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
