18d26c0d81f1d0e39e90e1339b73037762df4e6f
Tom Hughes <tom@compton.nu>
2015-10-27 11:49:06 +0000

    Use PNG logo for og:image

--- app/helpers/open_graph_helper.rb
+++ app/helpers/open_graph_helper.rb
@@ -4,9 +4,8 @@ module OpenGraphHelper
       "og:site_name" => t("layouts.project_name.title"),
       "og:title" => [t("layouts.project_name.title"), title].compact.join(" | "),
       "og:type" => "website",
-      "og:image" => image_path("osm_logo.svg", :host => SERVER_URL),
-      "og:image:width" => "200",
-      "og:image:height" => "200"
+      "og:image" => image_path("osm_logo_256.png", :host => SERVER_URL, :protocol => "http"),
+      "og:image:secure_url" => image_path("osm_logo_256.png", :host => SERVER_URL, :protocol => "https")
     }
 
     tags.map do |property, content|
