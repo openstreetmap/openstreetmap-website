be624d64f395c40a78249754825cecdd9b050398
Tom Hughes <tom@compton.nu>
2016-02-11 10:15:19 +0000

    Avoid using Rails.application.assets as it no longer set in production

--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -1,11 +1,7 @@
 module AssetHelper
   def assets(directory)
-    assets = {}
-
-    Rails.application.assets.index.each_logical_path("#{directory}/*") do |path|
-      assets[path.sub(%r{^#{directory}/}, "")] = asset_path(path)
+    Rails.application.assets_manifest.assets.keys.each_with_object({}) do |asset, assets|
+      assets[asset] = asset_path(asset) if asset.start_with?("#{directory}/")
     end
-
-    assets
   end
 end
