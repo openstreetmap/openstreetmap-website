e2ed7f1bb1fa50aca042fb49bcfa02a6f4c4a59a
Seth Fitzsimmons <seth@mojodna.net>
2016-03-25 22:51:11 -0500

    Revert "Support URL prefixes"
    This reverts commit eb18300fead72f19ff15cb00c601c60ab64ed5cb.
    
    This had made an assumption that relative URLs were only 1 path
    component deep. Given that other JavaScript makes similar assumptions,
    it's easier to just virtual host this.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
-  var params = OSM.params(),
-      parts = window.location.pathname.split("/"),
-      prefix = parts.slice(0, parts.length - 1).join("/");
-
+  var params = OSM.params();
 
   if (params.lat && params.lon) {
     params.lat = parseFloat(params.lat);
     params.lon = parseFloat(params.lon);
     params.zoom = params.zoom || 17;
 
-    var url = prefix + '/edit';
+    var url = '/edit';
 
     if (params.editor) {
       url += '?editor=' + params.editor;
@@ -21,7 +18,7 @@ $(document).ready(function() {
 
   } else {
     var geoSuccess = function (position) {
-      window.location = prefix + '/edit' + OSM.formatHash({
+      window.location = '/edit' + OSM.formatHash({
         zoom: 17,
         lat: position.coords.latitude,
         lon: position.coords.longitude
@@ -45,6 +42,6 @@ $(document).ready(function() {
   }
 
   function manualEdit() {
-    window.location = prefix + '/?edit_help=1';
+    window.location = '/?edit_help=1';
   }
 });
