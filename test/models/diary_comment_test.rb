ab12b6161aa61463d7fbb0a9c77ae1d9ae14082a
Andy Allan <git@gravitystorm.co.uk>
2016-09-08 08:58:18 +0100

    Update name of test

--- test/models/diary_comment_test.rb
+++ test/models/diary_comment_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class DiaryCommentTest < ActiveSupport::TestCase
-  def test_diary_comment_count
+  def test_diary_comment_exists
     comment = create(:diary_comment)
     assert_includes DiaryComment.all, comment
   end
