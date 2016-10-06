e4d3c7fb8ee478bb7e489577a2404328c6477e53
Andy Allan <git@gravitystorm.co.uk>
2016-10-06 08:56:45 +0100

    Update note model tests to use factories.
    The NoteTest.test_visible? has changed slightly, so that the second
    test of .visible? is using a closed note, which is a more useful test
    than the original.

--- test/models/note_comment_test.rb
+++ test/models/note_comment_test.rb
@@ -2,20 +2,20 @@
 require "test_helper"
 
 class NoteCommentTest < ActiveSupport::TestCase
-  fixtures :users, :notes, :note_comments
+  fixtures :users
 
   def test_event_valid
     ok = %w(opened closed reopened commented hidden)
     bad = %w(expropriated fubared)
 
     ok.each do |event|
-      note_comment = note_comments(:t1)
+      note_comment = create(:note_comment)
       note_comment.event = event
       assert note_comment.valid?, "#{event} is invalid, when it should be"
     end
 
     bad.each do |event|
-      note_comment = note_comments(:t1)
+      note_comment = create(:note_comment)
       note_comment.event = event
       assert !note_comment.valid?, "#{event} is valid when it shouldn't be"
     end
@@ -28,13 +28,13 @@ class NoteCommentTest < ActiveSupport::TestCase
            "foo\ufffebar", "foo\uffffbar"]
 
     ok.each do |body|
-      note_comment = note_comments(:t1)
+      note_comment = create(:note_comment)
       note_comment.body = body
       assert note_comment.valid?, "#{body} is invalid, when it should be"
     end
 
     bad.each do |body|
-      note_comment = note_comments(:t1)
+      note_comment = create(:note_comment)
       note_comment.body = body
       assert !note_comment.valid?, "#{body} is valid when it shouldn't be"
     end
