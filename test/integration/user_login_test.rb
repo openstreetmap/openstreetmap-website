f7b4793c5048e7e939e7d614bed2c1b2473433d3
Tom Hughes <tom@compton.nu>
2016-10-27 12:15:28 +0100

    Require user_roles fixtures

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UserLoginTest < ActionDispatch::IntegrationTest
-  fixtures :users
+  fixtures :users, :user_roles
 
   def setup
     OmniAuth.config.test_mode = true
