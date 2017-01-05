0ca8b0a327408045f3d4dd05532e309ce26962ab
Tom Hughes <tom@compton.nu>
2017-01-05 08:45:59 +0000

    Place GPS tiles in the overlayPane

--- app/assets/javascripts/leaflet.map.js
+++ app/assets/javascripts/leaflet.map.js
@@ -57,6 +57,7 @@ L.OSM.Map = L.Map.extend({
     this.dataLayer.options.code = 'D';
 
     this.gpsLayer = new L.OSM.GPS({
+      pane: "overlayPane",
       code: "G",
       name: I18n.t("javascripts.map.base.gps")
     });
