f3941b7ee0427761f7e545a7ae402fb22efc8ac9
Herve Saint-Amand <git@saintamh.org>
2017-02-03 21:22:59 +0000

    Force the font on every <p> tag
    Without this Outlook and Windows Mail don't honour the font set at the top level. Note that this change predates all the screenshots I uploaded to the wiki on 2017-02-02, I'd just forgotten to commit them.

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -20,6 +20,9 @@ module NotifierHelper
   end
 
   def style_message(html)
-    html.gsub /<p>/, '<p style="color: black; margin: 0.75em 0">'
+    # Because we can't use stylesheets in HTML emails, we need to inline the
+    # styles. Rather than copy-paste the same string of CSS into every message,
+    # we apply it once here, after the message has been composed.
+    html.gsub /<p>/, '<p style="color: black; margin: 0.75em 0; font-family: \'Helvetica Neue\', Arial, Sans-Serif">'
   end
 end
