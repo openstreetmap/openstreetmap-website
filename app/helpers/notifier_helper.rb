852cc9770704a2f8580eac71d81e033fa5444331
Herve Saint-Amand <git@saintamh.org>
2017-02-09 20:38:02 +0000

    Fixed alignment & underlining bugs in Outlook / WinMail

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -5,10 +5,16 @@ module NotifierHelper
 
   def link_to_user(display_name)
     link_to(
-      display_name,
+      content_tag(
+        'strong',
+        display_name,
+        # NB we need "text-decoration: none" twice: GMail only honours it on
+        # the <a> but Outlook only on the <strong>
+        :style => "text-decoration: none"
+      ),
       user_url(display_name, :host => SERVER_URL),
       :target => "_blank",
-      :style => "text-decoration: none; color: #222; font-weight: bold"
+      :style => "text-decoration: none; color: #222"
     )
   end
 
