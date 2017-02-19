9b52542e521a0df1de742d9915770dcf8a07272e
Tom Hughes <tom@compton.nu>
2017-02-19 11:50:01 +0000

    Use simple string matches for user_blocks_helper tests
    Testing against the translations is not something we do anywhere
    else is and is dangerously close to testing the helper against
    itself which is a bit pointless.

--- test/helpers/user_blocks_helper_test.rb
+++ test/helpers/user_blocks_helper_test.rb
@@ -5,14 +5,12 @@ class UserBlocksHelperTest < ActionView::TestCase
 
   def test_block_status
     block = create(:user_block, :needs_view, :ends_at => Time.now.getutc)
-    assert_equal I18n.t("user_block.helper.until_login"), block_status(block)
+    assert_equal "Active until the user logs in.", block_status(block)
 
-    block_end = Time.now.getutc + 60.minutes
-    block = create(:user_block, :needs_view, :ends_at => block_end)
-    assert_equal I18n.t("user_block.helper.time_future_and_until_login", :time => friendly_date(block_end)), block_status(block)
+    block = create(:user_block, :needs_view, :ends_at => Time.now.getutc + 1.hour)
+    assert_match %r{^Ends in <span title=".*">about 1 hour</span> and after the user has logged in\.$}, block_status(block)
 
-    block_end = Time.now.getutc + 60.minutes
-    block = create(:user_block, :ends_at => block_end)
-    assert_equal I18n.t("user_block.helper.time_future", :time => friendly_date(block_end)), block_status(block)
+    block = create(:user_block, :ends_at => Time.now.getutc + 1.hour)
+    assert_match %r{^Ends in <span title=".*">about 1 hour</span>\.$}, block_status(block)
   end
 end
