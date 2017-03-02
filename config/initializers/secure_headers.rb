ee12eba234b93a81e666219ead19dacaf6b96bfb
Tom Hughes <tom@compton.nu>
2017-03-02 10:39:18 +0000

    Don't try and modify policy if we don't have one

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,24 +1,24 @@
-policy = if defined?(CSP_REPORT_URL)
-           {
-             :default_src => %w('self'),
-             :child_src => %w('self'),
-             :connect_src => %w('self'),
-             :font_src => %w('none'),
-             :form_action => %w('self'),
-             :frame_ancestors => %w('self'),
-             :img_src => %w('self' data: www.gravatar.com *.wp.com *.tile.openstreetmap.org *.tile.thunderforest.com *.openstreetmap.fr),
-             :media_src => %w('none'),
-             :object_src => %w('self'),
-             :plugin_types => %w('none'),
-             :script_src => %w('self'),
-             :style_src => %w('self' 'unsafe-inline'),
-             :report_uri => [CSP_REPORT_URL]
-           }
-         else
-           SecureHeaders::OPT_OUT
-         end
+if defined?(CSP_REPORT_URL)
+  policy = {
+    :default_src => %w('self'),
+    :child_src => %w('self'),
+    :connect_src => %w('self'),
+    :font_src => %w('none'),
+    :form_action => %w('self'),
+    :frame_ancestors => %w('self'),
+    :img_src => %w('self' data: www.gravatar.com *.wp.com *.tile.openstreetmap.org *.tile.thunderforest.com *.openstreetmap.fr),
+    :media_src => %w('none'),
+    :object_src => %w('self'),
+    :plugin_types => %w('none'),
+    :script_src => %w('self'),
+    :style_src => %w('self' 'unsafe-inline'),
+    :report_uri => [CSP_REPORT_URL]
+  }
 
-policy[:script_src] << PIWIK["location"] if defined?(PIWIK)
+  policy[:script_src] << PIWIK["location"] if defined?(PIWIK)
+else
+  policy = SecureHeaders::OPT_OUT
+end
 
 SecureHeaders::Configuration.default do |config|
   config.csp = SecureHeaders::OPT_OUT
