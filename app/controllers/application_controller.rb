727ffb3da3079c9444a402c87abceecbe1931acc
Tom Hughes <tom@compton.nu>
2017-03-05 12:09:48 +0000

    Update CSP to allow loads of tile status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -410,7 +410,7 @@ class ApplicationController < ActionController::Base
   def map_layout
     append_content_security_policy_directives(
       :child_src => %w[127.0.0.1:8111],
-      :connect_src => %w[nominatim.openstreetmap.org overpass-api.de router.project-osrm.org],
+      :connect_src => %w[*.tile.openstreetmap.org *.openstreetmap.fr nominatim.openstreetmap.org overpass-api.de router.project-osrm.org],
       :form_action => %w[render.openstreetmap.org],
       :script_src => %w[graphhopper.com open.mapquestapi.com],
       :img_src => %w[developer.mapquest.com]
