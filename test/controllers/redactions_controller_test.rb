912da4fc1ed4c0a68beb2850f0f404ce8082cad0
Andy Allan <git@gravitystorm.co.uk>
2017-03-05 16:34:36 +0000

    Use users factory for redactions_controller tests.

--- test/controllers/redactions_controller_test.rb
+++ test/controllers/redactions_controller_test.rb
@@ -53,7 +53,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_new_moderator
-    session[:user] = users(:moderator_user).id
+    session[:user] = create(:moderator_user).id
 
     get :new
     assert_response :success
@@ -61,7 +61,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_new_non_moderator
-    session[:user] = users(:public_user).id
+    session[:user] = create(:user).id
 
     get :new
     assert_response :redirect
@@ -69,7 +69,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_create_moderator
-    session[:user] = users(:moderator_user).id
+    session[:user] = create(:moderator_user).id
 
     post :create, :redaction => { :title => "Foo", :description => "Description here." }
     assert_response :redirect
@@ -77,7 +77,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_create_moderator_invalid
-    session[:user] = users(:moderator_user).id
+    session[:user] = create(:moderator_user).id
 
     post :create, :redaction => { :title => "Foo", :description => "" }
     assert_response :success
@@ -85,14 +85,14 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_create_non_moderator
-    session[:user] = users(:public_user).id
+    session[:user] = create(:user).id
 
     post :create, :redaction => { :title => "Foo", :description => "Description here." }
     assert_response :forbidden
   end
 
   def test_destroy_moderator_empty
-    session[:user] = users(:moderator_user).id
+    session[:user] = create(:moderator_user).id
 
     # remove all elements from the redaction
     redaction = redactions(:example)
@@ -106,7 +106,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_destroy_moderator_non_empty
-    session[:user] = users(:moderator_user).id
+    session[:user] = create(:moderator_user).id
 
     # leave elements in the redaction
     redaction = redactions(:example)
@@ -118,7 +118,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_delete_non_moderator
-    session[:user] = users(:public_user).id
+    session[:user] = create(:user).id
 
     delete :destroy, :id => redactions(:example).id
     assert_response :forbidden
@@ -131,14 +131,14 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_edit_moderator
-    session[:user] = users(:moderator_user).id
+    session[:user] = create(:moderator_user).id
 
     get :edit, :id => redactions(:example).id
     assert_response :success
   end
 
   def test_edit_non_moderator
-    session[:user] = users(:public_user).id
+    session[:user] = create(:user).id
 
     get :edit, :id => redactions(:example).id
     assert_response :redirect
@@ -146,7 +146,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_update_moderator
-    session[:user] = users(:moderator_user).id
+    session[:user] = create(:moderator_user).id
 
     redaction = redactions(:example)
 
@@ -156,7 +156,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_update_moderator_invalid
-    session[:user] = users(:moderator_user).id
+    session[:user] = create(:moderator_user).id
 
     redaction = redactions(:example)
 
@@ -166,7 +166,7 @@ class RedactionsControllerTest < ActionController::TestCase
   end
 
   def test_updated_non_moderator
-    session[:user] = users(:public_user).id
+    session[:user] = create(:user).id
 
     redaction = redactions(:example)
 
