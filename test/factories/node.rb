66ae9a48a698b1f2b3f638117cbf64030ce90797
Andy Allan <git@gravitystorm.co.uk>
2017-03-30 13:57:07 +0100

    For deleted nodes created via factories, make sure the most recent old_node is also deleted

--- test/factories/node.rb
+++ test/factories/node.rb
@@ -18,6 +18,13 @@ FactoryGirl.define do
         (1..node.version).each do |n|
           create(:old_node, :node_id => node.id, :version => n)
         end
+
+        # For deleted nodes, make sure the most recent old_node is also deleted.
+        if node.visible == false
+          latest = node.old_nodes.find_by(:version => node.version)
+          latest.visible = false
+          latest.save
+        end
       end
     end
   end
