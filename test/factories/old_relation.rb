27c3edae25e2ee2a3ef0ee0a354447694eef93c6
Andy Allan <git@gravitystorm.co.uk>
2017-04-25 15:19:25 +0200

    Create an old_relation factory, and start using it for tests.

---
+++ test/factories/old_relation.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :old_relation do
+    timestamp Time.now
+    visible true
+    version 1
+
+    changeset
+    association :current_relation, :factory => :relation
+  end
+end
--- test/models/old_relation_test.rb
+++ test/models/old_relation_test.rb
@@ -3,32 +3,28 @@ require "test_helper"
 class OldRelationTest < ActiveSupport::TestCase
   api_fixtures
 
-  def test_db_count
-    assert_equal 14, OldRelation.count
-  end
-
   def test_relation_tags
-    taglist_v3 = create_list(:old_relation_tag, 3, :old_relation => relations(:relation_with_versions_v3))
-    taglist_v4 = create_list(:old_relation_tag, 2, :old_relation => relations(:relation_with_versions_v4))
+    old_relation_v1 = create(:old_relation, :version => 1)
+    old_relation_v2 = create(:old_relation, :current_relation => old_relation_v1.current_relation, :version => 2)
+    old_relation_v3 = create(:old_relation, :current_relation => old_relation_v1.current_relation, :version => 3)
+    old_relation_v4 = create(:old_relation, :current_relation => old_relation_v1.current_relation, :version => 4)
+    taglist_v3 = create_list(:old_relation_tag, 3, :old_relation => old_relation_v3)
+    taglist_v4 = create_list(:old_relation_tag, 2, :old_relation => old_relation_v4)
 
-    relation = relations(:relation_with_versions_v1)
-    tags = OldRelation.find(relation.id).old_tags.order(:k)
+    tags = OldRelation.find(old_relation_v1.id).old_tags.order(:k)
     assert_equal 0, tags.count
 
-    relation = relations(:relation_with_versions_v2)
-    tags = OldRelation.find(relation.id).old_tags.order(:k)
+    tags = OldRelation.find(old_relation_v2.id).old_tags.order(:k)
     assert_equal 0, tags.count
 
-    relation = relations(:relation_with_versions_v3)
-    tags = OldRelation.find(relation.id).old_tags.order(:k)
+    tags = OldRelation.find(old_relation_v3.id).old_tags.order(:k)
     assert_equal taglist_v3.count, tags.count
     taglist_v3.sort_by!(&:k).each_index do |i|
       assert_equal taglist_v3[i].k, tags[i].k
       assert_equal taglist_v3[i].v, tags[i].v
     end
 
-    relation = relations(:relation_with_versions_v4)
-    tags = OldRelation.find(relation.id).old_tags.order(:k)
+    tags = OldRelation.find(old_relation_v4.id).old_tags.order(:k)
     assert_equal taglist_v4.count, tags.count
     taglist_v4.sort_by!(&:k).each_index do |i|
       assert_equal taglist_v4[i].k, tags[i].k
@@ -101,26 +97,26 @@ class OldRelationTest < ActiveSupport::TestCase
   end
 
   def test_tags
-    taglist_v3 = create_list(:old_relation_tag, 3, :old_relation => relations(:relation_with_versions_v3))
-    taglist_v4 = create_list(:old_relation_tag, 2, :old_relation => relations(:relation_with_versions_v4))
+    old_relation_v1 = create(:old_relation, :version => 1)
+    old_relation_v2 = create(:old_relation, :current_relation => old_relation_v1.current_relation, :version => 2)
+    old_relation_v3 = create(:old_relation, :current_relation => old_relation_v1.current_relation, :version => 3)
+    old_relation_v4 = create(:old_relation, :current_relation => old_relation_v1.current_relation, :version => 4)
+    taglist_v3 = create_list(:old_relation_tag, 3, :old_relation => old_relation_v3)
+    taglist_v4 = create_list(:old_relation_tag, 2, :old_relation => old_relation_v4)
 
-    relation = relations(:relation_with_versions_v1)
-    tags = OldRelation.find(relation.id).tags
+    tags = OldRelation.find(old_relation_v1.id).tags
     assert_equal 0, tags.size
 
-    relation = relations(:relation_with_versions_v2)
-    tags = OldRelation.find(relation.id).tags
+    tags = OldRelation.find(old_relation_v2.id).tags
     assert_equal 0, tags.size
 
-    relation = relations(:relation_with_versions_v3)
-    tags = OldRelation.find(relation.id).tags
+    tags = OldRelation.find(old_relation_v3.id).tags
     assert_equal taglist_v3.count, tags.count
     taglist_v3.each do |tag|
       assert_equal tag.v, tags[tag.k]
     end
 
-    relation = relations(:relation_with_versions_v4)
-    tags = OldRelation.find(relation.id).tags
+    tags = OldRelation.find(old_relation_v4.id).tags
     assert_equal taglist_v4.count, tags.count
     taglist_v4.each do |tag|
       assert_equal tag.v, tags[tag.k]
