0d5f1300f99b1080d5a801505954cb30308ee929
Hamel Desai <hdesai719@gmail.com>
2017-05-14 14:48:31 -0400

    Issue #1391: Added the gem social-share-button

---
+++ config/initializers/social_share_button.rb
@@ -0,0 +1,5 @@
+SocialShareButton.configure do |config|
+  config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark
+                          delicious tumblr pinterest email linkedin wechat vkontakte
+                          xing reddit hacker_news telegram odnoklassniki)
+end
