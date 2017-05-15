865452912914f5748549f3b12cca4ccd09f21f12
Hamel Desai <hdesai719@gmail.com>
2017-05-15 08:58:32 -0400

    Issue #1391: Changed config file

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,5 +1,10 @@
+# SocialShareButton.configure do |config|
+#   config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark
+#                           delicious tumblr pinterest email linkedin wechat vkontakte
+#                           xing reddit hacker_news telegram odnoklassniki)
+# end
+
+
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark
-                          delicious tumblr pinterest email linkedin wechat vkontakte
-                          xing reddit hacker_news telegram odnoklassniki)
+  config.allow_sites = %w(twitter facebook google_plus)
 end
