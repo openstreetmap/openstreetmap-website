da355dc4fe53056738c15a323df20512f3e80809
Hamel Desai <hdesai719@gmail.com>
2017-05-15 12:24:54 -0400

    Issue #1391: Icons are officially being loaded on the screen

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,10 +1,5 @@
-# SocialShareButton.configure do |config|
-#   config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark
-#                           delicious tumblr pinterest email linkedin wechat vkontakte
-#                           xing reddit hacker_news telegram odnoklassniki)
-# end
-
-
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus)
+  config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark
+                          delicious tumblr pinterest email linkedin wechat vkontakte
+                          xing reddit hacker_news telegram odnoklassniki)
 end
