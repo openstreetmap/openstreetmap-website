948fd8c8faf1016deb23b73eca0dadb6267603cf
Andy Allan <git@gravitystorm.co.uk>
2017-09-06 17:57:54 +0100

    Use reports_count so that counter_cache works automatically.

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report < ActiveRecord::Base
-  belongs_to :issue
+  belongs_to :issue, :counter_cache => true
   belongs_to :user, :class_name => "User", :foreign_key => :reporter_user_id
 end
--- db/migrate/20160822153153_add_reports_count_to_issues.rb
+++ db/migrate/20160822153153_add_reports_count_to_issues.rb
@@ -1,6 +1,6 @@
-class AddReportCountToIssues < ActiveRecord::Migration
+class AddReportsCountToIssues < ActiveRecord::Migration
   def change
-    add_column :issues, :report_count, :integer, :default => 0
+    add_column :issues, :reports_count, :integer, :default => 0
     add_foreign_key :issues, :users, :column => :updated_by, :name => "issues_updated_by_fkey", :on_delete => :cascade
     add_index :issues, :updated_by
   end
