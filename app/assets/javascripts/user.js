833579778a8ec756fd6ae30a458ee89224ad3a3c
Holger Jeromin <mailgithub@katur.de>
2017-10-18 15:05:11 +0200

    Removes DOM difference in location button

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -28,6 +28,7 @@ $(document).ready(function () {
       .removeClass('leaflet-control-locate leaflet-bar')
       .addClass('control-locate')
       .children("a")
+      .attr('href', '#')
       .removeClass('leaflet-bar-part leaflet-bar-part-single')
       .addClass('control-button');
 
