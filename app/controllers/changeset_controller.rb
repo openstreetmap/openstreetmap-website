b2a3955d936b448562db9981560135cf0303a0e0
mmd-osm <mmd.osm@gmail.com>
2017-12-16 13:32:04 +0100

    Allow (un)subscribing discussions also when changeset still open Fixes #1627

--- app/controllers/changeset_controller.rb
+++ app/controllers/changeset_controller.rb
@@ -357,7 +357,6 @@ class ChangesetController < ApplicationController
 
     # Find the changeset and check it is valid
     changeset = Changeset.find(id)
-    raise OSM::APIChangesetNotYetClosedError, changeset if changeset.is_open?
     raise OSM::APIChangesetAlreadySubscribedError, changeset if changeset.subscribers.exists?(current_user.id)
 
     # Add the subscriber
@@ -378,7 +377,6 @@ class ChangesetController < ApplicationController
 
     # Find the changeset and check it is valid
     changeset = Changeset.find(id)
-    raise OSM::APIChangesetNotYetClosedError, changeset if changeset.is_open?
     raise OSM::APIChangesetNotSubscribedError, changeset unless changeset.subscribers.exists?(current_user.id)
 
     # Remove the subscriber
