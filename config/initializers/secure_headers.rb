b396c8cbe5473746007369003cdac07224133f9e
Tom Hughes <tom@compton.nu>
2018-01-11 19:19:36 +0000

    Allow apache to control the HSTS setting

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -27,7 +27,6 @@ cookie_policy = {
 }
 
 SecureHeaders::Configuration.default do |config|
-  config.hsts = "max-age=0"
   config.csp = SecureHeaders::OPT_OUT
   config.csp_report_only = csp_policy
   config.cookies = cookie_policy
