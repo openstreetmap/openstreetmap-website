56448b50d06159e9ffaab70e962403a7753eb0bc
Tom Hughes <tom@compton.nu>
2018-01-18 17:32:33 +0000

    Update leaflet to 1.3.1

--- vendor/assets/leaflet/leaflet.js
+++ vendor/assets/leaflet/leaflet.js
@@ -1,5 +1,5 @@
 /* @preserve
- * Leaflet 1.3.0, a JS library for interactive maps. http://leafletjs.com
+ * Leaflet 1.3.1, a JS library for interactive maps. http://leafletjs.com
  * (c) 2010-2017 Vladimir Agafonkin, (c) 2010-2011 CloudMade
  */
 
@@ -9,7 +9,7 @@
 	(factory((global.L = {})));
 }(this, (function (exports) { 'use strict';
 
-var version = "1.3.0";
+var version = "1.3.1";
 
 /*
  * @namespace Util
@@ -11498,7 +11498,7 @@ var TileLayer = GridLayer.extend({
 			s: this._getSubdomain(coords),
 			x: coords.x,
 			y: coords.y,
-			z: coords.z ? coords.z : this._getZoomForUrl()
+			z: this._getZoomForUrl()
 		};
 		if (this._map && !this._map.options.crs.infinite) {
 			var invertedY = this._globalTileRange.max.y - coords.y;
