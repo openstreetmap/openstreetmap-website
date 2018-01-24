b590fdccfc03799b55e0996266de00e50cdbc88e
Tom Hughes <tom@compton.nu>
2018-01-24 14:37:37 +0000

    Fix rubocop warnings

--- test/integration/compressed_requests_test.rb
+++ test/integration/compressed_requests_test.rb
@@ -38,7 +38,7 @@ CHANGESET
     post "/api/0.6/changeset/#{changeset.id}/upload",
          :params => diff,
          :headers => {
-           "HTTP_AUTHORIZATION" => format("Basic %s", Base64.encode64("#{user.display_name}:test")),
+           "HTTP_AUTHORIZATION" => format("Basic %{auth}", :auth => Base64.encode64("#{user.display_name}:test")),
            "HTTP_CONTENT_TYPE" => "application/xml"
          }
     assert_response :success,
@@ -87,7 +87,7 @@ CHANGESET
     post "/api/0.6/changeset/#{changeset.id}/upload",
          :params => gzip_content(diff),
          :headers => {
-           "HTTP_AUTHORIZATION" => format("Basic %s", Base64.encode64("#{user.display_name}:test")),
+           "HTTP_AUTHORIZATION" => format("Basic %{auth}", :auth => Base64.encode64("#{user.display_name}:test")),
            "HTTP_CONTENT_ENCODING" => "gzip",
            "HTTP_CONTENT_TYPE" => "application/xml"
          }
@@ -137,7 +137,7 @@ CHANGESET
     post "/api/0.6/changeset/#{changeset.id}/upload",
          :params => deflate_content(diff),
          :headers => {
-           "HTTP_AUTHORIZATION" => format("Basic %s", Base64.encode64("#{user.display_name}:test")),
+           "HTTP_AUTHORIZATION" => format("Basic %{auth}", :auth => Base64.encode64("#{user.display_name}:test")),
            "HTTP_CONTENT_ENCODING" => "deflate",
            "HTTP_CONTENT_TYPE" => "application/xml"
          }
@@ -158,7 +158,7 @@ CHANGESET
     post "/api/0.6/changeset/#{changeset.id}/upload",
          :params => "",
          :headers => {
-           "HTTP_AUTHORIZATION" => format("Basic %s", Base64.encode64("#{user.display_name}:test")),
+           "HTTP_AUTHORIZATION" => format("Basic %{auth}", :auth => Base64.encode64("#{user.display_name}:test")),
            "HTTP_CONTENT_ENCODING" => "unknown",
            "HTTP_CONTENT_TYPE" => "application/xml"
          }
