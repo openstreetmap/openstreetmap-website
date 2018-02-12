c12ed852334cdaa1fa583f84641c967bf1d68429
Jamie Guthrie <jamie.guthrie@gmail.com>
2018-02-12 07:29:31 +0000

    Use both name and ref when naming roads in directions
    Closes #1741

--- app/assets/javascripts/index/directions/osrm.js
+++ app/assets/javascripts/index/directions/osrm.js
@@ -95,7 +95,18 @@ function OSRMEngine() {
         Array.prototype.push.apply(line, step_geometry);
 
         var instText = "<b>" + (idx + 1) + ".</b> ";
-        var name = step.name ? "<b>" + step.name + "</b>" : I18n.t('javascripts.directions.instructions.unnamed');
+        
+        var name;
+        if (step.name && step.ref) {
+          name = "<b>" + step.name + " (" + step.ref + ")</b>";
+        } else if (step.name) {
+          name = "<b>" + step.name + "</b>";
+        } else if (step.ref) {
+          name = "<b>" + step.ref + "</b>";
+        } else {
+          name = I18n.t('javascripts.directions.instructions.unnamed');
+        }
+        
         if (step.maneuver.type.match(/rotary|roundabout/)) {
           instText += I18n.t(template + '_with_exit', { exit: step.maneuver.exit, name: name } );
         } else {
