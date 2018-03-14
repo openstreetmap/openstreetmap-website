5e7abf67fbda21735d12660a537a96e39bb87107
Andy Allan <git@gravitystorm.co.uk>
2018-03-14 15:20:04 +0800

    Remove nonsense has_one relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,7 +74,6 @@ class User < ActiveRecord::Base
   has_many :roles, :class_name => "UserRole"
 
   has_many :issues, :class_name => "Issue", :foreign_key => :reported_user_id
-  has_one :issue, :class_name => "Issue", :foreign_key => :updated_by
   has_many :issue_comments
 
   has_many :reports
