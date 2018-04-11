0a86a1ea8db0867c34f7d9dff602ee3da6036bcd
Andy Allan <git@gravitystorm.co.uk>
2018-04-11 14:03:17 +0800

    Fix handling of title when replying to diary entries

---
+++ test/system/diary_entry_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class DiaryEntryTest < ApplicationSystemTestCase
+  def setup
+    create(:language, :code => "en")
+    @diary_entry = create(:diary_entry)
+  end
+
+  test "reply to diary entry should prefill the message subject" do
+    sign_in_as(create(:user))
+    visit diary_path
+
+    click_on "Reply to this entry"
+
+    assert page.has_content? "Send a new message"
+    assert_equal "Re: #{@diary_entry.title}", page.find_field("Subject").value
+  end
+end
