5deba2782a57a8d84024c941bf0a31b7c0b64c49
Tom Hughes <tom@compton.nu>
2018-05-23 12:09:21 +0100

    Completely remove form-action restrictions for OAuth callbacks
    The CSP3 draft only allows a * rule match network schemes and
    mobile devices often use callbacks to custom URL schemes.

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -38,7 +38,7 @@ class OauthController < ApplicationController
   protected
 
   def oauth1_authorize
-    append_content_security_policy_directives(:form_action => %w[*])
+    override_content_security_policy_directives(:form_action => false)
 
     if @token.invalidated?
       @message = t "oauth.authorize_failure.invalid"
