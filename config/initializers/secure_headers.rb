84fa19f2da8e8e414a34a4b224a8c506efbca5e8
Chris Flipse <cflipse@gmail.com>
2018-06-08 10:30:55 -0400

    Guard against undefined CSP_ENFORCE constant

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 csp_policy = {
   :preserve_schemes => true,
-  :default_src => %w['self'],
-  :child_src => %w['self'],
-  :connect_src => %w['self'],
-  :font_src => %w['none'],
-  :form_action => %w['self'],
-  :frame_ancestors => %w['self'],
-  :frame_src => %w['self'],
-  :img_src => %w['self' data: www.gravatar.com *.wp.com *.tile.openstreetmap.org *.tile.thunderforest.com *.openstreetmap.fr],
-  :manifest_src => %w['none'],
-  :media_src => %w['none'],
-  :object_src => %w['self'],
+  :default_src => %w[self],
+  :child_src => %w[self],
+  :connect_src => %w[self],
+  :font_src => %w[none],
+  :form_action => %w[self],
+  :frame_ancestors => %w[self],
+  :frame_src => %w[self],
+  :img_src => %w[self data: www.gravatar.com *.wp.com *.tile.openstreetmap.org *.tile.thunderforest.com *.openstreetmap.fr],
+  :manifest_src => %w[none],
+  :media_src => %w[none],
+  :object_src => %w[self],
   :plugin_types => %w[],
-  :script_src => %w['self'],
-  :style_src => %w['self'],
-  :worker_src => %w['none'],
+  :script_src => %w[self],
+  :style_src => %w[self],
+  :worker_src => %w[none],
   :report_uri => []
 }
 
@@ -31,7 +33,7 @@ cookie_policy = {
 SecureHeaders::Configuration.default do |config|
   config.hsts = SecureHeaders::OPT_OUT
 
-  if CSP_ENFORCE
+  if defined?(CSP_ENFORCE) && CSP_ENFORCE
     config.csp = csp_policy
     config.csp_report_only = SecureHeaders::OPT_OUT
   elsif defined?(CSP_REPORT_URL)
