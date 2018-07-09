53eadb36fcec6c0d07ba550a2f72c4d3932ffead
Tom Hughes <tom@compton.nu>
2018-07-09 20:36:41 +0100

    Fix another hostip stub

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -16,7 +16,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :poltergeist, :screen_size => [1400, 1400]
 
   def initialize(*args)
-    stub_request(:get, "http://api.hostip.info/country.php?ip=127.0.0.1")
+    stub_request(:get, "https://api.hostip.info/country.php?ip=127.0.0.1")
       .to_return(:status => 404)
     super(*args)
   end
