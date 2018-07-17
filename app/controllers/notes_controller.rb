1aa0e35a7a3d9a4ff8090f5a618babbd2a452757
Frederik Ramm <frederik@remote.org>
2018-07-17 12:41:49 +0200

    do not allow anonymous users to comment on notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,7 +5,7 @@ class NotesController < ApplicationController
   before_action :check_api_readable
   before_action :authorize_web, :only => [:mine]
   before_action :setup_user_auth, :only => [:create, :comment]
-  before_action :authorize, :only => [:close, :reopen, :destroy]
+  before_action :authorize, :only => [:close, :reopen, :destroy, :comment]
   before_action :require_moderator, :only => [:destroy]
   before_action :check_api_writable, :only => [:create, :comment, :close, :reopen, :destroy]
   before_action :require_allow_write_notes, :only => [:create, :comment, :close, :reopen, :destroy]
