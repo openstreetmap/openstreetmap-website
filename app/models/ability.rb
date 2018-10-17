ce761b3193c41081182e129d4f4aa18268e69264
Andy Allan <git@gravitystorm.co.uk>
2018-10-17 13:33:47 +0200

    Combine site permissions declarations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :index, :site
-    can [:permalink, :edit, :help, :fixthemap, :offline, :export, :about, :preview, :copyright, :key, :id], :site
+    can [:index, :permalink, :edit, :help, :fixthemap, :offline, :export, :about, :preview, :copyright, :key, :id], :site
     can [:index, :rss, :show, :comments], DiaryEntry
     can [:search, :search_latlon, :search_ca_postcode, :search_osm_nominatim,
          :search_geonames, :search_osm_nominatim_reverse, :search_geonames_reverse], :geocoder
