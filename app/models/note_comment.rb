d9a48d66f977afbe9d5e196e9e22d882b16b3566
mmd-osm <mmd.osm@gmail.com>
2018-11-03 18:32:02 +0100

    Limit notes sizes to 2000 characters

--- app/models/note_comment.rb
+++ app/models/note_comment.rb
@@ -33,7 +33,8 @@ class NoteComment < ActiveRecord::Base
   validates :visible, :inclusion => [true, false]
   validates :author, :associated => true
   validates :event, :inclusion => %w[opened closed reopened commented hidden]
-  validates :body, :format => /\A[^\x00-\x08\x0b-\x0c\x0e-\x1f\x7f\ufffe\uffff]*\z/
+  validates :body, :allow_blank => false, :length => { :maximum => 2000 },
+                   :format => /\A[^\x00-\x08\x0b-\x0c\x0e-\x1f\x7f\ufffe\uffff]*\z/
 
   # Return the comment text
   def body
