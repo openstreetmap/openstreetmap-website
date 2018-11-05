e93e5fbe63d89e2ea546fbb294d8d96e06ef39f1
Tom Hughes <tom@compton.nu>
2018-11-05 09:24:51 +0000

    Allow note comments with no body

--- app/models/note_comment.rb
+++ app/models/note_comment.rb
@@ -33,7 +33,7 @@ class NoteComment < ActiveRecord::Base
   validates :visible, :inclusion => [true, false]
   validates :author, :associated => true
   validates :event, :inclusion => %w[opened closed reopened commented hidden]
-  validates :body, :allow_blank => false, :length => { :maximum => 2000 },
+  validates :body, :length => { :maximum => 2000 },
                    :format => /\A[^\x00-\x08\x0b-\x0c\x0e-\x1f\x7f\ufffe\uffff]*\z/
 
   # Return the comment text
