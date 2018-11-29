db91b7c82c50dd82ad6e0b157eabc40b45267353
John Kirby <john.kirby.14@cnu.edu>
2018-11-29 13:16:39 -0500

    including comments in 058_name_unnamed_redactions.rb file

--- db/migrate/058_name_unnamed_redactions.rb
+++ db/migrate/058_name_unnamed_redactions.rb
@@ -1,6 +1,9 @@
 class NameUnnamedRedactions < ActiveRecord::Migration[5.0]
   def self.up
+  	# after titles are fixed we change the column so to not be null
     change_column_null "redactions", "title", false
+    # gets each redaction
+    # sets redaction title equal to a string of the id if there is no title
     Redaction.find_each do |redaction|
       if redaction.title.empty?
         redaction.title = redaction.id.to_s
@@ -10,6 +13,7 @@ class NameUnnamedRedactions < ActiveRecord::Migration[5.0]
   end
 
   def self.down
+  	# for down-migration we allow title column to be null
     change_column_null "redactions", "title", true
   end
 end
