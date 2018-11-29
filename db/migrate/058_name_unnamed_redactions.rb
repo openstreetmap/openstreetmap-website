51e59f291bd64e52cc86e7a7212d17f3c2bece02
William Drumheller <william@royalldesigns.com>
2018-11-28 20:46:05 -0500

    Add column changes to up (not null) and down (null) redaction migration
    Based on input from @tomhughes, removed unneeded changes from the down
    migration since we do not need to undo renaming unnamed redactions.
    Cleaned up the migration to have the de facto 2 space indentation.
    Added change_column_null method to up migration to make title column
    not null and to down migration to make title column null allowed.

--- db/migrate/058_name_unnamed_redactions.rb
+++ db/migrate/058_name_unnamed_redactions.rb
@@ -1,19 +1,15 @@
 class NameUnnamedRedactions < ActiveRecord::Migration[5.0]
   def self.up
+    change_column_null "redactions", "title", false
     Redaction.find_each do |redaction|
-        if redaction.title.empty?
-            redaction.title = redaction.id.to_s
-            redaction.save!
-        end
+      if redaction.title.empty?
+        redaction.title = redaction.id.to_s
+        redaction.save!
+      end
     end
   end
 
   def self.down
-    Redaction.find_each do |redaction|
-        if redaction.title.eql? redaction.id.to_s
-            redaction.title = ""
-            redaction.save!
-        end
-    end
+    change_column_null "redactions", "title", true
   end
 end
