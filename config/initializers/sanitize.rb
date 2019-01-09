df232ec96f0568c4a8659f73eba13642aed3bd3e
Tom Hughes <tom@compton.nu>
2019-01-09 18:00:47 +0000

    Add noopener and noreferer to links in user generated content

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -1,5 +1,5 @@
 Sanitize::Config::OSM = Sanitize::Config::RELAXED.dup
 
 Sanitize::Config::OSM[:elements] -= %w[div style]
-Sanitize::Config::OSM[:add_attributes] = { "a" => { "rel" => "nofollow" } }
+Sanitize::Config::OSM[:add_attributes] = { "a" => { "rel" => "nofollow noopener noreferer" } }
 Sanitize::Config::OSM[:remove_contents] = %w[script style]
