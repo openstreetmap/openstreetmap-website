3b96bbc80976836c6783ddd81db2f93b7e3fa3a2
Andy Allan <git@gravitystorm.co.uk>
2019-01-23 14:47:49 +0100

    Basic tests for the trace jobs

--- test/jobs/trace_destroyer_job_test.rb
+++ test/jobs/trace_destroyer_job_test.rb
@@ -1,7 +1,18 @@
-require 'test_helper'
+require "test_helper"
+require "minitest/mock"
 
 class TraceDestroyerJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_destroy_called
+    trace = Minitest::Mock.new
+
+    # Tiny little bit of mocking to make activejob happy
+    trace.expect :is_a?, false, [TraceDestroyerJob]
+
+    # Check that trace.destroy is called
+    trace.expect :destroy, true
+
+    TraceDestroyerJob.perform_now(trace)
+
+    assert_mock trace
+  end
 end
