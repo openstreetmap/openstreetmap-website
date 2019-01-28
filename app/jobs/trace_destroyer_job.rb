c583ca92edcb498fdd1f1560479254e388103d64
Tom Hughes <tom@compton.nu>
2019-01-28 19:10:35 +0000

    Don't swallow exceptions deleting traces

--- app/jobs/trace_destroyer_job.rb
+++ app/jobs/trace_destroyer_job.rb
@@ -3,8 +3,5 @@ class TraceDestroyerJob < ApplicationJob
 
   def perform(trace)
     trace.destroy
-  rescue StandardError => ex
-    logger.info ex.to_s
-    ex.backtrace.each { |l| logger.info l }
   end
 end
