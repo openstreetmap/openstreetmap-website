a5f412468107055f38b3f537b4bb8a01aec1db34
Andy Allan <git@gravitystorm.co.uk>
2019-03-27 17:15:18 +0100

    Refactor notes#mine to simplify code

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -16,13 +16,11 @@ class NotesController < ApplicationController
       if @user = User.active.find_by(:display_name => params[:display_name])
         @params = params.permit(:display_name)
         @title = t "notes.mine.title", :user => @user.display_name
-        @heading = t "notes.mine.heading", :user => @user.display_name
-        @description = t "notes.mine.subheading", :user => render_to_string(:partial => "user", :object => @user)
         @page = (params[:page] || 1).to_i
         @page_size = 10
         @notes = @user.notes
         @notes = @notes.visible unless current_user&.moderator?
-        @notes = @notes.order("updated_at DESC, id").distinct.offset((@page - 1) * @page_size).limit(@page_size).preload(:comments => :author).to_a
+        @notes = @notes.order("updated_at DESC, id").distinct.offset((@page - 1) * @page_size).limit(@page_size).preload(:comments => :author)
       else
         @title = t "users.no_such_user.title"
         @not_found_user = params[:display_name]
