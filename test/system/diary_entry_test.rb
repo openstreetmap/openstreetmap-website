be4eb547ad2feb0db6a240d260814b362dc688c9
Conor Souhrada <ConorSouhrada@gmail.com>
2019-04-15 12:12:24 -0600

    adds change to test

--- test/system/diary_entry_test.rb
+++ test/system/diary_entry_test.rb
@@ -10,7 +10,7 @@ class DiaryEntrySystemTest < ApplicationSystemTestCase
     sign_in_as(create(:user))
     visit diary_path
 
-    click_on "Reply to this entry"
+    click_on "Contact the author"
 
     assert page.has_content? "Send a new message"
     assert_equal "Re: #{@diary_entry.title}", page.find_field("Subject").value
