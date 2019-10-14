981fe0dab00868f5ac6e8765c55e8ea331ea4def
Brian DeRocher <brian@derocher.org>
2019-10-14 15:59:58 -0400

    safety is assured here

--- db/migrate/20190903030453_add_location_to_microcosms.rb
+++ db/migrate/20190903030453_add_location_to_microcosms.rb
@@ -1,13 +1,16 @@
 class AddLocationToMicrocosms < ActiveRecord::Migration[5.2]
   def change
-    change_table "microcosms", :bulk => true do |t|
-      t.string "location", :null => false
-      t.decimal "lat", :null => false
-      t.decimal "lon", :null => false
-      t.integer "min_lat", :null => false
-      t.integer "max_lat", :null => false
-      t.integer "min_lon", :null => false
-      t.integer "max_lon", :null => false
+    # This group of migrations for microcosms will be run together.
+    safety_assured do
+      change_table "microcosms", :bulk => true do |t|
+        t.string "location", :null => false
+        t.decimal "lat", :null => false
+        t.decimal "lon", :null => false
+        t.integer "min_lat", :null => false
+        t.integer "max_lat", :null => false
+        t.integer "min_lon", :null => false
+        t.integer "max_lon", :null => false
+      end
     end
   end
 end
