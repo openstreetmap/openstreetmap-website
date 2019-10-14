4d7df136293835fa6bdd6a49b4c90e73d33373c7
Brian DeRocher <brian@derocher.org>
2019-10-14 14:43:09 -0400

    CI was failing with "TypeError: StrongMigrations is not a class".

--- db/migrate/20190901143302_create_friendly_id_slugs.rb
+++ db/migrate/20190901143302_create_friendly_id_slugs.rb
@@ -1,4 +1,11 @@
-class CreateFriendlyIdSlugs < ActiveRecord::Migration[5.2]
+MIGRATION_CLASS =
+  if ActiveRecord::VERSION::MAJOR >= 5
+    ActiveRecord::Migration["#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}"]
+  else
+    ActiveRecord::Migration
+  end
+
+class CreateFriendlyIdSlugs < MIGRATION_CLASS
   def change
     create_table :friendly_id_slugs do |t|
       t.string :slug, :null => false
