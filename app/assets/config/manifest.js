30accae458c747dee3d8c64553004f79c4e27be0
Tom Hughes <tom@compton.nu>
2019-10-22 20:20:30 +0100

    Update to sprockets 4.x

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,23 @@
+//= link browserconfig.xml
+//= link manifest.json
+
+//= link_tree ../favicons
+//= link_tree ../images
+//= link_tree ../javascripts .js
+//= link_tree ../opensearch .xml
+//= link_directory ../stylesheets .css
+
+//= link_tree ../../../vendor/assets/iD/iD/img
+//= link_directory ../../../vendor/assets/iD/iD/locales .json
+
+//= link_directory ../../../vendor/assets/javascripts .js
+
+//= link_tree ../../../vendor/assets/leaflet .png
+
+//= link_directory ../../../vendor/assets/polyfill .js
+
+//= link_directory ../../../vendor/assets/potlatch2/help
+//= link_tree ../../../vendor/assets/potlatch2 .swf
+//= link_tree ../../../vendor/assets/potlatch2 .zip
+
+//= link_directory ../../../vendor/assets/swfobject
