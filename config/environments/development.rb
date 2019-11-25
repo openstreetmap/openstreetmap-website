621780c9cf2e0e0098f27e9ffacd1f7f6789fa2f
Tom Hughes <tom@compton.nu>
2019-11-25 12:45:39 +0000

    Disable host validation in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Disable host validation.
+  config.hosts = []
 end
