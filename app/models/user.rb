040302286f0fab0f9a4e448b5a9d835f2b6e106c
Andy Allan <git@gravitystorm.co.uk>
2019-12-11 18:08:31 +0100

    Remove ignoring of nearby column
    This is the final stage in the process. Now that the migrations are run,
    and apps restarted, it is safe to remove the ignore_column declaration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,8 +46,6 @@
 class User < ApplicationRecord
   require "xml/libxml"
 
-  self.ignored_columns = ["nearby"]
-
   has_many :traces, -> { where(:visible => true) }
   has_many :diary_entries, -> { order(:created_at => :desc) }
   has_many :diary_comments, -> { order(:created_at => :desc) }
