5ce28365b19c250cd4a569e98e55557737d8e77d
Brian DeRocher <brian@derocher.org>
2020-01-20 16:21:14 -0500

    Resolve this "Setting NOT NULL on a column requires an AccessExclusiveLock, which is expensive on large tables.  Instead use a contraint and validate it in a separate migration with a more agreeable RowShareLock."

--- db/migrate/20190901151436_add_slug_to_microcosms.rb
+++ db/migrate/20190901151436_add_slug_to_microcosms.rb
@@ -1,13 +1,28 @@
+#
+# Al of this junk will be reduced when I collapse the microcosm migrations.
+#
+
 class AddSlugToMicrocosms < ActiveRecord::Migration[5.2]
   def up
     add_column :microcosms, :slug, :string
     Microcosm.update_all ["slug = key"]
-    change_column_null :microcosms, :slug, false
+    #change_column_null :microcosms, :slug, false
+    safety_assured do
+      execute 'ALTER TABLE "microcosms" ADD CONSTRAINT "microcosms_slug_null" CHECK ("slug" is NOT NULL) NOT VALID'
+    end
   end
 
   def down
     Microcosm.update_all ["key = slug"]
     remove_column :microcosms, :slug
+  end
+end
+
+class ValidateAddSlugToMicrocosms < ActiveRecord::Migration[5.2]
+  def change
+    safety_assured do
+      execute 'ALTER TABLE "microcosms" VALIDATE CONSTRAINT "microcosms_slug_null"'
+    end
   end
 end
 
