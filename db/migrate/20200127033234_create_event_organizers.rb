af1ff77f21858aaa86bdb468d4567176a2a3a84c
Brian DeRocher <brian@derocher.org>
2020-01-27 23:18:58 -0500

    rubocop

--- db/migrate/20200127033234_create_event_organizers.rb
+++ db/migrate/20200127033234_create_event_organizers.rb
@@ -1,8 +1,8 @@
 class CreateEventOrganizers < ActiveRecord::Migration[6.0]
   def change
     create_table :event_organizers do |t|
-      t.references :event, foreign_key: true
-      t.references :user, foreign_key: true
+      t.references :event, :foreign_key => true
+      t.references :user, :foreign_key => true
 
       t.timestamps
     end
