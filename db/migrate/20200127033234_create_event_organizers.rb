8479c7165d262855c25cb577be0f497d91e1c4ce
Brian DeRocher <brian@derocher.org>
2020-01-27 23:11:32 -0500

    Forgot to include the migration

---
+++ db/migrate/20200127033234_create_event_organizers.rb
@@ -0,0 +1,10 @@
+class CreateEventOrganizers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :event_organizers do |t|
+      t.references :event, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
