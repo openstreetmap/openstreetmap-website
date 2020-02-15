dd446f4db23e09a4e0afe2f36e75f41ed83e5812
Brian DeRocher <brian@derocher.org>
2020-02-15 17:07:31 -0500

    Add some validation and model testing for microcosms.

--- test/factories/microcosms.rb
+++ test/factories/microcosms.rb
@@ -1,9 +1,14 @@
 FactoryBot.define do
   factory :microcosm do
-    name { "MyString" }
-    key { "MyString" }
-    facebook { "MyString" }
-    twitter { "MyString" }
-    description { "MyText" }
+    sequence(:name) { |n| "Microcosm #{n}" }
+    sequence(:description) { |n| "This is description #{n}" }
+    latitude { rand(-90.0...90.0) }
+    longitude { rand(-180.0...180.0) }
+    min_lat { rand(-90.0...90.0) }
+    max_lat { rand(-90.0...90.0) }
+    min_lon { rand(-180.0...180.0) }
+    max_lon { rand(-180.0...180.0) }
+
+    #user
   end
 end
--- test/models/microcosm_test.rb
+++ test/models/microcosm_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class MicrocosmTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_microcosm_validations
+    microcosm_valid({})
+
+    microcosm_valid({ :name => nil }, false)
+    microcosm_valid({ :name => "" }, false)
+    microcosm_valid(:name => "a" * 255)
+    microcosm_valid({ :name => "a" * 256 }, false)
+
+    microcosm_valid({ :description => "" }, false)
+    microcosm_valid(:description => "a" * 1023)
+    microcosm_valid({ :description => "a" * 1024 }, false)
+
+    microcosm_valid(:latitude => 90)
+    microcosm_valid({ :latitude => 90.00001 }, false)
+    microcosm_valid(:latitude => -90)
+    microcosm_valid({ :latitude => -90.00001 }, false)
+
+    microcosm_valid(:longitude => 180)
+    microcosm_valid({ :longitude => 180.00001 }, false)
+    microcosm_valid(:longitude => -180)
+    microcosm_valid({ :longitude => -180.00001 }, false)
+
+    [:min, :max].each do |extremum|
+      [:lat, :lon].each do |coord|
+        attr =  "#{extremum}_#{coord}"
+        microcosm_valid({attr => nil}, false)
+        microcosm_valid({attr => -200}, false)
+        microcosm_valid({attr => 200}, false)
+      end
+    end
+  end
+
+  def microcosm_valid(attrs, result = true)
+    mic = build(:microcosm, attrs)
+    #print(mic.inspect)
+    assert_equal result, mic.valid?, "Expected #{attrs.inspect} to be #{result}"
+  end
 end
