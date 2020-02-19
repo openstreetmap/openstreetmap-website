e95fa3fe77432054bfaefd5a3839363bcb022cf9
Brian DeRocher <brian@derocher.org>
2020-02-19 07:12:17 -0500

    Add missing colon for null symbol.

--- db/migrate/20190905160802_create_events.rb
+++ db/migrate/20190905160802_create_events.rb
@@ -5,7 +5,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.datetime :moment
       t.string :location
       t.text :description
-      t.references :microcosm, :foreign_key => true, null => false
+      t.references :microcosm, :foreign_key => true, :null => false
       t.timestamps
     end
   end
