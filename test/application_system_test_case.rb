8773ec6e57bfcd2e3c316ae5bdbb36fec2a3f104
Tom Hughes <tom@compton.nu>
2020-04-22 12:23:18 +0100

    Switch to using selenium for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,21 +1,13 @@
 require "test_helper"
-require "capybara/poltergeist"
 
-# Work around weird debian/ubuntu phantomjs
-# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=817277
-# https://github.com/ariya/phantomjs/issues/14376
-ENV["QT_QPA_PLATFORM"] = "offscreen" if IO.popen(["phantomjs", "--version"], :err => :close).read.empty?
+ENV.delete("http_proxy")
 
 ActiveSupport.on_load(:action_dispatch_system_test_case) do
   ActionDispatch::SystemTesting::Server.silence_puma = true
 end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :poltergeist, :screen_size => [1400, 1400], :options => { :timeout => 120 }
-
-  # Phantomjs can pick up browser Accept-Language preferences from your desktop environment.
-  # We don't want this to happen during the tests!
-  setup do
-    page.driver.add_headers("Accept-Language" => "en")
+  driven_by :selenium, :using => :headless_firefox do |options|
+    options.add_preference("intl.accept_languages", "en")
   end
 end
