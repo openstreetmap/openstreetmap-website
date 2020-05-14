43bd03a791de106161400f62eb04db67b11444c9
Brian DeRocher <brian@derocher.org>
2020-05-13 22:19:33 -0400

    When running these migrations the table will be empty, so adding 3 columns is fine.

--- db/migrate/20200214135750_add_coords_to_event.rb
+++ db/migrate/20200214135750_add_coords_to_event.rb
@@ -1,9 +1,11 @@
 class AddCoordsToEvent < ActiveRecord::Migration[6.0]
   def change
-    change_table :events, :bulk => true do |t|
-      t.float :latitude
-      t.float :longitude
-      t.string :location_url
+    safety_assured do
+      change_table :events, :bulk => true do |t|
+        t.float :latitude
+        t.float :longitude
+        t.string :location_url
+      end
     end
   end
 end
