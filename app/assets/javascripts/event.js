8c652ed6685631343255db821ea0713ee059638e
Brian DeRocher <brian@derocher.org>
2020-07-01 23:26:43 -0400

    Don't pollute the global namespace.

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,14 +1,14 @@
 /*global showMap,formMapInput*/
 
-function init_event_form() {
-  formMapInput("event_map_form", "event");
-}
-
-function init_event_show() {
-  showMap("event_map_show");
-}
-
 $(document).ready(function () {
+  function init_event_form() {
+    formMapInput("event_map_form", "event");
+  }
+
+  function init_event_show() {
+    showMap("event_map_show");
+  }
+
   if ($("#event_map_form").length) {
     init_event_form();
   } else if ($("#event_map_show").length) {
