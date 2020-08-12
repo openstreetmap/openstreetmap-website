8df30107667e28bfb7f99a13c679c174645b81bf
Andy Allan <git@gravitystorm.co.uk>
2020-08-12 16:55:34 +0200

    Refactor reports form to use bootstrap

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,5 +1,14 @@
+require "ostruct"
+
 module ReportsHelper
   def report_link(name, reportable)
     link_to name, new_report_url(:reportable_id => reportable.id, :reportable_type => reportable.class.name)
+  end
+
+  # Convert a list of strings into objects with methods that the collection_radio_buttons helper expects
+  def report_categories(reportable)
+    Report.categories_for(reportable).map do |c|
+      OpenStruct.new(:id => c, :label => t(".categories.#{reportable.class.name.underscore}.#{c}_label"))
+    end
   end
 end
