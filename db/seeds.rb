865d20af80a70929c761e3ea6d1010669bb4ed62
Andy Allan <git@gravitystorm.co.uk>
2020-09-30 15:09:49 +0200

    Use the db:seed task to load languages
    This is seed data that should reasonably be loaded into every database,
    even those that are otherwise empty (e.g. no geo data). Using the seeds
    process means that it will usually be loaded by default, and is easier
    to find for existing rails developers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Language.load(Rails.root.join("config/languages.yml"))
