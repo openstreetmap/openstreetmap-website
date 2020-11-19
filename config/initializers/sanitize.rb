d516ba53357e8673ca35979de4a4ffc4ab142e00
Tom Hughes <tom@compton.nu>
2020-11-19 10:17:21 +0000

    Add bootstrap classes to markdown tables

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -3,3 +3,6 @@ Sanitize::Config::OSM = Sanitize::Config::RELAXED.dup
 Sanitize::Config::OSM[:elements] -= %w[div style]
 Sanitize::Config::OSM[:add_attributes] = { "a" => { "rel" => "nofollow noopener noreferrer" } }
 Sanitize::Config::OSM[:remove_contents] = %w[script style]
+Sanitize::Config::OSM[:transformers] = lambda do |env|
+  env[:node].add_class("table table-sm w-auto") if env[:node_name] == "table"
+end
