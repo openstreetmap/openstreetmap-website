127880a73fa461400c53b29c98721e34660bca5e
Tom Hughes <tom@compton.nu>
2020-12-10 14:46:41 +0000

    Switch to using the zeitwork autoloader

--- app/validators/utf8_validator.rb
+++ app/validators/utf8_validator.rb
@@ -2,7 +2,7 @@
 # validation method to be included like any other validations methods
 # in the models definitions. this one checks that the named attribute
 # is a valid UTF-8 format string.
-class Utf8Validator < ActiveModel::EachValidator
+class UTF8Validator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors.add(attribute, " is invalid UTF-8") unless UTF8.valid? value
   end
