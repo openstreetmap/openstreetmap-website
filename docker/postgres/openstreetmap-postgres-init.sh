87c62fec3bd84dbdc16145e42b412369905fa744
Michal Migurski <mike@teczno.com>
2021-02-03 20:37:06 -0800

    Added note about password and privileges for OSM DB user

--- docker/postgres/openstreetmap-postgres-init.sh
+++ docker/postgres/openstreetmap-postgres-init.sh
@@ -2,6 +2,7 @@
 set -ex
 
 # Create 'openstreetmap' user
+# Password and superuser privilege are needed to successfully run test suite
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
     CREATE USER openstreetmap SUPERUSER PASSWORD 'openstreetmap';
     GRANT ALL PRIVILEGES ON DATABASE openstreetmap TO openstreetmap;
