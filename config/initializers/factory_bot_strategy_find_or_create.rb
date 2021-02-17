e944d1e72bc3cf9ae9fdeb7735e68e5023ef7a99
Andy Allan <git@gravitystorm.co.uk>
2021-02-17 14:34:42 +0000

    Ensure the find_or_create strategy is only available in the development environment
    We don't want any FactoryBot stuff available in production, and this shouldn't be used
    by mistake in the test environment either.

--- config/initializers/factory_bot_strategy_find_or_create.rb
+++ config/initializers/factory_bot_strategy_find_or_create.rb
@@ -1,31 +1,33 @@
 # From https://stackoverflow.com/a/65700370/
-module FactoryBot
-  module Strategy
-    # Does not work when passing objects as associations: `FactoryBot.find_or_create(:entity, association: object)`
-    # Instead do: `FactoryBot.find_or_create(:entity, association_id: id)`
-    class FindOrCreate
-      def initialize
-        @build_strategy = FactoryBot.strategy_by_name(:build).new
-      end
+if Rails.env.development?
+  module FactoryBot
+    module Strategy
+      # Does not work when passing objects as associations: `FactoryBot.find_or_create(:entity, association: object)`
+      # Instead do: `FactoryBot.find_or_create(:entity, association_id: id)`
+      class FindOrCreate
+        def initialize
+          @build_strategy = FactoryBot.strategy_by_name(:build).new
+        end
 
-      delegate :association, :to => :@build_strategy
+        delegate :association, :to => :@build_strategy
 
-      def result(evaluation)
-        attributes = attributes_shared_with_build_result(evaluation)
-        evaluation.object.class.where(attributes).first || FactoryBot.strategy_by_name(:create).new.result(evaluation)
-      end
+        def result(evaluation)
+          attributes = attributes_shared_with_build_result(evaluation)
+          evaluation.object.class.where(attributes).first || FactoryBot.strategy_by_name(:create).new.result(evaluation)
+        end
 
-      private
+        private
 
-      # Here we handle possible mismatches between initially provided attributes and actual model attrbiutes
-      # For example, devise's User model is given a `password` and generates an `encrypted_password`
-      # In this case, we shouldn't use `password` in the `where` clause
-      def attributes_shared_with_build_result(evaluation)
-        object_attributes = evaluation.object.attributes
-        evaluation.hash.filter { |k, _v| object_attributes.key?(k.to_s) }
+        # Here we handle possible mismatches between initially provided attributes and actual model attrbiutes
+        # For example, devise's User model is given a `password` and generates an `encrypted_password`
+        # In this case, we shouldn't use `password` in the `where` clause
+        def attributes_shared_with_build_result(evaluation)
+          object_attributes = evaluation.object.attributes
+          evaluation.hash.filter { |k, _v| object_attributes.key?(k.to_s) }
+        end
       end
     end
-  end
 
-  register_strategy(:find_or_create, Strategy::FindOrCreate)
+    register_strategy(:find_or_create, Strategy::FindOrCreate)
+  end
 end
