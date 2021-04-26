46eae20478aa4b4317ea1654b8c16854f8d5aa02
Tom Hughes <tom@compton.nu>
2021-04-26 22:07:37 +0100

    Monkey patch oauth gem to avoid use of deprecated URI.unescape

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -13,6 +13,12 @@ module OAuth
         end.join
       end.force_encoding(Encoding::US_ASCII)
     end
+
+    def unescape(value)
+      value.to_s.gsub(/%\h{2}/) do |c|
+        c[1..-1].to_i(16).chr
+      end.force_encoding(Encoding::UTF_8)
+    end
   end
 
   module RequestProxy
