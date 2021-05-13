c7ad8880150d2997cdf1a9f79269a2aada12f425
Tom Hughes <tom@compton.nu>
2021-05-13 20:25:42 +0100

    Enable new Active Job defaults

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -13,11 +13,11 @@
 # Rails.application.config.active_storage.track_variants = true
 
 # Apply random variation to the delay when retrying failed jobs.
-# Rails.application.config.active_job.retry_jitter = 0.15
+Rails.application.config.active_job.retry_jitter = 0.15
 
 # Stop executing `after_enqueue`/`after_perform` callbacks if
 # `before_enqueue`/`before_perform` respectively halts with `throw :abort`.
-# Rails.application.config.active_job.skip_after_callbacks_if_terminated = true
+Rails.application.config.active_job.skip_after_callbacks_if_terminated = true
 
 # Specify cookies SameSite protection level: either :none, :lax, or :strict.
 #
