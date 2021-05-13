94c515106498fe655c0722689abb3984556df96a
Tom Hughes <tom@compton.nu>
2021-05-13 19:57:47 +0100

    Enable link header for asset preloading

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -64,4 +64,4 @@ Rails.application.config.active_record.legacy_connection_handling = false
 
 # Generate a `Link` header that gives a hint to modern browsers about
 # preloading assets when using `javascript_include_tag` and `stylesheet_link_tag`.
-# Rails.application.config.action_view.preload_links_header = true
+Rails.application.config.action_view.preload_links_header = true
