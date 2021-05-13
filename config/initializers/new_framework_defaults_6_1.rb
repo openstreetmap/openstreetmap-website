afc4c6fde1f1ed9350cd35a433e841dbf07d244f
Tom Hughes <tom@compton.nu>
2021-05-13 19:52:02 +0100

    Enable use of URL safe CSRF tokens

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -29,7 +29,7 @@
 #
 # This change is not backwards compatible with earlier Rails versions.
 # It's best enabled when your entire app is migrated and stable on 6.1.
-# Rails.application.config.action_controller.urlsafe_csrf_tokens = true
+Rails.application.config.action_controller.urlsafe_csrf_tokens = true
 
 # Specify whether `ActiveSupport::TimeZone.utc_to_local` returns a time with an
 # UTC offset or a UTC time.
