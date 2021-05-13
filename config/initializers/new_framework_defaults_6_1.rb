32ebe67c0070e202e6ecd457ed43ffcdbdd181d2
Tom Hughes <tom@compton.nu>
2021-05-13 19:54:07 +0100

    Enable new connection handling API

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -42,7 +42,7 @@ Rails.application.config.action_controller.urlsafe_csrf_tokens = true
 # Use new connection handling API. For most applications this won't have any
 # effect. For applications using multiple databases, this new API provides
 # support for granular connection swapping.
-# Rails.application.config.active_record.legacy_connection_handling = false
+Rails.application.config.active_record.legacy_connection_handling = false
 
 # Make `form_with` generate non-remote forms by default.
 # Rails.application.config.action_view.form_with_generates_remote_forms = false
