ba8093f13a5ff0a380971c511a70ee3e2f44e14a
Tom Hughes <tom@compton.nu>
2021-07-06 19:30:05 +0100

    Allow cross origin access to OAuth 2 token endpoints

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -22,6 +22,9 @@ Rails.application.config.middleware.insert_before 0, OpenStreetMap::Cors do
   allow do
     origins "*"
     resource "/oauth/*", :headers => :any, :methods => [:get, :post]
+    resource "/oauth2/token", :headers => :any, :methods => [:post]
+    resource "/oauth2/revoke", :headers => :any, :methods => [:post]
+    resource "/oauth2/introspect", :headers => :any, :methods => [:post]
     resource "/api/*", :headers => :any, :methods => [:get, :post, :put, :delete]
     resource "/diary/rss", :headers => :any, :methods => [:get]
     resource "/diary/*/rss", :headers => :any, :methods => [:get]
