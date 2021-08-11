59e6cdebdc84b408b51a870eab9ff83209074da5
Adam Hoyle <atomoil@gmail.com>
2021-08-11 23:45:44 +0100

    add osm-community-index and parse in communities

--- app/models/communities.rb
+++ app/models/communities.rb
@@ -1,9 +1,36 @@
 
-
+# require 'sprockets/railtie'
 
 class Communities
 
+  # include Sprockets::Helpers::RailsHelper
+
   def self.local_chapters
-    array_string_ = ["Local Chapter 1", "Local Chapter 2", "Local Chapter 3", "Another one", "And Another"]
+    self.load_local_chapters
   end
+
+  protected
+
+  def self.load_local_chapters
+    puts Dir.pwd
+    json_file = File.expand_path("node_modules/osm-community-index/dist/completeFeatureCollection.json", Dir.pwd);
+    # json_file = File.expand_path("./node_modules/osm-community-index", Dir.pwd);
+
+    path = File.exist?(json_file) # Dir.pwd # File.open(json_file, "r")
+    community_index = JSON.parse(File.read(json_file))
+
+    array_of_entries = []
+    community_index['features'].each do |feature|
+      feature['properties']['resources'].each do |id, data|
+        data.each do |key, value|
+          if key == "type" and value == "osm-lc"
+            array_of_entries.push(id);
+          end
+        end
+      end
+    end
+
+    return array_of_entries
+  end
+
 end
