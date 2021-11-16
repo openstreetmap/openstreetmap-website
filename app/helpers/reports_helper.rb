e3966479a6ea5c579706d227fd6b55a41fb183db
Tom Hughes <tom@compton.nu>
2021-11-16 08:30:04 +0000

    Fix new rubocop warnings

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,6 +1,8 @@
 require "ostruct"
 
 module ReportsHelper
+  Category = Struct.new(:id, :label)
+
   def report_link(name, reportable)
     link_to name, new_report_url(:reportable_id => reportable.id, :reportable_type => reportable.class.name)
   end
@@ -8,7 +10,7 @@ module ReportsHelper
   # Convert a list of strings into objects with methods that the collection_radio_buttons helper expects
   def report_categories(reportable)
     Report.categories_for(reportable).map do |c|
-      OpenStruct.new(:id => c, :label => t(".categories.#{reportable.class.name.underscore}.#{c}_label"))
+      Category.new(c, t(".categories.#{reportable.class.name.underscore}.#{c}_label"))
     end
   end
 end
