707ebddbb5da007406ae04e9a418882fc278be01
Tom Hughes <tom@compton.nu>
2022-01-17 09:33:28 +0000

    Remove form_action restrictions for ouath2_authorizations#create
    Fixes #3424

--- app/controllers/oauth2_authorizations_controller.rb
+++ app/controllers/oauth2_authorizations_controller.rb
@@ -3,12 +3,13 @@ class Oauth2AuthorizationsController < Doorkeeper::AuthorizationsController
 
   prepend_before_action :authorize_web
   before_action :set_locale
+  before_action :allow_all_form_action, :only => [:new, :create]
 
   authorize_resource :class => false
 
-  def new
-    override_content_security_policy_directives(:form_action => []) if Settings.csp_enforce || Settings.key?(:csp_report_url)
+  private
 
-    super
+  def allow_all_form_action
+    override_content_security_policy_directives(:form_action => []) if Settings.csp_enforce || Settings.key?(:csp_report_url)
   end
 end
