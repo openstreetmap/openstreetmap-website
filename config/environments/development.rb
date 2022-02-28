2122d1843ebee7dd3fe21a0b647cfd9e3750dfb7
Tom Hughes <tom@compton.nu>
2022-02-28 18:47:51 +0000

    Restore asset debugging in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,6 +56,11 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true unless Settings.status == "database_offline"
 
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
