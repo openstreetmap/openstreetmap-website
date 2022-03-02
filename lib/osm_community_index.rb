1bcde09125777a0fd36ecb82a21deff558f4265a
Andy Allan <git@gravitystorm.co.uk>
2022-03-02 16:23:07 +0000

    Unnest OsmCommunityIndex
    No need for OsmCommunityIndex::OsmCommunityIndex.foo

---
+++ lib/osm_community_index.rb
@@ -0,0 +1,12 @@
+module OsmCommunityIndex
+  require "yaml"
+
+  def self.community_index
+    @community_index ||= community_index_from_json
+  end
+
+  def self.community_index_from_json
+    json_file = Rails.root.join("node_modules/osm-community-index/dist/resources.json")
+    JSON.parse(File.read(json_file))
+  end
+end
