73000191c679d5da420f7bcd74e177dc1dc50300
Anton Khorev <tony29@yandex.ru>
2022-07-19 15:06:59 +0300

    Move date range code into its own method

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -285,26 +285,10 @@ module Api
 
       # Add any date filter
       if params[:from]
-        begin
-          from = Time.parse(params[:from]).utc
-        rescue ArgumentError
-          raise OSM::APIBadUserInput, "Date #{params[:from]} is in a wrong format"
-        end
-
-        begin
-          to = if params[:to]
-                 Time.parse(params[:to]).utc
-               else
-                 Time.now.utc
-               end
-        rescue ArgumentError
-          raise OSM::APIBadUserInput, "Date #{params[:to]} is in a wrong format"
-        end
-
         @notes = if params[:sort] == "updated_at"
-                   @notes.where(:updated_at => from..to)
+                   @notes.where(:updated_at => date_range)
                  else
-                   @notes.where(:created_at => from..to)
+                   @notes.where(:created_at => date_range)
                  end
       end
 
@@ -374,6 +358,28 @@ module Api
       else
         notes.where(:status => "open")
       end
+    end
+
+    ##
+    # Get date interval out of from/to parameters assuming that from parameter is present
+    def date_range
+      begin
+        from = Time.parse(params[:from]).utc
+      rescue ArgumentError
+        raise OSM::APIBadUserInput, "Date #{params[:from]} is in a wrong format"
+      end
+
+      begin
+        to = if params[:to]
+               Time.parse(params[:to]).utc
+             else
+               Time.now.utc
+             end
+      rescue ArgumentError
+        raise OSM::APIBadUserInput, "Date #{params[:to]} is in a wrong format"
+      end
+
+      from..to
     end
 
     ##
