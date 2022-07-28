cbb87726852e4d614d8e54ffb5573b28288b3d47
Anton Khorev <tony29@yandex.ru>
2022-07-28 22:04:38 +0300

    Fix map key tooltip

--- test/system/site_test.rb
+++ test/system/site_test.rb
@@ -6,4 +6,35 @@ class SiteTest < ApplicationSystemTestCase
 
     assert_selector "h1", :text => "OpenStreetMap"
   end
+
+  test "tooltip shows for Layers button" do
+    visit "/"
+
+    assert_no_selector ".tooltip"
+    button = find ".control-layers .control-button"
+    button.hover
+    assert_selector ".tooltip", :text => "Layers"
+  end
+
+  test "tooltip shows for Map Key button on Standard layer" do
+    visit "/"
+
+    assert_no_selector ".tooltip"
+    button = find ".control-key .control-button"
+    button.hover
+    tooltip = find ".tooltip"
+    tooltip.assert_text "Map Key"
+    tooltip.assert_no_text "not available"
+  end
+
+  test "tooltip shows for Map Key button on a layer without a key provided" do
+    visit "/#layers=Y" # assumes that CyclOSM layer has no map key
+
+    assert_no_selector ".tooltip"
+    button = find ".control-key .control-button"
+    button.hover
+    tooltip = find ".tooltip"
+    tooltip.assert_text "Map Key"
+    tooltip.assert_text "not available"
+  end
 end
