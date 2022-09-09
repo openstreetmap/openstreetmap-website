dbcf25697613cdb2641f991759c75d890254db2a
Anton Khorev <tony29@yandex.ru>
2022-09-09 22:58:26 +0300

    Use close button instead of geolink in sidebar content

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -406,4 +406,8 @@ $(document).ready(function () {
       e.preventDefault();
     }
   });
+
+  $(document).on("click", "#sidebar_content .btn-close", function () {
+    OSM.router.route("/" + OSM.formatHash(map));
+  });
 });
