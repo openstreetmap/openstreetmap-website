ea30d2d9880e4e04d07549cdbd2524d71a0575c2
Brian DeRocher <brian@derocher.org>
2022-09-09 19:15:57 -0400

    Don't pollute the global namespace.
    0203

--- app/assets/javascripts/microcosms.js
+++ app/assets/javascripts/microcosms.js
@@ -1,15 +1,15 @@
 /*global showMap,formMapInput*/
 
 
-function init_microcosm_form() {
-  formMapInput("microcosm_map_form", "microcosm");
-}
-
-function init_microcosm_show() {
-  showMap("microcosm_map");
-}
-
 $(document).ready(function () {
+  function init_microcosm_form() {
+    formMapInput("microcosm_map_form", "microcosm");
+  }
+
+  function init_microcosm_show() {
+    showMap("microcosm_map");
+  }
+
   if ($("#microcosm_map_form").length) {
     init_microcosm_form();
   } else if ($("#microcosm_map").length) {
