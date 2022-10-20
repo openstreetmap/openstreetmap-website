0a5136fc55d219428deb37022574454c3c7e69e9
Anton Khorev <tony29@yandex.ru>
2022-10-20 15:29:57 +0300

    Use $.ajax instead of iframe to make remote control requests

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -255,20 +255,10 @@ $(document).ready(function () {
     });
 
     function sendRemoteEditCommand(url, callback) {
-      var iframe = $("<iframe>");
-      var timeoutId = setTimeout(function () {
-        alert(I18n.t("site.index.remote_failed"));
-        iframe.remove();
-      }, 5000);
-
-      iframe
-        .hide()
-        .appendTo("body")
-        .attr("src", url)
-        .on("load", function () {
-          clearTimeout(timeoutId);
-          iframe.remove();
-          if (callback) callback();
+      $.ajax(url, { timeout: 5000 })
+        .done(callback)
+        .fail(function () {
+          alert(I18n.t("site.index.remote_failed"));
         });
     }
 
