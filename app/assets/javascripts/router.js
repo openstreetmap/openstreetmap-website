593f5540564f1db4f9cb75841ec8eca02fb48b78
Anton Khorev <tony29@yandex.ru>
2022-10-28 15:42:52 +0300

    Introduce "reload" js controller method

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -18,6 +18,10 @@
        as arguments the URL path plus any matching arguments for placeholders
        in the path template.
 
+     * The `reload` method, when defined, is called by the router when a page
+       reload is requested by the user. When not defined, the `load` method is
+       called instead.
+
      * The `pushstate` method is called when a page which matches the route's path
        template is loaded via pushState. It is passed the same arguments as `load`.
 
@@ -79,6 +83,10 @@ OSM.Router = function (map, rts) {
       params = params.concat(Array.prototype.slice.call(arguments, 2));
 
       return (controller[action] || $.noop).apply(controller, params);
+    };
+
+    route.has = function (action) {
+      return action in controller;
     };
 
     return route;
@@ -179,7 +187,18 @@ OSM.Router = function (map, rts) {
   };
 
   router.load = function () {
-    var loadState = currentRoute.run("load", currentPath);
+    var method = "load";
+
+    if (currentRoute.has("reload") && window.performance) {
+      var wasReloaded = window.performance.getEntriesByType("navigation").some(function (entry) {
+        return entry.type === "reload";
+      });
+      if (wasReloaded) {
+        method = "reload";
+      }
+    }
+
+    var loadState = currentRoute.run(method, currentPath);
     router.stateChange(loadState || {});
   };
 
