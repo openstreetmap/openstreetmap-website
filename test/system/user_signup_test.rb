ace8e9a9c376a7bc1df4a89733f04386a1e5f333
Andy Allan <git@gravitystorm.co.uk>
2022-12-21 15:39:23 +0000

    Fix redirection to wiki when terms are declined
    Adds missing test to pick this up.
    
    Fixes #3826.

--- test/system/user_signup_test.rb
+++ test/system/user_signup_test.rb
@@ -8,4 +8,22 @@ class UserSignupTest < ApplicationSystemTestCase
 
     assert_content "Confirm Password"
   end
+
+  test "externally redirect when contributor terms declined" do
+    user = build(:user)
+
+    visit root_path
+    click_on "Sign Up"
+    fill_in "Email", :with => user.email
+    fill_in "Email Confirmation", :with => user.email
+    fill_in "Display Name", :with => user.display_name
+    fill_in "Password", :with => "testtest"
+    fill_in "Confirm Password", :with => "testtest"
+    click_button "Sign Up"
+
+    assert_content "Contributor terms"
+    click_on "Cancel"
+
+    assert_current_path "https://wiki.openstreetmap.org/wiki/Contributor_Terms_Declined"
+  end
 end
