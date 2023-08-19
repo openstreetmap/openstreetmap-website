3f6e344e6ad6f8e75cd0be3d02492cfc322893e8
Anton Khorev <tony29@yandex.ru>
2023-08-19 05:40:34 +0300

    Expose note query limit values in api capabilities

--- test/controllers/api/capabilities_controller_test.rb
+++ test/controllers/api/capabilities_controller_test.rb
@@ -37,6 +37,9 @@ module Api
                         "[default_query_limit='#{Settings.default_changeset_query_limit}']" \
                         "[maximum_query_limit='#{Settings.max_changeset_query_limit}']", :count => 1
           assert_select "relationmembers[maximum='#{Settings.max_number_of_relation_members}']", :count => 1
+          assert_select "notes" \
+                        "[default_query_limit='#{Settings.default_note_query_limit}']" \
+                        "[maximum_query_limit='#{Settings.max_note_query_limit}']", :count => 1
           assert_select "status[database='online']", :count => 1
           assert_select "status[api='online']", :count => 1
           assert_select "status[gpx='online']", :count => 1
@@ -61,6 +64,8 @@ module Api
       assert_equal Settings.default_changeset_query_limit, js["api"]["changesets"]["default_query_limit"]
       assert_equal Settings.max_changeset_query_limit, js["api"]["changesets"]["maximum_query_limit"]
       assert_equal Settings.max_number_of_relation_members, js["api"]["relationmembers"]["maximum"]
+      assert_equal Settings.default_note_query_limit, js["api"]["notes"]["default_query_limit"]
+      assert_equal Settings.max_note_query_limit, js["api"]["notes"]["maximum_query_limit"]
       assert_equal "online", js["api"]["status"]["database"]
       assert_equal "online", js["api"]["status"]["api"]
       assert_equal "online", js["api"]["status"]["gpx"]
