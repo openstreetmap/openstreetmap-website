cd0d0d086f74de58a59df8087d0050d909315ba0
Anton Khorev <tony29@yandex.ru>
2023-09-14 17:30:35 +0300

    Add changeset comments controller index test

--- test/controllers/changeset_comments_controller_test.rb
+++ test/controllers/changeset_comments_controller_test.rb
@@ -9,4 +9,29 @@ class ChangesetCommentsControllerTest < ActionDispatch::IntegrationTest
       { :controller => "changeset_comments", :action => "index", :display_name => "name" }
     )
   end
+
+  ##
+  # test comments webpage
+  def test_index
+    user = create(:user)
+    other_user = create(:user)
+    changeset = create(:changeset, :closed)
+    create_list(:changeset_comment, 3, :changeset => changeset, :author => user)
+    create_list(:changeset_comment, 2, :changeset => changeset, :author => other_user)
+
+    get changeset_comments_path(user)
+    assert_response :success
+    assert_select "table.table-striped tbody" do
+      assert_select "tr", :count => 3
+    end
+
+    create(:changeset_comment, :changeset => changeset, :author => user)
+    create(:changeset_comment, :changeset => changeset, :author => user, :visible => false)
+
+    get changeset_comments_path(user)
+    assert_response :success
+    assert_select "table.table-striped tbody" do
+      assert_select "tr", :count => 4
+    end
+  end
 end
