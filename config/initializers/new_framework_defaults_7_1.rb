4c6a56d002964c0f177f66b135a9adc9a3e7dd05
Tom Hughes <tom@compton.nu>
2023-10-27 17:42:23 +0100

    Commit transactions exited by return, break or throw

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -170,7 +170,7 @@ Rails.application.config.active_record.run_after_transaction_callbacks_in_order_
 
 # Whether a `transaction` block is committed or rolled back when exited via `return`, `break` or `throw`.
 #
-# Rails.application.config.active_record.commit_transaction_on_non_local_return = true
+Rails.application.config.active_record.commit_transaction_on_non_local_return = true
 
 # Controls when to generate a value for <tt>has_secure_token</tt> declarations.
 #
