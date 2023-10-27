39e66c69b75a99c42aab3267bee0f23f938fad3a
Tom Hughes <tom@compton.nu>
2023-10-27 17:31:23 +0100

    Stop parameter hashes comparing equal to normal hashes

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -27,7 +27,7 @@ Rails.application.config.action_dispatch.default_headers = {
 
 # Do not treat an `ActionController::Parameters` instance
 # as equal to an equivalent `Hash` by default.
-# Rails.application.config.action_controller.allow_deprecated_parameters_hash_equality = false
+Rails.application.config.action_controller.allow_deprecated_parameters_hash_equality = false
 
 # Active Record Encryption now uses SHA-256 as its hash digest algorithm. Important: If you have
 # data encrypted with previous Rails versions, there are two scenarios to consider:
