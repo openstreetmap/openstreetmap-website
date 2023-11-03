5d230ac7aedffb921590fdf4a38d4396ff8c0f27
Tom Hughes <tom@compton.nu>
2023-11-03 19:30:53 +0000

    Discard jobs that fail to deserialize

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class ApplicationJob < ActiveJob::Base
+class MailDeliveryJob < ActionMailer::MailDeliveryJob
   discard_on ActiveJob::DeserializationError
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  self.delivery_job = ActionMailer::MailDeliveryJob
+  self.delivery_job = MailDeliveryJob
 end
