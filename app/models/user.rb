aaed886890216b0e45a92b49fec7aad49e281e74
Tom Hughes <tom@compton.nu>
2023-11-16 00:54:56 +0000

    Hash passwords as soon as a new user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -115,6 +115,7 @@ class User < ApplicationRecord
 
   alias_attribute :created_at, :creation_time
 
+  after_initialize :encrypt_password
   before_save :encrypt_password
   before_save :update_tile
   after_save :spam_check
