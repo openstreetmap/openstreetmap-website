d9b9e13be14ac048bdbb501ac60fc1f0f02658e6
Anton Khorev <tony29@yandex.ru>
2023-12-08 13:21:57 +0300

    Allow to omit max_zoom if next map key entry has min_zoom

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -49,6 +49,11 @@ class SiteController < ApplicationController
   def key
     expires_in 7.days, :public => true
     @key = YAML.load_file(Rails.root.join("config/key.yml"))
+    @key.each_value do |layer_data|
+      layer_data.each_cons(2) do |entry, next_entry|
+        entry["max_zoom"] = next_entry["min_zoom"] - 1 if entry["name"] == next_entry["name"] && !entry["max_zoom"] && next_entry["min_zoom"]
+      end
+    end
     render :layout => false
   end
 
