f92183e52043a4bfa4e96392a2b7d767db1690d3
Anton Khorev <tony29@yandex.ru>
2023-12-19 15:41:10 +0300

    Use inline svg for solid map key images

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -1,8 +1,7 @@
 module SvgHelper
-  def solid_svg_image_tag(width, height, fill, **options)
-    svg = "<svg xmlns='http://www.w3.org/2000/svg' width='#{width}' height='#{height}'>" \
-          "<rect width='100%' height='100%' fill='#{fill}' />" \
-          "</svg>"
-    image_tag "data:image/svg+xml,#{u(svg)}", **options
+  def solid_svg_tag(width, height, fill, **options)
+    tag.svg :width => width, :height => height, **options do
+      tag.rect :width => "100%", :height => "100%", :fill => fill
+    end
   end
 end
