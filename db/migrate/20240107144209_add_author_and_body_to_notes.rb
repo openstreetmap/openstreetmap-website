9f1cf0512b458bbee17b857500b89576b1769fd4
Gregory Igelmund <github.com@grekko.de>
2024-02-04 14:41:43 +0100

    20240107144209_add_author_and_body_to_notes.rb: Remove data migration SQL

--- db/migrate/20240107144209_add_author_and_body_to_notes.rb
+++ db/migrate/20240107144209_add_author_and_body_to_notes.rb
@@ -6,26 +6,6 @@ class AddAuthorAndBodyToNotes < ActiveRecord::Migration[7.1]
         t.column :author_ip, :inet, :null => true
         t.column :body, :text, :null => true
       end
-
-      # TODO: Should this happen in a migration or within e.g. a rake task in
-      # which we can perform some sanity checks and which runs in the background.
-      # reversible do |dir|
-      #   dir.up do
-      #     execute <<-SQL.squish
-      #     UPDATE
-      #       notes
-      #     SET
-      #       body = c.body,
-      #       author_id = c.author_id,
-      #       author_ip = c.author_ip
-      #     FROM
-      #       note_comments c
-      #     WHERE
-      #       notes.id = c.note_id
-      #       AND c.event = 'opened';
-      #     SQL
-      #   end
-      # end
     end
 
     add_foreign_key :notes, :users, :column => :author_id, :validate => false
