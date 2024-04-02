90ee2dc59fedf7bab191b65fd0510f8f4625a331
Anton Khorev <tony29@yandex.ru>
2024-04-02 12:48:30 +0300

    Add generic api elements controller for multi fetches

---
+++ app/controllers/api/elements_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  class ElementsController < ApiController
+    private
+
+    def index_for_models(current_model)
+      type_plural = current_model.model_name.plural
+
+      raise OSM::APIBadUserInput, "The parameter #{type_plural} is required, and must be of the form #{type_plural}=id[,id[,id...]]" unless params[type_plural]
+
+      ids = params[type_plural].split(",").collect(&:to_i)
+
+      raise OSM::APIBadUserInput, "No #{type_plural} were given to search for" if ids.empty?
+
+      instance_variable_set :"@#{type_plural}", current_model.find(ids)
+
+      # Render the result
+      respond_to do |format|
+        format.xml
+        format.json
+      end
+    end
+  end
+end
