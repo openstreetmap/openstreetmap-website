5b5efc8da523cc6e000d3ac9803f6f6e787d9496
Anton Khorev <tony29@yandex.ru>
2024-04-04 17:27:05 +0300

    Keep timeouts only on map view db-reading pages and feeds

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -6,7 +6,6 @@ class BrowseController < ApplicationController
   before_action -> { check_database_readable(:need_api => true) }
   before_action :require_oauth
   before_action :update_totp, :only => [:query]
-  around_action :web_timeout
   authorize_resource :class => false
 
   def query; end
