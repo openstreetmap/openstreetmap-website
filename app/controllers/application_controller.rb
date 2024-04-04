8028f7441abbd5e329b23919545ccbf066b66d1f
Anton Khorev <tony29@yandex.ru>
2024-04-04 16:37:02 +0300

    Force map layout in web timeouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -211,14 +211,20 @@ class ApplicationController < ActionController::Base
 
     if e.is_a?(Timeout::Error) ||
        (e.is_a?(ActiveRecord::StatementInvalid) && e.message.include?("execution expired"))
-      ActiveRecord::Base.connection.raw_connection.cancel
-      render :action => "timeout"
+      respond_to_timeout
     else
       raise
     end
   rescue Timeout::Error
+    respond_to_timeout
+  end
+
+  def respond_to_timeout
     ActiveRecord::Base.connection.raw_connection.cancel
-    render :action => "timeout"
+    respond_to do |format|
+      format.html { render :action => "timeout", :layout => map_layout }
+      format.all { render :action => "timeout" }
+    end
   end
 
   ##
