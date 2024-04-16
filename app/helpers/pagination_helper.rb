c2b72e39108078ba1869de16b24bfda6a0d67d6a
Anton Khorev <tony29@yandex.ru>
2024-04-17 01:24:37 +0300

    Introduce pagination item helper

---
+++ app/helpers/pagination_helper.rb
@@ -0,0 +1,13 @@
+module PaginationHelper
+  def pagination_item(params, &block)
+    link_class = "page-link icon-link text-center"
+    page_link_content = capture(&block)
+    if params
+      page_link = link_to page_link_content, params, :class => link_class
+      tag.li page_link, :class => "page-item d-flex"
+    else
+      page_link = tag.span page_link_content, :class => link_class
+      tag.li page_link, :class => "page-item d-flex disabled"
+    end
+  end
+end
