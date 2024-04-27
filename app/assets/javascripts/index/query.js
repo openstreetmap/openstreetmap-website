63aaf045ac868675155c76468c9286eaaa3acd83
Anton Khorev <tony29@yandex.ru>
2024-04-27 06:57:20 +0300

    Remove action item class from non-action query results

--- app/assets/javascripts/index/query.js
+++ app/assets/javascripts/index/query.js
@@ -230,14 +230,14 @@ OSM.Query = function (map) {
 
         if (results.remark) {
           $("<li>")
-            .addClass("query-result list-group-item list-group-item-action")
+            .addClass("list-group-item")
             .text(I18n.t("javascripts.query.error", { server: url, error: results.remark }))
             .appendTo($ul);
         }
 
         if ($ul.find("li").length === 0) {
           $("<li>")
-            .addClass("query-result list-group-item list-group-item-action")
+            .addClass("list-group-item")
             .text(I18n.t("javascripts.query.nothing_found"))
             .appendTo($ul);
         }
@@ -246,7 +246,7 @@ OSM.Query = function (map) {
         $section.find(".loader").hide();
 
         $("<li>")
-          .addClass("query-result list-group-item list-group-item-action")
+          .addClass("list-group-item")
           .text(I18n.t("javascripts.query." + status, { server: url, error: error }))
           .appendTo($ul);
       }
