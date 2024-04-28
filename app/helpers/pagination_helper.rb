731219066451c486cefd5d91307d3536bc6b0fea
Anton Khorev <tony29@yandex.ru>
2024-04-28 04:02:59 +0300

    Add tooltips, make newest/oldest buttons tooltip only

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -1,9 +1,9 @@
 module PaginationHelper
-  def pagination_item(params, &block)
+  def pagination_item(params, title, &block)
     link_class = "page-link icon-link text-center"
     page_link_content = capture(&block)
     if params
-      page_link = link_to page_link_content, params, :class => link_class
+      page_link = link_to page_link_content, params, :class => link_class, :title => title
       tag.li page_link, :class => "page-item d-flex"
     else
       page_link = tag.span page_link_content, :class => link_class
