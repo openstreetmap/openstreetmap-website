15e1459f25673ec7b59392cc21a7533e0f15f9d7
Anton Khorev <tony29@yandex.ru>
2024-05-01 15:02:59 +0300

    Link to previous/next nonempty user's changeset on changeset pages

--- app/controllers/changesets_controller.rb
+++ app/controllers/changesets_controller.rb
@@ -84,8 +84,9 @@ class ChangesetsController < ApplicationController
     @way_pages, @ways = paginate(:old_ways, :conditions => { :changeset_id => @changeset.id }, :per_page => 20, :parameter => "way_page")
     @relation_pages, @relations = paginate(:old_relations, :conditions => { :changeset_id => @changeset.id }, :per_page => 20, :parameter => "relation_page")
     if @changeset.user.active? && @changeset.user.data_public?
-      @next_by_user = @changeset.user.changesets.where("id > ?", @changeset.id).reorder(:id => :asc).first
-      @prev_by_user = @changeset.user.changesets.where("id < ?", @changeset.id).reorder(:id => :desc).first
+      changesets = conditions_nonempty(@changeset.user.changesets)
+      @next_by_user = changesets.where("id > ?", @changeset.id).reorder(:id => :asc).first
+      @prev_by_user = changesets.where("id < ?", @changeset.id).reorder(:id => :desc).first
     end
     render :layout => map_layout
   rescue ActiveRecord::RecordNotFound
