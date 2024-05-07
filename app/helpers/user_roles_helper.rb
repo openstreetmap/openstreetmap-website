12d797b3513bba475a8939eda9fa202f28bc5306
Anton Khorev <tony29@yandex.ru>
2024-05-07 15:23:38 +0300

    Use inline svgs for moderator scope icons

--- app/helpers/user_roles_helper.rb
+++ app/helpers/user_roles_helper.rb
@@ -21,7 +21,7 @@ module UserRolesHelper
     end
   end
 
-  def role_icon_svg_tag(role, blank, title)
+  def role_icon_svg_tag(role, blank, title, **options)
     role_colors = {
       "administrator" => "#f69e42",
       "moderator" => "#0606ff",
@@ -30,7 +30,7 @@ module UserRolesHelper
     color = role_colors[role] || "currentColor"
 
     path_data = "M 10,2 8.125,8 2,8 6.96875,11.71875 5,18 10,14 15,18 13.03125,11.71875 18,8 11.875,8 10,2 z"
-    tag.svg(:width => 20, :height => 20) do
+    tag.svg(:width => 20, :height => 20, **options) do
       concat tag.title(title)
       concat tag.path(:d => path_data, :fill => color, :stroke => color, "stroke-width" => 2, "stroke-linejoin" => "round")
       concat tag.path(:d => path_data, :fill => "#fff") if blank
