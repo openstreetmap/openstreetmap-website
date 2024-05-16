64af2816a3f788c360434a73a234d41ef4c9f66e
Tom Hughes <tom@compton.nu>
2024-05-16 17:48:48 +0100

    Treat the body as UTF-8 for user_preferences#update

--- app/controllers/api/user_preferences_controller.rb
+++ app/controllers/api/user_preferences_controller.rb
@@ -66,7 +66,7 @@ module Api
         pref.k = params[:preference_key]
       end
 
-      pref.v = request.raw_post.chomp
+      pref.v = request.raw_post.chomp.force_encoding("UTF-8")
       pref.save!
 
       render :plain => ""
