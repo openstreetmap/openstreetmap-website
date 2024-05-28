c8757cf1d00836a34e8aa3e1244b3a0694914dc2
Anton Khorev <tony29@yandex.ru>
2024-05-28 06:49:04 +0300

    Use fixed small/compact nav breakpoint width

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -90,13 +90,13 @@ $(document).ready(function () {
   // See https://turbo.hotwired.dev/reference/drive#turbo.session.drive
   Turbo.session.drive = false;
 
-  var headerWidth = 0,
-      compactWidth = 0;
+  let headerWidth = 0;
+  const breakpointWidth = 768;
 
   function updateHeader() {
     var windowWidth = $(window).width();
 
-    if (windowWidth < compactWidth) {
+    if (windowWidth < breakpointWidth) {
       $("body").addClass("small-nav");
       expandSecondaryMenu();
     } else if (windowWidth < headerWidth) {
@@ -140,12 +140,6 @@ $(document).ready(function () {
   setTimeout(function () {
     $("header").children(":visible").each(function (i, e) {
       headerWidth += $(e).outerWidth();
-    });
-
-    collapseSecondaryMenu();
-
-    $("header").children(":visible").each(function (i, e) {
-      compactWidth += $(e).outerWidth();
     });
 
     $("header").removeClass("text-nowrap");
