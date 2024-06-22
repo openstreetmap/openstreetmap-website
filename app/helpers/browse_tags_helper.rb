06820acc4b3c818ad0e5d282490ac8413d966151
Anton Khorev <tony29@yandex.ru>
2024-06-22 21:48:42 +0300

    Discard fragments from wikimedia_commons tag values

--- app/helpers/browse_tags_helper.rb
+++ app/helpers/browse_tags_helper.rb
@@ -116,7 +116,7 @@ module BrowseTagsHelper
   end
 
   def wikimedia_commons_link(key, value)
-    if key == "wikimedia_commons" && value =~ /^(file|category):(.+)$/i
+    if key == "wikimedia_commons" && value =~ /^(file|category):([^#]+)/i
       namespace = Regexp.last_match(1)
       title = Regexp.last_match(2)
       return {
