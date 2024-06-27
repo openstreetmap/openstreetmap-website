81f2735fa0e15250e8411d95e720a3ab618a81c5
Anton Khorev <tony29@yandex.ru>
2024-06-27 03:26:35 +0300

    Show/hide marker in account home js controller

--- app/assets/javascripts/index/home.js
+++ app/assets/javascripts/index/home.js
@@ -1,8 +1,28 @@
 OSM.Home = function (map) {
+  var marker;
+
+  function clearMarker() {
+    if (marker) map.removeLayer(marker);
+    marker = null;
+  }
+
   var page = {};
 
   page.pushstate = page.popstate = page.load = function () {
     map.setSidebarOverlaid(true);
+    clearMarker();
+
+    OSM.router.withoutMoveListener(function () {
+      map.setView(OSM.home, 15, { reset: true });
+    });
+    marker = L.marker(OSM.home, {
+      icon: OSM.getUserIcon(),
+      title: I18n.t("javascripts.home.marker_title")
+    }).addTo(map);
+  };
+
+  page.unload = function () {
+    clearMarker();
   };
 
   return page;
