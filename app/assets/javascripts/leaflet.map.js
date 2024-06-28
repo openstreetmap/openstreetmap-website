cc03c277933c9cc472dcfad383a2fa9581df60aa
Anton Khorev <tony29@yandex.ru>
2024-06-28 04:04:55 +0300

    Refactor making map object layer

--- app/assets/javascripts/leaflet.map.js
+++ app/assets/javascripts/leaflet.map.js
@@ -272,21 +272,6 @@ L.OSM.Map = L.Map.extend({
   },
 
   addObject: function (object, callback) {
-    var objectStyle = {
-      color: "#FF6200",
-      weight: 4,
-      opacity: 1,
-      fillOpacity: 0.5
-    };
-
-    var changesetStyle = {
-      weight: 4,
-      color: "#FF9500",
-      opacity: 1,
-      fillOpacity: 0,
-      interactive: false
-    };
-
     var haloStyle = {
       weight: 2.5,
       radius: 20,
@@ -322,35 +307,55 @@ L.OSM.Map = L.Map.extend({
         dataType: "xml",
         success: function (xml) {
           map._object = object;
-
-          map._objectLayer = new L.OSM.DataLayer(null, {
-            styles: {
-              node: objectStyle,
-              way: objectStyle,
-              area: objectStyle,
-              changeset: changesetStyle
-            }
-          });
-
-          map._objectLayer.interestingNode = function (node, ways, relations) {
-            if (object.type === "node") {
-              return true;
-            } else if (object.type === "relation") {
-              for (var i = 0; i < relations.length; i++) {
-                if (relations[i].members.indexOf(node) !== -1) return true;
-              }
-            } else {
-              return false;
-            }
-          };
-
-          map._objectLayer.addData(xml);
-          map._objectLayer.addTo(map);
+          map._objectLayer = map.makeObjectLayer(object, xml);
 
           if (callback) callback(map._objectLayer.getBounds());
         }
       });
     }
+  },
+
+  makeObjectLayer: function (object, features) {
+    var objectStyle = {
+      color: "#FF6200",
+      weight: 4,
+      opacity: 1,
+      fillOpacity: 0.5
+    };
+
+    var changesetStyle = {
+      weight: 4,
+      color: "#FF9500",
+      opacity: 1,
+      fillOpacity: 0,
+      interactive: false
+    };
+
+    var objectLayer = new L.OSM.DataLayer(null, {
+      styles: {
+        node: objectStyle,
+        way: objectStyle,
+        area: objectStyle,
+        changeset: changesetStyle
+      }
+    });
+
+    objectLayer.interestingNode = function (node, ways, relations) {
+      if (object.type === "node") {
+        return true;
+      } else if (object.type === "relation") {
+        for (var i = 0; i < relations.length; i++) {
+          if (relations[i].members.indexOf(node) !== -1) return true;
+        }
+      } else {
+        return false;
+      }
+    };
+
+    objectLayer.addData(features);
+    objectLayer.addTo(this);
+
+    return objectLayer;
   },
 
   removeObject: function () {
