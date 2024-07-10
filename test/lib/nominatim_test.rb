63f0b9257d62818d435ef6510340b9681efe547b
Tom Hughes <tom@compton.nu>
2024-07-10 19:17:09 +0100

    Don't try and change the HTTP client user agent
    Because we only create the client once we can't override the
    server URL for a test to change the user agent as we don't know
    which test will create it.

--- test/lib/nominatim_test.rb
+++ test/lib/nominatim_test.rb
@@ -5,27 +5,25 @@ class NominatimTest < ActiveSupport::TestCase
     stub_request(:get, %r{^https://nominatim\.example\.com/reverse\?})
       .to_return(:body => "<reversegeocode><result>Target location</result></reversegeocode>")
 
-    with_settings(:nominatim_url => "https://nominatim.example.com/",
-                  :server_url => "osm-website.example.com") do
+    with_settings(:nominatim_url => "https://nominatim.example.com/") do
       location = Nominatim.describe_location(60, 30, 10, "en")
       assert_equal "Target location", location
     end
 
     assert_requested :get, "https://nominatim.example.com/reverse?lat=60&lon=30&zoom=10&accept-language=en",
-                     :headers => { "User-Agent" => "osm-website.example.com" }
+                     :headers => { "User-Agent" => Settings.server_url }
   end
 
   def test_describe_location_no_result
     stub_request(:get, %r{^https://nominatim\.example\.com/reverse\?})
       .to_return(:body => "<reversegeocode><error>Unable to geocode</error></reversegeocode>")
 
-    with_settings(:nominatim_url => "https://nominatim.example.com/",
-                  :server_url => "osm-website.example.com") do
+    with_settings(:nominatim_url => "https://nominatim.example.com/") do
       location = Nominatim.describe_location(1, 2, 14, "en")
       assert_equal "1.000, 2.000", location
     end
 
     assert_requested :get, "https://nominatim.example.com/reverse?lat=1&lon=2&zoom=14&accept-language=en",
-                     :headers => { "User-Agent" => "osm-website.example.com" }
+                     :headers => { "User-Agent" => Settings.server_url }
   end
 end
