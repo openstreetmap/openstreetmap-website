72177a3f20d31e081372f5174b72354192e40a3b
mmd-osm <mmd.osm@gmail.com>
2024-07-29 20:39:00 +0200

    Trace image with fixed size

--- app/helpers/trace_helper.rb
+++ app/helpers/trace_helper.rb
@@ -2,4 +2,12 @@ module TraceHelper
   def link_to_tag(tag)
     link_to(tag, :tag => tag, :page => nil)
   end
+
+  def trace_icon(trace, options = {})
+    options[:class] ||= "trace_image"
+    options[:alt] ||= ""
+
+    image_tag trace_icon_path(trace.user, trace),
+              options.merge(:size => 50)
+  end
 end
