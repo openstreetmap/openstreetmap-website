9969bc5a24eaeaeb8614d555c215310fc811ca0d
Martin Raifer <martin@raifer.tech>
2024-08-21 11:26:40 +0200

    Update to iD v2.30.1

--- app/assets/javascripts/id.js
+++ app/assets/javascripts/id.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
       parent.updateLinks(llz, zoom);
 
       // Manually resolve URL to avoid iframe JS context weirdness.
-      // http://bl.ocks.org/jfirebaugh/5439412
+      // https://gist.github.com/jfirebaugh/5439412
       var hash = parent.OSM.formatHash(llz);
       if (hash !== parent.location.hash) {
         parent.location.replace(parent.location.href.replace(/(#.*|$)/, hash));
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
       var data = parent.$(this).data();
 
       // 0ms timeout to avoid iframe JS context weirdness.
-      // http://bl.ocks.org/jfirebaugh/5439412
+      // https://gist.github.com/jfirebaugh/5439412
       setTimeout(function () {
         id.map().centerZoom(
           [data.lon, data.lat],
