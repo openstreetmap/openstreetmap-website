5047b5396739ee418ad6499b25c17a967b0b1a8e
Emin Kocan <kocanmn.dev@gmail.com>
2024-09-26 04:40:22 +0200

    Add filtering by status and date to user note page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,18 +13,52 @@ class NotesController < ApplicationController
   before_action :set_locale
   around_action :web_timeout
 
-  ##
-  # Display a list of notes by a specified user
+  ### Display a list of notes by a specified user
   def index
     param! :page, Integer, :min => 1
 
-    @params = params.permit(:display_name)
+    @params = params.permit(:display_name, :from, :to, :status, :sort_by, :sort_order)
     @title = t ".title", :user => @user.display_name
     @page = (params[:page] || 1).to_i
     @page_size = 10
+    @status = params[:status] || "open"
+
     @notes = @user.notes
     @notes = @notes.visible unless current_user&.moderator?
-    @notes = @notes.order("updated_at DESC, id").distinct.offset((@page - 1) * @page_size).limit(@page_size).preload(:comments => :author)
+
+    case params[:status]
+    when "open"
+      @notes = @notes.where(:status => "open")
+    when "closed"
+      @notes = @notes.where(:status => "closed")
+    end
+
+    # Filter by date range (from, to)
+    if params[:from].present?
+      from_date = begin
+        DateTime.parse(params[:from])
+      rescue StandardError
+        nil
+      end
+      @notes = @notes.where(:notes => { :created_at => from_date.. }) if from_date
+    end
+
+    if params[:to].present?
+      to_date = begin
+        DateTime.parse(params[:to])
+      rescue StandardError
+        nil
+      end
+      @notes = @notes.where(:notes => { :created_at => ..to_date }) if to_date
+    end
+
+    # Handle sorting
+    sort_by = params[:sort_by] || "updated_at"
+    sort_order = params[:sort_order] || "desc"
+    @notes = @notes.order("#{sort_by} #{sort_order}")
+
+    # Apply pagination and preload comments
+    @notes = @notes.distinct.offset((@page - 1) * @page_size).limit(@page_size).preload(:comments => :author)
 
     render :layout => "site"
   end
