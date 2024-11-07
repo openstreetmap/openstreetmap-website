71957f32106412f15b3009f5ea55045174d44a26
Anton Khorev <tony29@yandex.ru>
2024-11-07 14:42:50 +0300

    Add tags list to changeset tags page

--- test/controllers/changeset_tags_controller_test.rb
+++ test/controllers/changeset_tags_controller_test.rb
@@ -26,6 +26,42 @@ class ChangesetTagsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  def test_show_success_1_tag
+    changeset = create(:changeset)
+    create(:changeset_tag, :changeset => changeset, :k => "tested-tag-key", :v => "tested-tag-value")
+    moderator_user = create(:moderator_user)
+
+    session_for(moderator_user)
+
+    get changeset_tags_path(changeset)
+    assert_response :success
+
+    assert_dom ".content-body" do
+      assert_dom "tbody tr", :count => 1 do |rows|
+        check_tag_table_row rows[0], changeset, "tested-tag-key", "tested-tag-value"
+      end
+    end
+  end
+
+  def test_show_success_2_tags
+    changeset = create(:changeset)
+    create(:changeset_tag, :changeset => changeset, :k => "tested-1st-tag-key", :v => "tested-1st-tag-value")
+    create(:changeset_tag, :changeset => changeset, :k => "tested-2nd-tag-key", :v => "tested-2nd-tag-value")
+    moderator_user = create(:moderator_user)
+
+    session_for(moderator_user)
+
+    get changeset_tags_path(changeset)
+    assert_response :success
+
+    assert_dom ".content-body" do
+      assert_dom "tbody tr", :count => 2 do |rows|
+        check_tag_table_row rows[0], changeset, "tested-1st-tag-key", "tested-1st-tag-value"
+        check_tag_table_row rows[1], changeset, "tested-2nd-tag-key", "tested-2nd-tag-value"
+      end
+    end
+  end
+
   def test_show_fail_no_changeset
     moderator_user = create(:moderator_user)
 
@@ -50,5 +86,12 @@ class ChangesetTagsControllerTest < ActionDispatch::IntegrationTest
 
     get changeset_tags_path(changeset)
     assert_redirected_to :controller => :errors, :action => :forbidden
+  end
+
+  private
+
+  def check_tag_table_row(row, _changeset, key, value)
+    assert_dom row, "th", :text => key
+    assert_dom row, "td", :text => value
   end
 end
