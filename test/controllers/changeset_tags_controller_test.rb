4338f93a8e0362315ca1f1648ee81aaa4b4e900c
Anton Khorev <tony29@yandex.ru>
2024-11-07 14:42:50 +0300

    Add tags list to changeset tags page

--- test/controllers/changeset_tags_controller_test.rb
+++ test/controllers/changeset_tags_controller_test.rb
@@ -26,6 +26,45 @@ class ChangesetTagsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  def test_index_success_1_tag
+    changeset = create(:changeset)
+    create(:changeset_tag, :changeset => changeset, :k => "tested-tag-key", :v => "tested-tag-value")
+    moderator_user = create(:moderator_user)
+
+    session_for(moderator_user)
+
+    get changeset_tags_path(changeset)
+    assert_response :success
+
+    assert_dom ".content-body" do
+      assert_dom "tbody tr", :count => 1 do
+        assert_dom "th", :text => "tested-tag-key"
+        assert_dom "td", :text => "tested-tag-value"
+      end
+    end
+  end
+
+  def test_index_success_2_tags
+    changeset = create(:changeset)
+    create(:changeset_tag, :changeset => changeset, :k => "tested-1st-tag-key", :v => "tested-1st-tag-value")
+    create(:changeset_tag, :changeset => changeset, :k => "tested-2nd-tag-key", :v => "tested-2nd-tag-value")
+    moderator_user = create(:moderator_user)
+
+    session_for(moderator_user)
+
+    get changeset_tags_path(changeset)
+    assert_response :success
+
+    assert_dom ".content-body" do
+      assert_dom "tbody tr", :count => 2 do |rows|
+        assert_dom rows[0], "th", :text => "tested-1st-tag-key"
+        assert_dom rows[0], "td", :text => "tested-1st-tag-value"
+        assert_dom rows[1], "th", :text => "tested-2nd-tag-key"
+        assert_dom rows[1], "td", :text => "tested-2nd-tag-value"
+      end
+    end
+  end
+
   def test_index_fail_no_changeset
     moderator_user = create(:moderator_user)
 
