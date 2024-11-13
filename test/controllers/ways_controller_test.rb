af0dbc93a011e9b112188f82e45927cfceec2412
Anton Khorev <tony29@yandex.ru>
2024-11-13 18:54:00 +0300

    Show "node also part of ways" as nested lists on way pages

--- test/controllers/ways_controller_test.rb
+++ test/controllers/ways_controller_test.rb
@@ -47,4 +47,128 @@ class WaysControllerTest < ActionDispatch::IntegrationTest
     assert_dom "h2", "Timeout Error"
     assert_dom "p", /#{Regexp.quote("the way with the id #{way.id}")}/
   end
+
+  def test_show_nodes
+    way = create(:way_with_nodes, :nodes_count => 2)
+
+    get way_path(way, :xhr => true)
+    assert_response :success
+    assert_dom "ul:has(> li a[href='#{node_path way.nodes[0]}']):has(> li a[href='#{node_path way.nodes[0]}'])", :count => 1 do
+      assert_dom "> li", :count => 2 do |list_items|
+        assert_dom list_items[0], "a[href='#{node_path way.nodes[0]}']"
+        assert_dom list_items[1], "a[href='#{node_path way.nodes[1]}']"
+      end
+    end
+  end
+
+  ##
+  # shows another way that shares a node
+  #
+  # (b)--1--(a)--2--(c)
+  #
+  def test_show_nodes_also_part_of_way
+    way1 = create(:way)
+    way2 = create(:way)
+    node_a = create(:node)
+    node_b = create(:node)
+    node_c = create(:node)
+    create(:way_node, :way => way1, :node => node_a, :sequence_id => 1)
+    create(:way_node, :way => way1, :node => node_b, :sequence_id => 2)
+    create(:way_node, :way => way2, :node => node_a, :sequence_id => 1)
+    create(:way_node, :way => way2, :node => node_c, :sequence_id => 2)
+
+    get way_path(way1, :xhr => true)
+    assert_response :success
+    assert_dom "ul:has(> li a[href='#{node_path node_a}']):has(> li a[href='#{node_path node_b}'])", :count => 1 do
+      assert_dom "> li", :count => 2 do |list_items|
+        assert_dom list_items[0], "a[href='#{node_path node_a}']"
+        assert_dom list_items[1], "a[href='#{node_path node_b}']"
+        assert_dom list_items[1], "ul", :count => 0
+        assert_dom list_items[0], "ul", :count => 1 do
+          assert_dom "> li", :count => 1 do |sub_list_items|
+            assert_dom sub_list_items[0], "a[href='#{way_path way2}']"
+          end
+        end
+      end
+    end
+  end
+
+  ##
+  # node shared with a looped way shouldn't cause that way appear twice in the "part of ways" sublist
+  #
+  # (b)--1--(a)--2--(c)
+  #           \    /
+  #            \  /
+  #            (d)
+  #
+  def test_show_nodes_also_part_of_looped_way
+    way1 = create(:way)
+    way2 = create(:way)
+    node_a = create(:node)
+    node_b = create(:node)
+    node_c = create(:node)
+    node_d = create(:node)
+    create(:way_node, :way => way1, :node => node_a, :sequence_id => 1)
+    create(:way_node, :way => way1, :node => node_b, :sequence_id => 2)
+    create(:way_node, :way => way2, :node => node_a, :sequence_id => 1)
+    create(:way_node, :way => way2, :node => node_c, :sequence_id => 2)
+    create(:way_node, :way => way2, :node => node_d, :sequence_id => 3)
+    create(:way_node, :way => way2, :node => node_a, :sequence_id => 4)
+
+    get way_path(way1, :xhr => true)
+    assert_response :success
+    assert_dom "ul:has(> li a[href='#{node_path node_a}']):has(> li a[href='#{node_path node_b}'])", :count => 1 do
+      assert_dom "> li", :count => 2 do |list_items|
+        assert_dom list_items[0], "a[href='#{node_path node_a}']"
+        assert_dom list_items[1], "a[href='#{node_path node_b}']"
+        assert_dom list_items[1], "ul", :count => 0
+        assert_dom list_items[0], "ul", :count => 1 do
+          assert_dom "> li", :count => 1 do |sub_list_items|
+            assert_dom sub_list_items[0], "a[href='#{way_path way2}']"
+          end
+        end
+      end
+    end
+  end
+
+  ##
+  # shows two ways that share a node
+  #
+  # (b)--1--(a)--2--(c)
+  #           \
+  #            3
+  #             \
+  #             (d)
+  #
+  def test_show_nodes_also_part_of_2_ways
+    way1 = create(:way)
+    way2 = create(:way)
+    way3 = create(:way)
+    node_a = create(:node)
+    node_b = create(:node)
+    node_c = create(:node)
+    node_d = create(:node)
+    create(:way_node, :way => way1, :node => node_a, :sequence_id => 1)
+    create(:way_node, :way => way1, :node => node_b, :sequence_id => 2)
+    create(:way_node, :way => way2, :node => node_a, :sequence_id => 1)
+    create(:way_node, :way => way2, :node => node_c, :sequence_id => 2)
+    create(:way_node, :way => way3, :node => node_a, :sequence_id => 1)
+    create(:way_node, :way => way3, :node => node_d, :sequence_id => 2)
+
+    get way_path(way1, :xhr => true)
+    assert_response :success
+    assert_dom "ul:has(> li a[href='#{node_path node_a}']):has(> li a[href='#{node_path node_b}'])", :count => 1 do
+      assert_dom "> li", :count => 2 do |list_items|
+        assert_dom list_items[0], "a[href='#{node_path node_a}']"
+        assert_dom list_items[1], "a[href='#{node_path node_b}']"
+        assert_dom list_items[1], "ul", :count => 0
+        assert_dom list_items[0], "ul", :count => 1 do
+          assert_dom "> li", :count => 2 do |sub_list_items|
+            assert_dom sub_list_items[0], "a[href='#{way_path way2}']"
+            assert_dom sub_list_items[1], "a[href='#{way_path way3}']"
+          end
+        end
+      end
+    end
+  end
 end
