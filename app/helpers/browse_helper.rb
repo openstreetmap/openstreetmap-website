dc52cdc612b565658555feb3b12b921eb394f0cb
Anton Khorev <tony29@yandex.ru>
2024-11-27 17:37:54 +0300

    Adjust browse line/icon height/alignment for text-only zooming

--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -14,7 +14,7 @@ module BrowseHelper
 
     image_tag "browse/#{selected_icon_data[:filename]}",
               :size => 20,
-              :class => ["align-bottom object-fit-none", { "browse-icon-invertible" => selected_icon_data[:invert] }],
+              :class => ["align-bottom object-fit-none browse-icon", { "browse-icon-invertible" => selected_icon_data[:invert] }],
               :title => title
   end
 
@@ -27,7 +27,7 @@ module BrowseHelper
   end
 
   def element_list_item(type, object, &)
-    tag.li(tag.div(element_icon(type, object) + tag.div(&), :class => "d-flex gap-1"))
+    tag.li(tag.div(element_icon(type, object) + tag.div(:class => "align-self-center", &), :class => "d-flex gap-1"))
   end
 
   def element_list_item_with_strikethrough(type, object, &)
