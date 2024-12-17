e89a78fff858d5db07f6959a39dd9655dc46008e
Anton Khorev <tony29@yandex.ru>
2024-12-17 17:14:19 +0300

    Move api messages resources to api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -77,17 +77,17 @@ OpenStreetMap::Application.routes.draw do
         put "" => "user_preferences#update_all", :as => ""
       end
     end
+  end
 
-    resources :messages, :path => "user/messages", :constraints => { :id => /\d+/ }, :only => [:create, :show, :update, :destroy], :controller => "messages", :as => :api_messages do
+  namespace :api, :path => "api/0.6" do
+    resources :messages, :path => "user/messages", :constraints => { :id => /\d+/ }, :only => [:create, :show, :update, :destroy] do
       collection do
         get "inbox"
         get "outbox"
       end
     end
     post "/user/messages/:id" => "messages#update"
-  end
 
-  namespace :api, :path => "api/0.6" do
     resources :traces, :path => "gpx", :only => [:create, :show, :update, :destroy], :id => /\d+/ do
       scope :module => :traces do
         resource :data, :only => :show
