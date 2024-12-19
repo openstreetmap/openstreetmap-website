bb561d3ea9441b9a1c9032a80d751b640ddd2a23
Anton Khorev <tony29@yandex.ru>
2024-12-19 05:26:45 +0300

    Use within_sidebar in CreateNoteTest

--- test/system/create_note_test.rb
+++ test/system/create_note_test.rb
@@ -4,20 +4,24 @@ class CreateNoteTest < ApplicationSystemTestCase
   test "can create note" do
     visit new_note_path(:anchor => "map=18/0/0")
 
-    assert_button "Add Note", :disabled => true
+    within_sidebar do
+      assert_button "Add Note", :disabled => true
 
-    fill_in "text", :with => "Some newly added note description"
-    click_on "Add Note"
+      fill_in "text", :with => "Some newly added note description"
+      click_on "Add Note"
 
-    assert_content "Unresolved note ##{Note.last.id}"
-    assert_content "Some newly added note description"
+      assert_content "Unresolved note ##{Note.last.id}"
+      assert_content "Some newly added note description"
+    end
   end
 
   test "cannot create note when api is readonly" do
     with_settings(:status => "api_readonly") do
       visit new_note_path(:anchor => "map=18/0/0")
 
-      assert_no_button "Add Note", :disabled => true
+      within_sidebar do
+        assert_no_button "Add Note", :disabled => true
+      end
     end
   end
 end
