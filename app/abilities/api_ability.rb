cf365e499e260b35e96301ad1c9c19dc790e014b
Anton Khorev <tony29@yandex.ru>
2024-12-21 01:57:18 +0300

    Move write_api scope check to decrease number of ifs

--- app/abilities/api_ability.rb
+++ app/abilities/api_ability.rb
@@ -37,10 +37,10 @@ class ApiAbility
         can [:inbox, :outbox, :read, :update, :destroy], Message if scope?(token, :consume_messages)
         can :create, Message if scope?(token, :send_messages)
 
-        if user.terms_agreed?
-          can [:create, :update, :upload, :close, :subscribe, :unsubscribe], Changeset if scope?(token, :write_api)
-          can :create, ChangesetComment if scope?(token, :write_api)
-          can [:create, :update, :delete], [Node, Way, Relation] if scope?(token, :write_api)
+        if user.terms_agreed? && scope?(token, :write_api)
+          can [:create, :update, :upload, :close, :subscribe, :unsubscribe], Changeset
+          can :create, ChangesetComment
+          can [:create, :update, :delete], [Node, Way, Relation]
         end
 
         if user.moderator?
