00b3d9a5f272bf33fcd5cb4b363a71d9a0a12868
Anton Khorev <tony29@yandex.ru>
2025-01-01 05:31:05 +0300

    Skip blocks check when getting active blocks list

--- app/controllers/api/user_blocks/active_lists_controller.rb
+++ app/controllers/api/user_blocks/active_lists_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module UserBlocks
     class ActiveListsController < ApiController
+      before_action :disable_blocks_check
       before_action :authorize
 
       authorize_resource :class => :active_user_blocks_list
@@ -8,6 +9,12 @@ module Api
       before_action :set_request_formats
 
       def show; end
+
+      private
+
+      def disable_blocks_check
+        flash.now[:skip_blocks] = true
+      end
     end
   end
 end
