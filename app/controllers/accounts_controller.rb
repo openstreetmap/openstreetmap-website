b052e77b610cc6ebb550a1e6845ad1efd3e382c5
Tom Hughes <tom@compton.nu>
2025-01-01 12:11:22 +0000

    Replace count.zero? with empty?

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -31,7 +31,7 @@ class AccountsController < ApplicationController
        (params[:user][:auth_provider] == current_user.auth_provider &&
         params[:user][:auth_uid] == current_user.auth_uid)
       update_user(current_user, user_params)
-      if current_user.errors.count.zero?
+      if current_user.errors.empty?
         redirect_to edit_account_path
       else
         render :edit, :status => :unprocessable_entity
