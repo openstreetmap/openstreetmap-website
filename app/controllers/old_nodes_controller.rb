cb80bd3b966e1f780b7a1db1cf2d32716643caea
Anton Khorev <tony29@yandex.ru>
2025-01-04 17:00:43 +0300

    Add OldElementsController base class

--- app/controllers/old_nodes_controller.rb
+++ app/controllers/old_nodes_controller.rb
@@ -1,16 +1,4 @@
-class OldNodesController < ApplicationController
-  layout :map_layout
-
-  before_action :authorize_web
-  before_action :set_locale
-  before_action -> { check_database_readable(:need_api => true) }
-  before_action :require_oauth
-
-  authorize_resource
-
-  before_action :require_moderator_for_unredacted_history
-  around_action :web_timeout
-
+class OldNodesController < OldElementsController
   def index
     @type = "node"
     @feature = Node.preload(:node_tags, :old_nodes => [:old_tags, { :changeset => [:changeset_tags, :user] }]).find(params[:id])
@@ -24,11 +12,5 @@ class OldNodesController < ApplicationController
     @feature = OldNode.preload(:old_tags, :changeset => [:changeset_tags, :user]).find([params[:id], params[:version]])
   rescue ActiveRecord::RecordNotFound
     render :action => "not_found", :status => :not_found
-  end
-
-  private
-
-  def require_moderator_for_unredacted_history
-    deny_access(nil) if params[:show_redactions] && !current_user&.moderator?
   end
 end
--- app/controllers/old_relations_controller.rb
+++ app/controllers/old_relations_controller.rb
@@ -1,16 +1,4 @@
-class OldRelationsController < ApplicationController
-  layout :map_layout
-
-  before_action :authorize_web
-  before_action :set_locale
-  before_action -> { check_database_readable(:need_api => true) }
-  before_action :require_oauth
-
-  authorize_resource
-
-  before_action :require_moderator_for_unredacted_history
-  around_action :web_timeout
-
+class OldRelationsController < OldElementsController
   def index
     @type = "relation"
     @feature = Relation.preload(:relation_tags, :old_relations => [:old_tags, { :changeset => [:changeset_tags, :user], :old_members => :member }]).find(params[:id])
@@ -24,11 +12,5 @@ class OldRelationsController < ApplicationController
     @feature = OldRelation.preload(:old_tags, :changeset => [:changeset_tags, :user], :old_members => :member).find([params[:id], params[:version]])
   rescue ActiveRecord::RecordNotFound
     render :action => "not_found", :status => :not_found
-  end
-
-  private
-
-  def require_moderator_for_unredacted_history
-    deny_access(nil) if params[:show_redactions] && !current_user&.moderator?
   end
 end
