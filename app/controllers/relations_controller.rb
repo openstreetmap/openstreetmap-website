e74273d496b05c4c951c88195b76cdf505309425
Anton Khorev <tony29@yandex.ru>
2025-01-06 17:42:51 +0300

    Add ElementsController base class

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,15 +1,4 @@
-class RelationsController < ApplicationController
-  layout :map_layout
-
-  before_action :authorize_web
-  before_action :set_locale
-  before_action -> { check_database_readable(:need_api => true) }
-  before_action :require_oauth
-
-  authorize_resource
-
-  around_action :web_timeout
-
+class RelationsController < ElementsController
   def show
     @type = "relation"
     @feature = Relation.preload(:relation_tags, :containing_relation_members, :changeset => [:changeset_tags, :user], :relation_members => :member).find(params[:id])
--- app/controllers/ways_controller.rb
+++ app/controllers/ways_controller.rb
@@ -1,15 +1,4 @@
-class WaysController < ApplicationController
-  layout :map_layout
-
-  before_action :authorize_web
-  before_action :set_locale
-  before_action -> { check_database_readable(:need_api => true) }
-  before_action :require_oauth
-
-  authorize_resource
-
-  around_action :web_timeout
-
+class WaysController < ElementsController
   def show
     @type = "way"
     @feature = Way.preload(:way_tags, :containing_relation_members, :changeset => [:changeset_tags, :user], :nodes => [:node_tags, { :ways => :way_tags }]).find(params[:id])
