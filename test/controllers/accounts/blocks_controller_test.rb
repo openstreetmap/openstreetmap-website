d4e3323856eb62a356f896e5636ea2f9509bfcad
Anton Khorev <tony29@yandex.ru>
2025-01-20 06:37:05 +0300

    Add redirect from accounts block page to needs_view block

--- test/controllers/accounts/blocks_controller_test.rb
+++ test/controllers/accounts/blocks_controller_test.rb
@@ -23,5 +23,28 @@ module Accounts
       get account_blocks_path
       assert_response :success
     end
+
+    def test_index_with_unseen_block
+      user = create(:user)
+      session_for(user)
+      unseen_block = create(:user_block, :needs_view, :user => user)
+
+      get account_blocks_path
+      assert_redirected_to user_block_path(unseen_block)
+    end
+
+    def test_index_with_two_unseen_blocks
+      user = create(:user)
+      session_for(user)
+      unseen_block1 = create(:user_block, :needs_view, :user => user)
+      unseen_block2 = create(:user_block, :needs_view, :user => user)
+
+      get account_blocks_path
+      assert_redirected_to user_block_path(unseen_block1)
+      follow_redirect!
+
+      get account_blocks_path
+      assert_redirected_to user_block_path(unseen_block2)
+    end
   end
 end
