518936bdb6a406af4df829c3033819148ae6fd05
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2025-02-03 10:06:30 +0100

    Remove parseFloat calls and restructure

--- app/assets/javascripts/fixthemap.js
+++ app/assets/javascripts/fixthemap.js
@@ -2,11 +2,7 @@ $(document).ready(function () {
   var params = OSM.params();
 
   var url = "/note/new";
-  if (params.lat && params.lon) {
-    params.lat = parseFloat(params.lat);
-    params.lon = parseFloat(params.lon);
-    params.zoom = params.zoom || 17;
-    url += OSM.formatHash(params);
-  }
+  if (!params.zoom) params.zoom = 17;
+  if (params.lat && params.lon) url += OSM.formatHash(params);
   $(".icon.note").attr("href", url);
 });
