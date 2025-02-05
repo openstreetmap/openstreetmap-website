a4e6934d599e92d755ab1ff949ed468eea1a9bf0
Holger Jeromin <h.jeromin@beckhoff.com>
2025-02-05 17:50:40 +0100

    Fix all no-implicit-globals eslint errors

--- app/assets/javascripts/social_share_button.js
+++ app/assets/javascripts/social_share_button.js
@@ -1,15 +1,15 @@
-function openShareUrl(url, initialWidth = 640, initialHeight = 480) {
-  const width = Math.max(100, Math.min(screen.width, initialWidth));
-  const height = Math.max(100, Math.min(screen.height, initialHeight));
-
-  const left = screenLeft + ((outerWidth - width) / 2);
-  const top = screenTop + ((outerHeight - height) / 2);
-  const opts = `width=${width},height=${height},left=${left},top=${top},menubar=no,status=no,location=no`;
-
-  window.open(url, "popup", opts);
-}
-
 $(document).ready(function () {
+  function openShareUrl(url, initialWidth = 640, initialHeight = 480) {
+    const width = Math.max(100, Math.min(screen.width, initialWidth));
+    const height = Math.max(100, Math.min(screen.height, initialHeight));
+
+    const left = screenLeft + ((outerWidth - width) / 2);
+    const top = screenTop + ((outerHeight - height) / 2);
+    const opts = `width=${width},height=${height},left=${left},top=${top},menubar=no,status=no,location=no`;
+
+    window.open(url, "popup", opts);
+  }
+
   $(".ssb-icon").on("click", function (e) {
     const shareUrl = $(this).attr("href");
     if (!shareUrl.startsWith("mailto:")) {
@@ -18,4 +18,3 @@ $(document).ready(function () {
     }
   });
 });
-
