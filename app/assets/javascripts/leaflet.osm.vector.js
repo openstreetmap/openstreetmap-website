cc0297185f0ca64ce0b5ba06f2797b4d65bbc89b
Martin Ždila <m.zdila@gmail.com>
2025-02-10 13:47:11 +0100

    Add MapTiler OMT vector map

---
+++ app/assets/javascripts/leaflet.osm.vector.js
@@ -0,0 +1,37 @@
+//= require maplibre-gl
+//= require @maplibre/maplibre-gl-leaflet
+//= require @maptiler/maplibre-gl-omt-language
+
+maplibregl.setRTLTextPlugin(OSM.RTL_TEXT_PLUGIN, true);
+
+L.OSM.OpenMapTiles = L.MaplibreGL.extend({
+  initialize: function (options) {
+    L.MaplibreGL.prototype.initialize.call(this, {
+      maxZoom: 23,
+      style:
+        "https://api.maptiler.com/maps/openstreetmap/style.json?key=" +
+        options.apikey,
+      ...options
+    });
+  },
+  onAdd: function (map) {
+    L.MaplibreGL.prototype.onAdd.call(this, map);
+    const maplibreMap = this.getMaplibreMap();
+    const supportedLanguages = maplibregl.Map.prototype.supportedLanguages;
+    for (const preferredLanguage of OSM.preferred_languages) {
+      const normalizedPreferredLanguage = preferredLanguage
+        .toLowerCase()
+        .replace("-", "_");
+      const matchedLanguage = supportedLanguages.find(
+        (supported) => supported.toLowerCase() === normalizedPreferredLanguage
+      );
+      if (matchedLanguage) {
+        maplibreMap.setLanguage(matchedLanguage);
+        break;
+      }
+    }
+  },
+  onRemove: function (map) {
+    L.MaplibreGL.prototype.onRemove.call(this, map);
+  }
+});
