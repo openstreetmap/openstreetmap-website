0e84df58b8ef69a33c7a9f4823b3c1c74d9aad84
Anton Khorev <tony29@yandex.ru>
2025-02-11 04:30:35 +0300

    Add base old element redactions controller

---
+++ app/controllers/api/old_elements/redactions_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  module OldElements
+    class RedactionsController < ApiController
+      before_action :check_api_writable
+      before_action :authorize
+
+      authorize_resource :class => :element_version_redaction
+
+      before_action :lookup_old_element
+
+      def create
+        redaction_id = params["redaction"]
+        if redaction_id.nil?
+          # legacy unredact if no redaction ID was provided
+          destroy
+        else
+          redaction = Redaction.find(redaction_id.to_i)
+          @old_element.redact!(redaction)
+          head :ok
+        end
+      end
+
+      def destroy
+        @old_element.redact!(nil)
+        head :ok
+      end
+    end
+  end
+end
