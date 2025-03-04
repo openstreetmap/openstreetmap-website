047e972b2d448ade83382c0f824cb6e123b9bacb
Nenad Vujicic <nenadus@gmail.com>
2025-03-04 16:04:45 +0100

    Updates note_comments and notes factories
    Updates note_comments factory to create "commented" note comments by default. Improves notes factory to set note's description to "Default note's description" by default and to set first note's comment type to "opened" when used note_with_comments.

--- test/factories/notes.rb
+++ test/factories/notes.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :note do
     latitude { 1 * GeoRecord::SCALE }
     longitude { 1 * GeoRecord::SCALE }
-    # tile { QuadTile.tile_for_point(1,1) }
+    description { "Default note's description" }
 
     trait :closed do
       transient do
@@ -23,7 +23,8 @@ FactoryBot.define do
       end
 
       after(:create) do |note, evaluator|
-        create_list(:note_comment, evaluator.comments_count, :note => note)
+        create(:note_comment, :event => "opened", :note => note)
+        create_list(:note_comment, evaluator.comments_count - 1, :note => note)
       end
     end
   end
