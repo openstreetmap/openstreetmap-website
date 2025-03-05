07893ac5bbdbf3024131ad353514e9ee36e3c2b3
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2025-03-05 19:27:43 +0100

    Set fixthemap note link in template

--- app/assets/javascripts/fixthemap.js
+++ app/assets/javascripts/fixthemap.js
@@ -1,8 +1,8 @@
 $(function () {
   const params = new URLSearchParams(location.search);
 
-  let url = "/note/new";
   if (!params.has("zoom")) params.set("zoom", 17);
-  if (params.has("lat") && params.has("lon")) url += OSM.formatHash(params);
-  $(".icon.note").attr("href", url);
+  if (params.has("lat") && params.has("lon")) {
+    $("a.new-note")[0].href += OSM.formatHash(params);
+  }
 });
