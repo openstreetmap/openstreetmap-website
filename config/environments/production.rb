fc6f7f8338b7a8cd5c562d7f74b4b476b7144a08
Tom Hughes <tom@compton.nu>
2025-03-24 17:36:42 +0000

    Re-enable support for logging to a file in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,11 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [:request_id]
-  config.logger   = ActiveSupport::TaggedLogging.logger($stdout)
+  config.logger   = if Settings.key?(:log_path)
+                      ActiveSupport::TaggedLogging.logger(Settings.log_path)
+                    else
+                      ActiveSupport::TaggedLogging.logger($stdout)
+                    end
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
