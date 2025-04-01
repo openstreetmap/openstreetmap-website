7b2d020d37f0be749890001ee499faaf549f80b7
Anton Khorev <tony29@yandex.ru>
2025-04-01 04:10:47 +0300

    Convert zoom to number in OSM.formatHash

---
+++ test/system/fixthemap_test.rb
@@ -0,0 +1,11 @@
+require "application_system_test_case"
+
+class FixthemapTest < ApplicationSystemTestCase
+  test "should have 'create a note' link with correct map hash" do
+    visit fixthemap_path(:lat => 60, :lon => 30, :zoom => 10)
+
+    within_content_body do
+      assert_link "Add a note to the map", :href => %r{/note/new#map=10/60(\.\d+)?/30(\.\d+)?}
+    end
+  end
+end
