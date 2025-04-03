ec8b06f3aa40cf0aa224aec2309c29cafd9782f7
Nenad Vujicic <nenadus@gmail.com>
2025-04-03 17:20:39 +0200

    Adds note_tag_version factory and NoteTagVersion model unit tests
    Adds registering new factory bot for note_tag_version and added new unit tests to NoteTagVersionTest for checking if key length is valid, value length is valid, key length is invalid, value length is invalid, orphaned tag is invalid and note_tag_versions are unique.

--- test/models/note_tag_version_test.rb
+++ test/models/note_tag_version_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class NoteTagTest < ActiveSupport::TestCase
+class NoteTagVersionTest < ActiveSupport::TestCase
   def test_length_key_valid
-    tag = create(:note_tag)
+    tag = create(:note_tag_version)
     [0, 255].each do |i|
       tag.k = "k" * i
       assert_predicate tag, :valid?
@@ -10,7 +10,7 @@ class NoteTagTest < ActiveSupport::TestCase
   end
 
   def test_length_value_valid
-    tag = create(:note_tag)
+    tag = create(:note_tag_version)
     [0, 255].each do |i|
       tag.v = "v" * i
       assert_predicate tag, :valid?
@@ -18,29 +18,29 @@ class NoteTagTest < ActiveSupport::TestCase
   end
 
   def test_length_key_invalid
-    tag = create(:note_tag)
+    tag = create(:note_tag_version)
     tag.k = "k" * 256
-    assert_not_predicate tag, :valid?, "Key should be too long"
+    assert_not_predicate tag, :valid?
     assert_predicate tag.errors[:k], :any?
   end
 
   def test_length_value_invalid
-    tag = create(:note_tag)
+    tag = create(:note_tag_version)
     tag.v = "v" * 256
     assert_not_predicate tag, :valid?, "Value should be too long"
     assert_predicate tag.errors[:v], :any?
   end
 
   def test_orphaned_tag_invalid
-    tag = create(:note_tag)
-    tag.note = nil
+    tag = create(:note_tag_version)
+    tag.note_version = nil
     assert_not_predicate tag, :valid?, "Orphaned tag should be invalid"
-    assert_predicate tag.errors[:note], :any?
+    assert_predicate tag.errors[:note_version], :any?
   end
 
   def test_uniqueness
-    existing = create(:note_tag)
-    tag = build(:note_tag, :note => existing.note, :k => existing.k, :v => existing.v)
+    existing = create(:note_tag_version)
+    tag = build(:note_tag_version, :note_version => existing.note_version, :version => existing.version, :k => existing.k, :v => existing.v)
     assert_predicate tag, :new_record?
     assert_not_predicate tag, :valid?
     assert_raise(ActiveRecord::RecordInvalid) { tag.save! }
