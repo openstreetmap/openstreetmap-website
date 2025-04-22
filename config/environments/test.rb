a6035d07815fca75cd68ea596e8bd347bf7b5295
Robert Koeze <rkoeze@gmail.com>
2025-04-21 20:56:14 -0400

    Prefer default hash syntax in config files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { :host => Settings.server_url }
+  config.action_mailer.default_url_options = { host: Settings.server_url }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
