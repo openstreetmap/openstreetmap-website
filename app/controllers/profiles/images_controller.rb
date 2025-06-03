e97f8240d5b87a5de0995f689ba45d4c4756dc30
Anton Khorev <tony29@yandex.ru>
2025-06-03 18:20:28 +0300

    Move profile image editing to image page

--- app/controllers/profiles/images_controller.rb
+++ app/controllers/profiles/images_controller.rb
@@ -3,6 +3,18 @@ module Profiles
     private
 
     def update_profile
+      case params[:avatar_action]
+      when "new"
+        current_user.avatar.attach(params[:user][:avatar])
+        current_user.image_use_gravatar = false
+      when "delete"
+        current_user.avatar.purge_later
+        current_user.image_use_gravatar = false
+      when "gravatar"
+        current_user.avatar.purge_later
+        current_user.image_use_gravatar = true
+      end
+
       current_user.save
     end
   end
