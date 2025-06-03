15a9bff336d09bf563c6f8cb88d47240cac358b3
Anton Khorev <tony29@yandex.ru>
2025-06-03 19:02:45 +0300

    Move profile links editing to links page

--- test/controllers/profiles/descriptions_controller_test.rb
+++ test/controllers/profiles/descriptions_controller_test.rb
@@ -41,34 +41,14 @@ module Profiles
       user = create(:user)
       session_for(user)
 
-      # Updating the description should work
       put profile_description_path, :params => { :user => { :description => "new description" } }
+
       assert_redirected_to user_path(user)
       follow_redirect!
       assert_response :success
       assert_template :show
-      assert_select ".alert-success", /^Profile updated./
-      assert_select "div", "new description"
-
-      # Updating social links should work
-      put profile_description_path, :params => { :user => { :description => user.description, :social_links_attributes => [{ :url => "https://test.com/test" }] } }
-      assert_redirected_to user_path(user)
-      follow_redirect!
-      assert_response :success
-      assert_template :show
-      assert_select ".alert-success", /^Profile updated./
-      assert_select "a", "test.com/test"
-    end
-
-    def test_update_empty_social_link
-      user = create(:user)
-      session_for(user)
-
-      put profile_description_path, :params => { :user => { :description => user.description, :social_links_attributes => [{ :url => "" }] } }
-
-      assert_response :success
-      assert_template :show
-      assert_dom ".alert-danger", :text => "Couldn't update profile."
+      assert_dom ".alert-success", :text => "Profile updated."
+      assert_dom "div", "new description"
     end
   end
 end
