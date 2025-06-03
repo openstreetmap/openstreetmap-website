fc19a3770e9e4ec8a510a429bd93045501cf0a4f
Anton Khorev <tony29@yandex.ru>
2025-06-03 18:23:35 +0300

    Split profile image update test

--- test/controllers/profiles/images_controller_test.rb
+++ test/controllers/profiles/images_controller_test.rb
@@ -31,39 +31,55 @@ module Profiles
       assert_redirected_to login_path(:referer => profile_image_path)
     end
 
-    def test_update
+    def test_update_replace
+      image = Rack::Test::UploadedFile.new("test/gpx/fixtures/a.gif", "image/gif")
       user = create(:user)
       session_for(user)
 
-      # Changing to an uploaded image should work
-      image = Rack::Test::UploadedFile.new("test/gpx/fixtures/a.gif", "image/gif")
       put profile_image_path, :params => { :avatar_action => "new", :user => { :avatar => image, :description => user.description } }
+
       assert_redirected_to user_path(user)
       follow_redirect!
       assert_response :success
       assert_template :show
       assert_dom ".alert-success", :text => "Profile updated."
+
       get profile_image_path
+
       assert_dom "form > div > div.col-sm-10 > div.form-check > input[name=avatar_action][checked][value=?]", "keep"
+    end
 
-      # Changing to a gravatar image should work
+    def test_update_gravatar
+      user = create(:user)
+      session_for(user)
+
       put profile_image_path, :params => { :avatar_action => "gravatar", :user => { :description => user.description } }
-      assert_redirected_to user_path(user)
-      follow_redirect!
-      assert_response :success
-      assert_template :show
-      assert_dom ".alert-success", :text => "Profile updated."
-      get profile_image_path
-      assert_dom "form > div > div.col-sm-10 > div > div.form-check > input[name=avatar_action][checked][value=?]", "gravatar"
 
-      # Removing the image should work
-      put profile_image_path, :params => { :avatar_action => "delete", :user => { :description => user.description } }
       assert_redirected_to user_path(user)
       follow_redirect!
       assert_response :success
       assert_template :show
       assert_dom ".alert-success", :text => "Profile updated."
+
       get profile_image_path
+
+      assert_dom "form > div > div.col-sm-10 > div > div.form-check > input[name=avatar_action][checked][value=?]", "gravatar"
+    end
+
+    def test_update_remove
+      user = create(:user)
+      session_for(user)
+
+      put profile_image_path, :params => { :avatar_action => "delete", :user => { :description => user.description } }
+
+      assert_redirected_to user_path(user)
+      follow_redirect!
+      assert_response :success
+      assert_template :show
+      assert_dom ".alert-success", :text => "Profile updated."
+
+      get profile_image_path
+
       assert_dom "form > div > div.col-sm-10 > div > input[name=avatar_action][checked]", false
       assert_dom "form > div > div.col-sm-10 > div > div.form-check > input[name=avatar_action][checked]", false
     end
