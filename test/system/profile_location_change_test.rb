fcee3918d8dc07c5e7f3f9d2b3f2e8a716da12b6
Anton Khorev <tony29@yandex.ru>
2025-06-04 04:39:49 +0300

    Add "Edit Location" button to user profile

--- test/system/profile_location_change_test.rb
+++ test/system/profile_location_change_test.rb
@@ -1,36 +1,62 @@
 require "application_system_test_case"
 
 class ProfileLocationChangeTest < ApplicationSystemTestCase
-  def setup
-    stub_request(:get, /.*gravatar.com.*d=404/).to_return(:status => 404)
-  end
-
-  test "User can change their location" do
+  test "can't change location when unauthorized" do
     user = create(:user)
-    sign_in_as(user)
 
     visit user_path(user)
 
     within_content_heading do
-      assert_no_selector ".bi.bi-geo-alt-fill"
+      assert_no_link "Edit Location"
     end
+  end
 
-    within_content_body do
-      click_on "Edit Description"
-    end
+  test "can't change location of another user" do
+    user = create(:user)
+    another_user = create(:user)
+
+    sign_in_as(user)
+    visit user_path(another_user)
+
     within_content_heading do
-      click_on "Location"
+      assert_no_link "Edit Location"
+    end
+  end
+
+  test "can change location" do
+    user = create(:user)
+
+    sign_in_as(user)
+    visit user_path(user)
+
+    within_content_heading do
+      assert_text "No home location specified"
+
+      click_on "Edit Location"
     end
 
     within_content_body do
-      fill_in "Home location name", :with => "Test Location"
+      fill_in "Home location name", :with => "Test Place"
       click_on "Update Profile"
     end
 
     assert_text "Profile location updated."
 
     within_content_heading do
-      assert_text "Test Location"
+      assert_text "Home location Test Place"
+
+      click_on "Edit Location"
+    end
+
+    within_content_body do
+      fill_in "Home location name", :with => "New Test Place"
+      click_on "Update Profile"
+    end
+
+    assert_text "Profile location updated."
+
+    within_content_heading do
+      assert_text "Home location New Test Place"
     end
   end
 end
