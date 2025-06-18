3261cbefbef108d46fee92646ece9a7822ddb2e5
Anton Khorev <tony29@yandex.ru>
2025-06-19 01:07:04 +0300

    Add older/newer element versions buttons

--- app/controllers/old_nodes_controller.rb
+++ app/controllers/old_nodes_controller.rb
@@ -1,7 +1,12 @@
 class OldNodesController < OldElementsController
   def index
     @type = "node"
-    @feature = Node.preload(:node_tags, :old_nodes => [:old_tags, { :changeset => [:changeset_tags, :user] }]).find(params[:id])
+    @feature = Node.preload(:node_tags).find(params[:id])
+    @old_features, @newer_features_version, @older_features_version = get_page_items(
+      OldNode.where(:node_id => params[:id]),
+      :cursor_column => :version,
+      :includes => [:old_tags, { :changeset => [:changeset_tags, :user] }]
+    )
   rescue ActiveRecord::RecordNotFound
     render "browse/not_found", :status => :not_found
   end
