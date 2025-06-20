747a5bef6952a27e4d7d5ea291a02b0e49a8b8cd
Anton Khorev <tony29@yandex.ru>
2025-06-20 14:41:24 +0300

    Change language instead of opening preferences when logged in

--- test/system/select_language_test.rb
+++ test/system/select_language_test.rb
@@ -17,4 +17,33 @@ class SelectLanguageTest < ApplicationSystemTestCase
       assert_text "Obtenir de l’aide"
     end
   end
+
+  test "can select language when logged in" do
+    user = create(:user, :display_name => "LanguageTestUser")
+    sign_in_as(user)
+
+    visit help_path
+
+    assert_no_text "Préférences mises à jour"
+    within_content_heading do
+      assert_text "Getting Help"
+      assert_no_text "Obtenir de l’aide"
+    end
+
+    click_on "Select Language"
+    click_on "français"
+
+    assert_text "Préférences mises à jour"
+    within_content_heading do
+      assert_no_text "Getting Help"
+      assert_text "Obtenir de l’aide"
+    end
+
+    click_on "LanguageTestUser"
+    click_on "Mes préférences"
+
+    within_content_body do
+      assert_select "Langue préférée", :selected => "français"
+    end
+  end
 end
