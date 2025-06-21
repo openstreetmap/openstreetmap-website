8f01d356522a9430e1ecc37413cec7312878b1ae
Anton Khorev <tony29@yandex.ru>
2025-06-21 17:18:33 +0300

    WIP pull out href

--- app/helpers/numbered_pagination_helper.rb
+++ app/helpers/numbered_pagination_helper.rb
@@ -5,7 +5,8 @@ module NumberedPaginationHelper
     if top_version <= 5
       lists << tag.ul(:class => "pagination pagination-sm mt-1") do
         (1..top_version).each do |v|
-          concat element_versions_pagination_item(:version => v, :active => v == active_version)
+          concat element_versions_pagination_item(v, :href => { :version => v },
+                                                     :active => v == active_version)
         end
       end
     else
@@ -19,10 +20,10 @@ module NumberedPaginationHelper
       lists << tag.ul(:id => "versions-navigation-list-start",
                       :class => "pagination pagination-sm mt-1") do
         (start_list_version_from..start_list_version_to).each do |v|
-          concat element_versions_pagination_item(:version => v,
-                                                  :active => v == active_version,
-                                                  :last_item => v == start_list_version_to,
-                                                  :edge_item_border => true)
+          concat element_versions_pagination_item(v, :href => { :version => v },
+                                                     :active => v == active_version,
+                                                     :last_item => v == start_list_version_to,
+                                                     :edge_item_border => true)
         end
       end
       lists << tag.ul(:id => "versions-navigation-list-scrollable",
@@ -32,22 +33,22 @@ module NumberedPaginationHelper
                         "pt-1 px-1 mx-n1", # space reserved for focus outlines
                         "position-relative" # required for centering when clicking "Version #n"
                       ]) do
-        concat element_versions_pagination_item(:first_item => true) if middle_list_version_from > start_list_version_to + 1
+        concat element_versions_pagination_item("...", :first_item => true) if middle_list_version_from > start_list_version_to + 1
         (middle_list_version_from..middle_list_version_to).each do |v|
-          concat element_versions_pagination_item(:version => v,
-                                                  :active => v == active_version,
-                                                  :first_item => v == start_list_version_to + 1,
-                                                  :last_item => v == end_list_version_from - 1)
+          concat element_versions_pagination_item(v, :href => { :version => v },
+                                                     :active => v == active_version,
+                                                     :first_item => v == start_list_version_to + 1,
+                                                     :last_item => v == end_list_version_from - 1)
         end
-        concat element_versions_pagination_item(:last_item => true) if middle_list_version_to < end_list_version_from - 1
+        concat element_versions_pagination_item("...", :last_item => true) if middle_list_version_to < end_list_version_from - 1
       end
       lists << tag.ul(:id => "versions-navigation-list-end",
                       :class => "pagination pagination-sm mt-1") do
         (end_list_version_from..end_list_version_to).each do |v|
-          concat element_versions_pagination_item(:version => v,
-                                                  :active => v == active_version,
-                                                  :first_item => v == end_list_version_from,
-                                                  :edge_item_border => true)
+          concat element_versions_pagination_item(v, :href => { :version => v },
+                                                     :active => v == active_version,
+                                                     :first_item => v == end_list_version_from,
+                                                     :edge_item_border => true)
         end
       end
     end
@@ -55,17 +56,17 @@ module NumberedPaginationHelper
     tag.div safe_join(lists), :class => "d-flex align-items-start"
   end
 
-  def element_versions_pagination_item(version: nil, active: false, first_item: false, last_item: false, edge_item_border: false)
+  def element_versions_pagination_item(body, href: nil, active: false, first_item: false, last_item: false, edge_item_border: false)
     link_class = ["page-link", { "rounded-start-0" => first_item,
                                  "border-start-0" => first_item && !edge_item_border,
                                  "rounded-end-0" => last_item,
                                  "border-end-0" => last_item && !edge_item_border }]
-    link = if version
-             link_to version, { :version => version }, :class => link_class
+    link = if href
+             link_to body, href, :class => link_class
            else
-             tag.span "...", :class => link_class
+             tag.span body, :class => link_class
            end
     tag.li link, :id => ("versions-navigation-current-page-item" if active),
-                 :class => ["page-item", { "disabled" => !version, "active" => active }]
+                 :class => ["page-item", { "disabled" => !href, "active" => active }]
   end
 end
