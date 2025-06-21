5582a8351126e9d2267abb58694ff4fd04690ad4
Anton Khorev <tony29@yandex.ru>
2025-06-21 16:08:57 +0300

    WIP rewrite helper, but has double border

--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -1,6 +1,4 @@
 module BrowseHelper
-  ELEMENT_PAGINATION_WINDOW_HALF_SIZE = 50
-
   def element_icon(type, object)
     selected_icon_data = { :filename => "#{type}.svg", :priority => 1 }
 
@@ -95,7 +93,7 @@ module BrowseHelper
     end
   end
 
-  def element_versions_pagination(displayed_version, top_version)
+  def element_versions_pagination(displayed_version, top_version, window_half_size: 50)
     lists = []
 
     if top_version <= 5
@@ -107,13 +105,15 @@ module BrowseHelper
       start_list_version_to = displayed_version < 3 ? displayed_version + 1 : 1
       end_list_version_from = displayed_version > top_version - 2 ? displayed_version - 1 : top_version
       end_list_version_to = top_version
-      middle_list_version_from = [displayed_version - ELEMENT_PAGINATION_WINDOW_HALF_SIZE, start_list_version_to + 1].max
-      middle_list_version_to = [displayed_version + ELEMENT_PAGINATION_WINDOW_HALF_SIZE, end_list_version_to - 1].min
+      middle_list_version_from = [displayed_version - window_half_size, start_list_version_to + 1].max
+      middle_list_version_to = [displayed_version + window_half_size, end_list_version_from - 1].min
 
       lists << tag.ul(:id => "versions-navigation-list-start",
                       :class => "pagination pagination-sm mt-1") do
         (start_list_version_from..start_list_version_to).each do |v|
-          concat element_versions_pagination_item(v, v == displayed_version, { "rounded-end-0" => v == start_list_version_to })
+          concat element_versions_pagination_item(:version => v,
+                                                  :active => v == displayed_version,
+                                                  :last_item => v == start_list_version_to)
         end
       end
       lists << tag.ul(:id => "versions-navigation-list-scrollable",
@@ -123,24 +123,21 @@ module BrowseHelper
                         "pt-1 px-1 mx-n1", # space reserved for focus outlines
                         "position-relative" # required for centering when clicking "Version #n"
                       ]) do
-        if middle_list_version_from > start_list_version_to + 1
-          concat element_versions_pagination_item_span("...", false)
-        end
+        concat element_versions_pagination_item(:first_item => true) if middle_list_version_from > start_list_version_to + 1
         (middle_list_version_from..middle_list_version_to).each do |v|
-          is_first_item = v == start_list_version_to + 1
-          is_last_item = v == end_list_version_from - 1
-          concat element_versions_pagination_item(v, v == displayed_version, { "rounded-0" => is_first_item || is_last_item,
-                                                                               "border-start-0" => is_first_item,
-                                                                               "border-end-0" => is_last_item })
+          concat element_versions_pagination_item(:version => v,
+                                                  :active => v == displayed_version,
+                                                  :first_item => v == start_list_version_to + 1,
+                                                  :last_item => v == end_list_version_from - 1)
         end
-        if middle_list_version_to < end_list_version_from - 1
-          concat element_versions_pagination_item_span("...", false)
-        end
+        concat element_versions_pagination_item(:last_item => true) if middle_list_version_to < end_list_version_from - 1
       end
       lists << tag.ul(:id => "versions-navigation-list-end",
                       :class => "pagination pagination-sm mt-1") do
         (end_list_version_from..end_list_version_to).each do |v|
-          concat element_versions_pagination_item(v, v == displayed_version, { "rounded-start-0" => v == end_list_version_from })
+          concat element_versions_pagination_item(:version => v,
+                                                  :active => v == displayed_version,
+                                                  :first_item => v == end_list_version_from)
         end
       end
     end
@@ -148,16 +145,16 @@ module BrowseHelper
     tag.div safe_join(lists), :class => "d-flex align-items-start"
   end
 
-  def element_versions_pagination_item(version, active, extra_classes = {})
-    link = link_to version, { :version => version }, :class => ["page-link", extra_classes]
+  def element_versions_pagination_item(version: nil, active: false, first_item: false, last_item: false)
+    link_class = ["page-link", { "rounded-start-0" => first_item,
+                                 "rounded-end-0" => last_item }]
+    link = if version
+             link_to version, { :version => version }, :class => link_class
+           else
+             tag.span "...", :class => link_class
+           end
     tag.li link, :id => ("versions-navigation-current-page-item" if active),
-                 :class => ["page-item", { "active" => active }]
-  end
-
-  def element_versions_pagination_item_span(version, active, extra_classes = {}) # TODO: can't be active
-    link = tag.span version, :class => ["page-link", extra_classes]
-    tag.li link, :id => ("versions-navigation-current-page-item" if active),
-                 :class => ["page-item disabled", { "active" => active }]
+                 :class => ["page-item", { "disabled" => !version, "active" => active }]
   end
 
   private
