0814fd0e9b8fa3262a49ef050b2c2c61de8dd6c0
Anton Khorev <tony29@yandex.ru>
2025-06-22 13:54:27 +0300

    Implement MappedElement as extension of Element

--- app/assets/javascripts/index/element.js
+++ app/assets/javascripts/index/element.js
@@ -1,28 +1,31 @@
 (function () {
-  OSM.Element = function () {
+  OSM.Element = function (map, type) {
     const page = {};
 
-    page.pushstate = page.popstate = function (path) {
-      OSM.loadSidebarContent(path);
+    page.pushstate = page.popstate = function (path, id, version) {
+      OSM.loadSidebarContent(path, function () {
+        page._addObject(type, id, version);
+      });
     };
+
+    page.load = function (path, id, version) {
+      page._addObject(type, id, version, true);
+    };
+
+    page.unload = function () {
+      page._removeObject();
+    };
+
+    page._addObject = function () {};
+    page._removeObject = function () {};
 
     return page;
   };
 
   OSM.MappedElement = function (map, type) {
-    const page = {};
+    const page = OSM.Element(map, type);
 
-    page.pushstate = page.popstate = function (path, id, version) {
-      OSM.loadSidebarContent(path, function () {
-        addObject(type, id, version);
-      });
-    };
-
-    page.load = function (path, id, version) {
-      addObject(type, id, version, true);
-    };
-
-    function addObject(type, id, version, center) {
+    page._addObject = function (type, id, version, center) {
       const hashParams = OSM.parseHash();
       map.addObject({ type: type, id: parseInt(id, 10), version: version && parseInt(version, 10) }, function (bounds) {
         if (!hashParams.center && bounds.isValid() &&
@@ -32,9 +35,9 @@
           });
         }
       });
-    }
+    };
 
-    page.unload = function () {
+    page._removeObject = function () {
       map.removeObject();
     };
 
