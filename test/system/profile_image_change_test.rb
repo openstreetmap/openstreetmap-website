b3b69a6726e6435c9a6b5ad16367701c5c234b67
Anton Khorev <tony29@yandex.ru>
2025-07-01 19:59:53 +0300

    Turn own profile image into an edit link

--- test/system/profile_image_change_test.rb
+++ test/system/profile_image_change_test.rb
@@ -6,6 +6,10 @@ class ProfileImageChangeTest < ApplicationSystemTestCase
 
     visit user_path(user)
 
+    within_content_heading do
+      assert_no_link "Change Image"
+    end
+
     within_content_body do
       assert_no_button "Edit Profile Details"
       assert_no_link "Change Image"
@@ -18,6 +22,10 @@ class ProfileImageChangeTest < ApplicationSystemTestCase
 
     sign_in_as(user)
     visit user_path(another_user)
+
+    within_content_heading do
+      assert_no_link "Change Image"
+    end
 
     within_content_body do
       assert_no_button "Edit Profile Details"
@@ -69,6 +77,43 @@ class ProfileImageChangeTest < ApplicationSystemTestCase
       assert_no_field "Keep the current image"
       assert_no_field "Remove the current image"
       assert_no_field "Replace the current image"
+    end
+  end
+
+  test "can add image by clicking the placeholder image" do
+    user = create(:user)
+
+    sign_in_as(user)
+    visit user_path(user)
+
+    within_content_heading do
+      click_on "Change Image"
+    end
+
+    within_content_body do
+      assert_unchecked_field "Add an image"
+      assert_no_field "Keep the current image"
+      assert_no_field "Remove the current image"
+      assert_no_field "Replace the current image"
+
+      attach_file "Avatar", "test/gpx/fixtures/a.gif"
+
+      assert_checked_field "Add an image"
+
+      click_on "Update Profile"
+    end
+
+    assert_text "Profile image updated."
+
+    within_content_heading do
+      click_on "Change Image"
+    end
+
+    within_content_body do
+      assert_no_field "Add an image"
+      assert_checked_field "Keep the current image"
+      assert_unchecked_field "Remove the current image"
+      assert_unchecked_field "Replace the current image"
     end
   end
 end
