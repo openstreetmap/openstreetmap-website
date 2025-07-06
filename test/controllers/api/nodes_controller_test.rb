0d621a4ac8fc24aebd07a06e684fa9a0302161e9
Anton Khorev <tony29@yandex.ru>
2025-07-06 22:34:21 +0300

    Replace multiline strings with heredocs api elements test xml

--- test/controllers/api/nodes_controller_test.rb
+++ test/controllers/api/nodes_controller_test.rb
@@ -512,9 +512,13 @@ module Api
 
       # try and put something into a string that the API might
       # use unquoted and therefore allow code injection...
-      xml = "<osm><node lat='0' lon='0' changeset='#{private_changeset.id}'>" \
-            "<tag k='\#{@user.inspect}' v='0'/>" \
-            "</node></osm>"
+      xml = <<~OSM
+        <osm>
+          <node lat='0' lon='0' changeset='#{private_changeset.id}'>
+            <tag k='\#{@user.inspect}' v='0'/>
+          </node>
+        </osm>
+      OSM
       post api_nodes_path, :params => xml, :headers => auth_header
       assert_require_public_data "Shouldn't be able to create with non-public user"
 
@@ -523,9 +527,13 @@ module Api
 
       # try and put something into a string that the API might
       # use unquoted and therefore allow code injection...
-      xml = "<osm><node lat='0' lon='0' changeset='#{changeset.id}'>" \
-            "<tag k='\#{@user.inspect}' v='0'/>" \
-            "</node></osm>"
+      xml = <<~OSM
+        <osm>
+          <node lat='0' lon='0' changeset='#{changeset.id}'>
+            <tag k='\#{@user.inspect}' v='0'/>
+          </node>
+        </osm>
+      OSM
       post api_nodes_path, :params => xml, :headers => auth_header
       assert_response :success
       nodeid = @response.body
