b06cc0ede70d20140ec6b5327862ebc734fd6bef
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2025-07-12 21:13:34 +0200

    Store routing icons separately

---
+++ config/initializers/inline_svg.rb
@@ -0,0 +1,13 @@
+module OpenStreetMap
+  class SvgToSymbolTransform < InlineSvg::CustomTransformation
+    def transform(doc)
+      with_svg(doc) do |svg|
+        svg.name = "symbol"
+      end
+    end
+  end
+end
+
+InlineSvg.configure do |config|
+  config.add_custom_transformation(:attribute => :to_symbol, :transform => OpenStreetMap::SvgToSymbolTransform)
+end
