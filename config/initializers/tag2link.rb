bd1f6aecf812046ae653ffc74b8b613b4cf4079c
Kyle Hensel <k.y.l.e@outlook.co.nz>
2025-07-17 18:39:25 +1000

    code review suggestions
    i'll squash these commits later

--- config/initializers/tag2link.rb
+++ config/initializers/tag2link.rb
@@ -1,25 +1,11 @@
+TAG2LINK_RANKS = %w[deprecated normal preferred].freeze
+
 # A map of each OSM key to its formatter URL. For example:
 # { "ref:vatin" => "https://example.com/$1" }
-TAG2LINK = lambda {
-  # the JSON data is an array with duplicate entries, which is not efficient for lookups.
-  # So, convert it to a hash and only keep the item with the best rank.
-  array = JSON.parse(Rails.root.join("node_modules/tag2link/index.json").read)
-
-  ranks = %w[deprecated normal preferred].freeze
-
-  output = {}
-
-  all_keys = array.map { |item| item["key"] }.uniq
-
-  all_keys.each do |key|
-    # for each key, find the item with the best rank
-    best_definition = array
-                      .select { |item| item["key"] == key }
-                      .reject { |item| item["rank"] == "deprecated" }
-                      .max_by { |item| ranks.index(item["rank"]) }
-
-    output[key.sub(/^Key:/, "")] = best_definition["url"]
-  end
-
-  output
-}.call
+# The JSON data is an array with duplicate entries, which is not efficient for lookups.
+# So, convert it to a hash and only keep the item with the best rank.
+TAG2LINK = JSON.parse(Rails.root.join("node_modules/tag2link/index.json").read)
+               .reject { |item| item["rank"] == "deprecated" }
+               .group_by { |item| item["key"] }
+               .transform_keys { |key| key.sub(/^Key:/, "") }
+               .transform_values { |items| items.max_by { |item| TAG2LINK_RANKS.index(item["rank"]) }.fetch("url") }
