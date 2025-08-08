5ba52627024c35cca6825f262c0f62e539021af6
Anton Khorev <tony29@yandex.ru>
2025-08-08 20:59:32 +0300

    Remove classic pagination

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module OpenStreetMap
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(:ignore => %w[assets classic_pagination tasks])
+    config.autoload_lib(:ignore => %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
