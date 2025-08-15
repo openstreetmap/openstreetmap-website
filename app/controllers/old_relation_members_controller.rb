de51ec959644154e7b1564fd96b4ddbb120a1209
mmd-osm <mmd.osm@gmail.com>
2025-08-15 17:29:43 +0200

    Lazy loading relation members

---
+++ app/controllers/old_relation_members_controller.rb
@@ -0,0 +1,14 @@
+class OldRelationMembersController < OldElementsController
+  def show
+    @type = "relation"
+    @current_feature = Relation.find(params[:id])
+    @feature = OldRelation.preload(:old_members => :member).find([params[:id], params[:version]])
+    @frame_id = "member_relation_#{@feature.id}"
+
+    return deny_access(nil) if @feature.redacted? && !params[:show_redactions]
+
+    render :partial => "browse/relation_member_frame", :locals => { :relation => @feature, :frame_id => @frame_id }
+  rescue ActiveRecord::RecordNotFound
+    render "browse/not_found", :status => :not_found
+  end
+end
---
+++ test/controllers/relation_members_controller_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class RelationMembersControllerTest < ActionDispatch::IntegrationTest
+  ##
+  # test all routes which lead to this controller
+  def test_routes
+    assert_routing(
+      { :path => "/relation/1/members", :method => :get },
+      { :controller => "relation_members", :action => "show", :id => "1" }
+    )
+  end
+
+  def test_show_with_members
+    relation = create(:relation)
+    get relation_members_path(relation)
+    assert_response :success
+  end
+
+  def test_show_not_found
+    get relation_members_path(0)
+
+    assert_response :not_found
+  end
+
+  def test_show_timeout
+    relation = create(:relation)
+
+    with_settings(:web_timeout => -1) do
+      get relation_members_path(relation, 1)
+    end
+
+    assert_response :error
+  end
+end
