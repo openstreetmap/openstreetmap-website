e0ce9b933d6edf7d1d70a1af92288ecd701725c3
Robert Koeze <rkoeze@gmail.com>
2025-08-30 22:04:31 -0400

    Add dismiss button for kramdown directions

--- test/system/rich_text_test.rb
+++ test/system/rich_text_test.rb
@@ -13,4 +13,17 @@ class RichTextSystemTest < ApplicationSystemTestCase
     click_on "Publish"
     assert_field "Body"
   end
+
+  test "closing help hides card and expands editor" do
+    sign_in_as create(:user)
+    visit new_diary_entry_path
+
+    # Ensure viewport is large enough such that Markdown guide is displayed next to textarea
+    page.current_window.resize_to(800, 800)
+
+    click_button(:class => "richtext_help_close")
+
+    assert_no_selector "button.richtext_help_close", :visible => true
+    assert_selector ".richtext_container .tab-content.col-sm-8[style*='width: 100%']"
+  end
 end
