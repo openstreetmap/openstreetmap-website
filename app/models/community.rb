a816d4413486b42be39d7b7d2e2d5ce6b2ae36dd
Tom Hughes <tom@compton.nu>
2025-10-22 19:09:35 +0100

    Update path to community index data files

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Community < FrozenRecord::Base
-  self.base_path = Rails.root.join("node_modules/osm-community-index/dist/")
+  self.base_path = Rails.root.join("node_modules/osm-community-index/dist/json/")
   self.backend = OsmCommunityIndex::ResourceBackend
 
   def url
