eb013997531f9a9a1b4d6b0730772ebb78299491
Minh Nguyễn <mxn@1ec5.org>
2025-10-23 15:50:54 -0700

    Fix slippy map when layer names are translated (#322)
    * Fix map issues in non-English locales
    
    * Hard-code package style names as part of constructors
    
    ---------
    
    Co-authored-by: Rub21 <rub2106@gmail.com>

--- app/assets/javascripts/leaflet.ohm.js
+++ app/assets/javascripts/leaflet.ohm.js
@@ -1,14 +1,14 @@
 L.OSM.OHM = L.OSM.MaplibreGL.extend({
   onAdd: function (map) {
     L.OSM.MaplibreGL.prototype.onAdd.call(this, map);
-    this.getMaplibreMap().setStyle(ohmVectorStyles[this.options.name.replaceAll(' ','')]);
+    this.getMaplibreMap().setStyle(ohmVectorStyles[this.ohmStyleName]);
   },
   onRemove: function (map) {
     L.OSM.MaplibreGL.prototype.onRemove.call(this, map);
   }
 });
 
-L.OSM.Historical = L.OSM.OHM.extend({});
-L.OSM.Railway = L.OSM.OHM.extend({});
-L.OSM.Woodblock = L.OSM.OHM.extend({});
-L.OSM.JapaneseScroll = L.OSM.OHM.extend({});
+L.OSM.Historical = L.OSM.OHM.extend({ ohmStyleName: "Historical" });
+L.OSM.Railway = L.OSM.OHM.extend({ ohmStyleName: "Railway"});
+L.OSM.Woodblock = L.OSM.OHM.extend({ ohmStyleName: "Woodblock" });
+L.OSM.JapaneseScroll = L.OSM.OHM.extend({ ohmStyleName: "JapaneseScroll" });
