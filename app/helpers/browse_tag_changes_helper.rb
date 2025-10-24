8978e002f4ef3bcf290a9c1b4d21a5dd56307ab8
Ian Dees <ian.dees@gmail.com>
2025-10-24 10:56:12 -0500

    Shorten browse_helper by moving some stuff to a new helper, fix lint errors

---
+++ app/helpers/browse_tag_changes_helper.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+module BrowseTagChangesHelper
+  def wrap_tags_with_version_changes(tags_to_values, current_version = nil, all_versions = [])
+    # Find the previous usable version by looking backwards through all versions
+    previous_version = all_versions
+                       .find_index { |v| v.version == current_version }
+                       &.then { |index| index.positive? ? all_versions[0...index].reverse : nil }
+                       &.find { |v| !v.redacted? || params[:show_redactions] }
+
+    previous_tags = previous_version&.tags || {}
+
+    tags_added = tags_modified = tags_unmodified = tags_removed = tags_with_unknown_versioning = {}
+
+    if all_versions.present?
+      tags_added = tags_to_values
+                   .except(*previous_tags.keys)
+                   .transform_values { |value| { :type => :added, :current => value } }
+
+      tags_modified = tags_to_values
+                      .filter { |name, value| previous_tags.key?(name) && previous_tags[name] != value }
+                      .each_with_object({}) do |(name, value), memo|
+        memo[name] = { :type => :modified, :current => value, :previous => previous_tags[name] }
+      end
+
+      tags_unmodified = tags_to_values
+                        .filter { |name, value| previous_tags[name] == value }
+                        .transform_values { |value| { :type => :unmodified, :current => value } }
+
+      tags_removed = previous_tags.keys.difference(tags_to_values.keys).index_with do |key|
+        { :type => :removed, :previous => previous_tags[key] }
+      end
+    else
+      tags_with_unknown_versioning = tags_to_values.transform_values do |value|
+        { :current => value }
+      end
+    end
+
+    tags_with_unknown_versioning.merge(tags_added, tags_modified, tags_unmodified, tags_removed)
+  end
+
+  def tag_change_class(change_type)
+    {
+      :added => "tag-added",
+      :modified => "tag-modified",
+      :removed => "tag-deleted",
+      :unmodified => "tag-unmodified"
+    }.fetch(change_type, "")
+  end
+
+  def format_tag_value_with_change(key, change_info)
+    case change_info[:type]
+    when :added, :unmodified
+      format_value(key, change_info[:current])
+    when :modified
+      # Return array of two values for the two rows that will be created
+      [
+        format_value(key, change_info[:previous]),
+        format_value(key, change_info[:current])
+      ]
+    when :removed
+      format_value(key, change_info[:previous] || "")
+    else
+      format_key(key)
+    end
+  end
+
+  def get_change_indicator_text(change_type)
+    case change_type
+    when :added
+      "+"
+    when :removed
+      "−"
+    else
+      " "
+    end
+  end
+
+  def get_indicator_cell_class(change_type)
+    case change_type
+    when :added
+      "diff-indicator-cell diff-added"
+    when :removed
+      "diff-indicator-cell diff-removed"
+    when :unmodified
+      "diff-indicator-cell diff-unmodified"
+    else
+      "diff-indicator-cell"
+    end
+  end
+
+  def format_tag_row_with_change(key, change_info)
+    case change_info[:type]
+    when :modified
+      # Generate two rows for modified tags
+      value_cells = format_tag_value_with_change(key, change_info)
+      [
+        tag.tr(:class => tag_change_class(change_info[:type])) do
+          safe_join([
+                      tag.th(format_key(key), :class => "diff-key-modified", :rowspan => 2),
+                      tag.td(get_change_indicator_text(:removed), :class => get_indicator_cell_class(:removed)),
+                      tag.td(value_cells[0], :class => "diff-cell diff-old-row")
+                    ])
+        end,
+        tag.tr(:class => tag_change_class(change_info[:type])) do
+          safe_join([
+                      tag.td(get_change_indicator_text(:added), :class => get_indicator_cell_class(:added)),
+                      tag.td(value_cells[1], :class => "diff-cell diff-new-row")
+                    ])
+        end
+      ]
+    else
+      # Generate single row for other change types
+      [
+        tag.tr(:class => tag_change_class(change_info[:type])) do
+          safe_join([
+                      tag.th(format_key(key)),
+                      tag.td(get_change_indicator_text(change_info[:type]), :class => get_indicator_cell_class(change_info[:type])),
+                      tag.td(format_tag_value_with_change(key, change_info), :class => "diff-cell")
+                    ])
+        end
+      ]
+    end
+  end
+end
