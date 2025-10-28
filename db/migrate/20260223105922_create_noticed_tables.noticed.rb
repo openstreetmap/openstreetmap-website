9b1a4a8044cc9cfc162a3ae6d3795629c60a3594
Pablo Brasero <pablo@pablobm.com>
2025-10-28 15:23:28 +0000

    Tweaks to migrations generated by Noticed
    - Removed code to detect the type of primary and foreign keys.
    - Converted to use Rails 8.1 semantics (originally it was 6.1).
    - Addressed lint issues.

--- db/migrate/20260223105922_create_noticed_tables.noticed.rb
+++ db/migrate/20260223105922_create_noticed_tables.noticed.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 # This migration comes from noticed (originally 20231215190233)
-class CreateNoticedTables < ActiveRecord::Migration[6.1]
+class CreateNoticedTables < ActiveRecord::Migration[8.1]
   def change
-    primary_key_type, foreign_key_type = primary_and_foreign_key_types
-    create_table :noticed_events, id: primary_key_type do |t|
+    create_table :noticed_events do |t|
       t.string :type
-      t.belongs_to :record, polymorphic: true, type: foreign_key_type
+      t.belongs_to :record, :polymorphic => true
       if t.respond_to?(:jsonb)
         t.jsonb :params
       else
@@ -14,24 +15,14 @@ class CreateNoticedTables < ActiveRecord::Migration[6.1]
       t.timestamps
     end
 
-    create_table :noticed_notifications, id: primary_key_type do |t|
+    create_table :noticed_notifications do |t|
       t.string :type
-      t.belongs_to :event, null: false, type: foreign_key_type
-      t.belongs_to :recipient, polymorphic: true, null: false, type: foreign_key_type
+      t.belongs_to :event, :null => false
+      t.belongs_to :recipient, :polymorphic => true, :null => false
       t.datetime :read_at
       t.datetime :seen_at
 
       t.timestamps
     end
-  end
-
-  private
-
-  def primary_and_foreign_key_types
-    config = Rails.configuration.generators
-    setting = config.options[config.orm][:primary_key_type]
-    primary_key_type = setting || :primary_key
-    foreign_key_type = setting || :bigint
-    [primary_key_type, foreign_key_type]
   end
 end
--- db/migrate/20260223105923_add_notifications_count_to_noticed_event.noticed.rb
+++ db/migrate/20260223105923_add_notifications_count_to_noticed_event.noticed.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 # This migration comes from noticed (originally 20240129184740)
-class AddNotificationsCountToNoticedEvent < ActiveRecord::Migration[6.1]
+class AddNotificationsCountToNoticedEvent < ActiveRecord::Migration[8.1]
   def change
     add_column :noticed_events, :notifications_count, :integer
   end
