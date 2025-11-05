a93730cb3f5ef1ed90414ba3baeb5721a7925bf6
Frank Elsinga <frank@elsinga.de>
2025-11-05 23:29:07 +0100

    Migrate the dashboard entry page from leaflet to maplibre
    Co-authored-by: Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,32 +1,35 @@
-//= require leaflet.locate
+//= require maplibre.map
 
 $(function () {
-  const defaultHomeZoom = 12;
+  const defaultHomeZoom = 11;
   let map;
 
   if ($("#map").length) {
-    map = L.map("map", {
+    map = new maplibregl.Map({
+      container: "map",
+      style: OSM.Mapnik,
       attributionControl: false,
-      zoomControl: false
-    }).addLayer(new L.OSM.Mapnik());
+      center: OSM.home ? [OSM.home.lon, OSM.home.lat] : [0, 0],
+      zoom: OSM.home ? defaultHomeZoom : 0
+    });
 
-    const position = $("html").attr("dir") === "rtl" ? "topleft" : "topright";
-
-    L.OSM.zoom({ position }).addTo(map);
-
-    L.OSM.locate({ position }).addTo(map);
-
-    if (OSM.home) {
-      map.setView([OSM.home.lat, OSM.home.lon], defaultHomeZoom);
-    } else {
-      map.setView([0, 0], 0);
-    }
+    const position = $("html").attr("dir") === "rtl" ? "top-left" : "top-right";
+    map.addControl(new maplibregl.NavigationControl({ showCompass: false }), position);
+    const geolocate = new maplibregl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    });
+    map.addControl(geolocate, position);
 
     $("[data-user]").each(function () {
       const user = $(this).data("user");
       if (user.lon && user.lat) {
-        L.marker([user.lat, user.lon], { icon: OSM.getMarker({ color: user.color }) }).addTo(map)
-          .bindPopup(user.description, { minWidth: 200 });
+        OSM.createMapLibreMarker({ icon: "dot", color: user.color })
+          .setLngLat([user.lon, user.lat])
+          .setPopup(OSM.createMapLibrePopup(user.description))
+          .addTo(map);
       }
     });
   }
