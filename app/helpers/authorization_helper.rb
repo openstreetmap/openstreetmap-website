6d25578ec9f34807b024f08750a5b4eca4ec63db
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2025-11-05 04:23:03 +0100

    Replace role icon

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -8,9 +8,15 @@ module AuthorizationHelper
     html << t("oauth.scopes.#{scope}")
     if Oauth::MODERATOR_SCOPES.include? scope
       html << " "
-      html << inline_svg_tag("icons/role-star.svg", :class => "role-icon moderator align-text-bottom",
-                                                    :title => t("oauth.for_roles.moderator"))
+      html << role_icon(:classes => "bi-star-fill role-moderator", :title => t("oauth.for_roles.moderator"))
     end
     safe_join(html)
+  end
+
+  def role_icon(classes: "", title: "")
+    safe_join([
+                tag.i(:class => ["bi fs-5 align-middle", classes], :title => title, :aria => { :hidden => "true" }),
+                tag.span(title, :class => "visually-hidden")
+              ])
   end
 end
