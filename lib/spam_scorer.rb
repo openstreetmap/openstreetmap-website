11ad38237af74bc7ad987920a5cb383d49522856
Pablo Brasero <pablo@pablobm.com>
2025-11-20 11:55:27 +0000

    Move into submodule ahead of new type of scorer

--- lib/spam_scorer.rb
+++ lib/spam_scorer.rb
@@ -1,41 +1,11 @@
 # frozen_string_literal: true
 
-class SpamScorer
+module SpamScorer
   SPAMMY_PHRASES = [
     "Business Description:", "Additional Keywords:"
   ].freeze
 
-  def initialize(text)
-    @text = text
+  def self.new_from_rich_text(text)
+    self::RichText.new(text)
   end
-
-  def score
-    link_count = 0
-    link_size = 0
-
-    doc = Nokogiri::HTML(text.to_html)
-
-    if doc.content.empty?
-      link_proportion = 0
-    else
-      doc.xpath("//a").each do |link|
-        link_count += 1
-        link_size += link.content.length
-      end
-
-      link_proportion = link_size.to_f / doc.content.length
-    end
-
-    spammy_phrases = SPAMMY_PHRASES.count do |phrase|
-      doc.content.include?(phrase)
-    end
-
-    ([link_proportion - 0.2, 0.0].max * 200) +
-      (link_count * 40) +
-      (spammy_phrases * 40)
-  end
-
-  private
-
-  attr_reader :text
 end
