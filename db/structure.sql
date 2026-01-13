37b2d0afbac67b4c34c9b50057c1ad11fe92ab13
Pablo Brasero <pablo@pablobm.com>
2026-01-13 14:27:42 +0000

    Enable PostGIS

--- db/structure.sql
+++ db/structure.sql
@@ -31,6 +31,20 @@ COMMENT ON EXTENSION btree_gist IS 'support for indexing common datatypes in GiS
 
 
 --
+-- Name: postgis; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION postgis IS 'PostGIS geometry and geography spatial types and functions';
+
+
+--
 -- Name: format_enum; Type: TYPE; Schema: public; Owner: -
 --
 
@@ -3564,6 +3578,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('22'),
 ('21'),
 ('20260218183352'),
+('20260113142804'),
 ('20251218105716'),
 ('20251121134648'),
 ('20250704143751'),
