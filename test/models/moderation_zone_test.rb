dff678473e3f22c4e2bf4d7725f77e366f405724
Pablo Brasero <pablo@pablobm.com>
2026-01-14 16:16:16 +0000

    Proof of concept

--- test/models/moderation_zone_test.rb
+++ test/models/moderation_zone_test.rb
@@ -3,4 +3,16 @@
 require "test_helper"
 
 class ModerationZoneTest < ActiveSupport::TestCase
+  def test_falls_within_any
+    create(:moderation_zone, :seville_cathedral)
+
+    # Dead center
+    assert ModerationZone.falls_within_any?(:lat => 37.385972, :lon => -5.993149)
+
+    # Inside, near the boundary
+    assert ModerationZone.falls_within_any?(:lat => 37.386658, :lon => -5.994024)
+
+    # Outside, near the boundary
+    assert_not ModerationZone.falls_within_any?(:lat => 37.386769, :lon => -5.994185)
+  end
 end
