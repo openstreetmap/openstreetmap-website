5e69cc0335936c50099f5df94eebe8b087b5be7e
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2026-03-04 09:26:38 +0000

    Make AssetHelper work without precompiled assets

--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -2,8 +2,20 @@
 
 module AssetHelper
   def assets(directory)
-    Rails.application.assets_manifest.assets.keys.each_with_object({}) do |asset, assets|
-      assets[asset] = asset_path(asset) if asset.start_with?("#{directory}/")
+    if (manifest = Rails.application.assets_manifest.assets) && !manifest.empty?
+      # With precompiled assets
+      manifest.keys.each_with_object({}) do |asset, assets|
+        assets[asset] = asset_path(asset) if asset.start_with?("#{directory}/")
+      end
+    elsif (env = Rails.application.assets) && env.present?
+      # Without precompiled assets
+      env.paths
+         .filter { |path| path.include?(directory) }
+         .flat_map { |path| Dir.glob(File.join(path, "**", "*")) }
+         .filter_map { |path| env.find_asset(path) }
+         .each_with_object({}) do |asset, assets|
+           assets[asset.logical_path] = asset_path(asset.logical_path) if asset.logical_path.start_with?("#{directory}/")
+         end
     end
   end
 end
