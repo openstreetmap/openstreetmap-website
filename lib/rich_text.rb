e9f83144b23bd23c5d3e80fc3e0d0f2fe228bce1
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2026-03-05 09:46:15 +0000

    Raise linkify regex timeout
    Integrate Tom's timeout patch that closes #6812 and closes #6799

--- lib/rich_text.rb
+++ lib/rich_text.rb
@@ -112,7 +112,7 @@ module RichText
           expanded_path = "#{rule.host || "#{Settings.server_protocol}://#{Settings.server_url}"}/#{rule.path_template}"
           rule.patterns
               .grep(String)
-              .map { |pattern| [Regexp.new("(?<=^|#{URL_UNSAFE_CHARS})#{pattern}", Regexp::IGNORECASE, :timeout => 0.05), expanded_path] }
+              .map { |pattern| [Regexp.new("(?<=^|#{URL_UNSAFE_CHARS})#{pattern}", Regexp::IGNORECASE, :timeout => 1), expanded_path] }
         end
     end
 
