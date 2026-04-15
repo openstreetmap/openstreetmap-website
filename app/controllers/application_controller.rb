0ddad1b231edca08d2846d6f8fefef7b0284ec72
Rub21 <rub2106@gmail.com>
2026-04-15 16:31:24 -0500

    Handle DatabaseConnectionError when gps-db is down adn the site is starting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,9 @@ class ApplicationController < ActionController::Base
   check_authorization
 
   rescue_from RailsParam::InvalidParameterError, :with => :invalid_parameter
+  
   rescue_from ActiveRecord::ConnectionNotEstablished,
+              ActiveRecord::DatabaseConnectionError,
               ActiveRecord::NoDatabaseError, :with => :gps_database_unavailable
 
   after_action :close_body
