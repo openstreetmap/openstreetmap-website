4eb17c5a5fde6fc26be87579d2bf5b50e660063b
Pablo Brasero <pablo@pablobm.com>
2026-04-20 15:26:48 +0100

    Skeleton to implement each type of web notification

--- test/factories/notifications.rb
+++ test/factories/notifications.rb
@@ -1,6 +1,37 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :notification, :class => "ApplicationNotifier::Notification" do
+    initialize_with do
+      notifier_class = self.notifier_class || "#{record.class.name}Notifier".constantize
+      notification_class = notifier_class::Notification
+      notification_class.new
+    end
+
+    recipient :factory => :user
+
+    event do
+      association :notifier, :record => record, :notifier_class => notifier_class, :params => notifier_params
+    end
+
+    transient do
+      record { nil }
+      notifier_class { nil }
+      notifier_params { nil }
+    end
+  end
+
+  factory :notifier, :class => "ApplicationNotifier" do
+    initialize_with do
+      notifier_class = self.notifier_class || "#{record.class.name}Notifier".constantize
+      notifier_class.new(:record => record)
+    end
+
+    transient do
+      notifier_class { nil }
+    end
+  end
+
   factory :changeset_comment_notification, :class => "ChangesetCommentNotifier::Notification" do
     event :factory => :changeset_comment_notifier
     recipient :factory => :user
