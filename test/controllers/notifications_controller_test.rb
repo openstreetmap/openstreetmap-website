4cd5bee0a64df37e47b394f5436a56641ff86e2e
Pablo Brasero <pablo@pablobm.com>
2026-04-20 14:29:16 +0100

    Create page to list notifications

---
+++ test/controllers/notifications_controller_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class NotificationsControllerTest < ActionDispatch::IntegrationTest
+  def test_index
+    session_for(create(:user))
+    get notifications_path
+
+    assert_response :success
+    assert_template "index"
+  end
+
+  def test_index_unauthorized
+    get notifications_path
+
+    assert_redirected_to login_path(:referer => notifications_path)
+  end
+end
