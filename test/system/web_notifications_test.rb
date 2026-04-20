f85d5d469233884efb18b610fdf7e64de3cd5255
Pablo Brasero <pablo@pablobm.com>
2026-04-20 15:26:48 +0100

    Implement web notifications for changeset comments

--- test/system/web_notifications_test.rb
+++ test/system/web_notifications_test.rb
@@ -13,4 +13,33 @@ class WebNotificationsTest < ApplicationSystemTestCase
     assert_text "Notifications"
     assert_text "You have no notifications"
   end
+
+  test "read latest notifications" do
+    changeset_author = create(:user)
+    commenter = create(:user, :display_name => "Commenter")
+    setup_changeset_comment(
+      :changeset_author => changeset_author,
+      :commenter => commenter
+    )
+
+    sign_in_as(changeset_author)
+
+    click_on changeset_author.display_name
+    click_on "My Notifications"
+
+    assert_text "Notifications"
+    assert_text "Changeset comment"
+    assert_text "User Commenter left a comment on changeset"
+  end
+
+  private
+
+  def setup_changeset_comment(changeset_author:, commenter:, comment_attrs: {})
+    changeset = create(:changeset, :user => changeset_author)
+    create(:changeset_subscription, :changeset => changeset, :subscriber => changeset_author)
+
+    comment = create(:changeset_comment, :changeset => changeset, :author => commenter, **comment_attrs)
+    create(:changeset_subscription, :changeset => changeset, :subscriber => commenter)
+    ChangesetCommentNotifier.with(:record => comment).deliver
+  end
 end
