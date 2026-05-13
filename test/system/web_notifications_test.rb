9a7e14e2e952ed8c160f32308eb512329a067c8e
Pablo Brasero <pablo@pablobm.com>
2026-05-13 11:00:50 +0100

    Paginate notifications

--- test/system/web_notifications_test.rb
+++ test/system/web_notifications_test.rb
@@ -32,6 +32,31 @@ class WebNotificationsTest < ApplicationSystemTestCase
     assert_text "User Commenter left a comment on changeset"
   end
 
+  test "pagination" do
+    changeset_author = create(:user)
+    commenter = create(:user, :display_name => "Commenter")
+    1.upto(30).each do |i|
+      setup_changeset_comment(
+        :changeset_author => changeset_author,
+        :commenter => commenter,
+        :comment_attrs => {
+          :body => "This is comment number #{i}"
+        }
+      )
+    end
+
+    sign_in_as(changeset_author)
+
+    click_on changeset_author.display_name
+    click_on "My Notifications"
+
+    assert_text "This is comment number 30"
+    assert_no_text "This is comment number 10"
+    click_on "Older Notifications"
+    assert_no_text "This is comment number 30"
+    assert_text "This is comment number 10"
+  end
+
   private
 
   def setup_changeset_comment(changeset_author:, commenter:, comment_attrs: {})
