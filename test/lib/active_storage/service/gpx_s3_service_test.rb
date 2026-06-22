a9b3a9588406b08b02680bba6710186f7728a7c3
Rub21 <rub2106@gmail.com>
2026-06-22 13:36:52 -0500

    Add S3 service that sets Content-Encoding on upload

---
+++ test/lib/active_storage/service/gpx_s3_service_test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class GpxS3ServiceTest < ActiveSupport::TestCase
+  def setup
+    @service = ActiveStorage::Service::GpxS3Service.new(
+      :bucket => "test-bucket",
+      :region => "eu-west-1",
+      :stub_responses => true
+    )
+  end
+
+  def test_sets_content_encoding_for_server_gzipped_flag
+    @service.upload("key", StringIO.new("data"),
+                    :content_type => "application/gpx+xml",
+                    :custom_metadata => { "server_gzipped" => true })
+
+    request = find_latest_put_object_request
+    assert_equal "gzip", request[:params][:content_encoding]
+    # The flag drives the header, it isn't stored as S3 metadata.
+    assert_empty(request[:params][:metadata] || {})
+  end
+
+  def test_keeps_default_upload_without_server_gzipped_flag
+    @service.upload("key", StringIO.new("data"), :content_type => "application/gpx+xml")
+
+    assert_nil find_latest_put_object_request[:params][:content_encoding]
+  end
+
+  def test_serves_content_encoding
+    assert_predicate @service, :serves_content_encoding?
+  end
+
+  private
+
+  def find_latest_put_object_request
+    @service.client.client.api_requests.reverse_each.find { |request| request[:operation_name] == :put_object }
+  end
+end
