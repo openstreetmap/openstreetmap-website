4b14eafab0ff3ddf23846202d1607744284e3c0b
Frank Elsinga <frank@elsinga.de>
2026-06-28 10:20:48 +0200

    Fix moveend listener leak in MapLibre AttributionControl

--- app/assets/javascripts/maplibre/attribution.js
+++ app/assets/javascripts/maplibre/attribution.js
@@ -8,6 +8,7 @@ OSM.MapLibre.AttributionControl = class extends maplibregl.AttributionControl {
     this._container = null;
     this._includeReportLink = Boolean(includeReportLink);
     this._credit = credit;
+    this._boundUpdateReportLink = this._updateReportLink.bind(this);
   }
 
   _updateAttributions() {
@@ -91,7 +92,7 @@ OSM.MapLibre.AttributionControl = class extends maplibregl.AttributionControl {
 
     if (this._includeReportLink) {
       map.once("load", this._updateAttributions.bind(this));
-      map.on("moveend", this._updateReportLink.bind(this));
+      map.on("moveend", this._boundUpdateReportLink);
     }
 
     return super.onAdd(map);
@@ -101,7 +102,7 @@ OSM.MapLibre.AttributionControl = class extends maplibregl.AttributionControl {
     if (!this._map) return;
 
     if (this._includeReportLink) {
-      this._map.off("moveend", this._updateReportLink.bind(this));
+      this._map.off("moveend", this._boundUpdateReportLink);
     }
     this._map = null;
     super.onRemove();
