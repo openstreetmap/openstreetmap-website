767fc338131eb87ed35a8f14b2b8978a887fa367
Tom Hughes <tom@compton.nu>
2026-09-17 18:54:37 +0100

    Send request parameters to any configured sentry endpoint

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,5 +5,7 @@ if Settings.key?(:sentry_dsn)
     config.dsn = Settings.sentry_dsn
     config.traces_sample_rate = Settings.sentry_traces_sample_rate if Settings.key?(:sentry_traces_sample_rate)
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.data_collection.url_query_params.mode = :deny_list
+    config.data_collection.url_query_params.terms = %w[password pass_crypt pass_crypt_confirmation]
   end
 end
